220d6808b2cbb48fedf1fc7069ca22b8dec56bc2
Aki Sasaki <aki@escapewindow.com>
2013-11-15 01:04:50 -0800

    no id for hashes

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,14 +6,13 @@ CREATE TABLE IF NOT EXISTS projects (
 );
 
 CREATE TABLE IF NOT EXISTS hashes (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     hg_changeset VARCHAR(40) NOT NULL,
     git_changeset VARCHAR(40) NOT NULL,
     project_id SMALLINT UNSIGNED NOT NULL,
     date_added int UNSIGNED NOT NULL,
-    PRIMARY KEY (id),
     INDEX (hg_changeset),
     INDEX (git_changeset),
     INDEX (project_id),
-    UNIQUE INDEX (hg_changeset, git_changeset, project_id)
+    UNIQUE INDEX (hg_changeset, project_id),
+    UNIQUE INDEX (git_changeset, project_id)
 );
