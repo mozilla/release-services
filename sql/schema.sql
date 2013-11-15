082d56f517a97ffcea190d18aeb3382d19ece3b9
Aki Sasaki <aki@escapewindow.com>
2013-11-15 00:02:57 -0800

    schema first pass

---
+++ sql/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS projects (
+   id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+   name VARCHAR(255) NOT NULL,
+   PRIMARY KEY (id),
+   UNIQUE KEY (name)
+);
+
+CREATE TABLE IF NOT EXISTS hashes (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   hg_changeset VARCHAR(40) NOT NULL,
+   git_changeset VARCHAR(40) NOT NULL,
+   date_added DATETIME NOT NULL,
+
+   PRIMARY KEY (id),
+   UNIQUE INDEX (hg_changeset),
+   UNIQUE INDEX (git_changeset)
+);
+
+CREATE TABLE IF NOT EXISTS projects_to_hashes (
+    project_id INT UNSIGNED NOT NULL,
+    hash_id INT UNSIGNED NOT NULL,
+    KEY (project_id),
+    KEY (hash_id)
+);
