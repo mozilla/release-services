46d96e457ecc9604e638e4321cb30642df790547
Aki Sasaki <aki@escapewindow.com>
2013-11-15 13:02:52 -0800

    woot, query db for changeset

--- mapper/app.py
+++ mapper/app.py
@@ -1,61 +1,28 @@
 #!/usr/bin/env python
-import os
 import logging
-import time
-
-import mmap
 
 from bottle import route, run, abort, default_app
 import bottle_mysql
 
-from mapper.bsearch import mapfile_binary_search
-
 log = logging.getLogger(__name__)
 
 
-mapfile_dir = os.path.join(os.getcwd(), "mapfiles")
-mapfile_cache = {}
-mapfile_last_check = {}
-mapfile_stat_cache = {}
-
-
-@route('/<project>/<vcs>/<rev>')
-def get_rev(project, vcs, rev):
+@route('/<project>/rev/<vcs>/<rev>')
+def get_rev(project, vcs, rev, db):
     """Translate git/hg revisions"""
     assert vcs in ("git", "hg")
     if vcs == 'git':
-        mapfile = 'hg2git'
+        target_column = 'git_changeset'
+        source_column = 'hg_changeset'
     elif vcs == 'hg':
-        mapfile = 'git2hg'
-    mapfile = os.path.join(mapfile_dir, project, mapfile)
-    if not os.path.exists(mapfile):
-        log.debug("%s doesn't exist", mapfile)
-        abort(404, "%s's mapfile not found" % project)
-
-    m = None
-    if mapfile in mapfile_cache:
-        # Check the file occasionally to see if we need to re-load it
-        m = mapfile_cache[mapfile]
-        if time.time() - mapfile_last_check[mapfile] > 10:
-            # Check it!
-            s = os.stat(mapfile)
-            if (s.st_ino, s.st_size, s.st_mtime) != mapfile_stat_cache[mapfile]:
-                # The mapfile has changed somehow. Reset m to None so that we
-                # enter the if block below and reload the mapfile.
-                log.info("reloading %s", mapfile)
-                m = None
-
-    if m is None:
-        fd = open(mapfile, 'rb')
-        m = mmap.mmap(fd.fileno(), 0, mmap.MAP_PRIVATE, mmap.PROT_READ)
-        mapfile_cache[mapfile] = m
-        mapfile_last_check[mapfile] = time.time()
-        s = os.fstat(fd.fileno())
-        mapfile_stat_cache[mapfile] = (s.st_ino, s.st_size, s.st_mtime)
-
-    s = time.time()
-    rev = mapfile_binary_search(m, rev)
-    return {"%s_rev" % vcs: rev}
+        target_column = 'hg_changeset'
+        source_column = 'git_changeset'
+    query = 'SELECT %s FROM hashes, projects WHERE %s LIKE "%s%%" AND projects.id=hashes.project_id and name="%s";' % (target_column, source_column, rev, project)
+    db.execute(query)
+    row = db.fetchone()
+    if row:
+        return row
+    abort(404, "%s - %s not found" % (query, target_column))
 
 
 def main():
@@ -66,6 +33,9 @@ def main():
 
 
 app = default_app()
+# TODO get this in a file
+plugin = bottle_mysql.Plugin(dbuser='user', dbpass='pass', dbname='mapper')
+app.install(plugin)
 
 if __name__ == '__main__':
     main()
