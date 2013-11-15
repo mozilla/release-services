d5bd1592674c4e97042264e58fb6c72e9fe1abec
Aki Sasaki <aki@escapewindow.com>
2013-11-15 15:03:50 -0800

    dependencies, bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(name='mapper',
-      version='0.1',
+      version='0.2',
       description='hg to git mapper',
       author='Chris AtLee',
       author_email='chris@atlee.ca',
       url='https://github.com/catlee/mapper',
       packages=['mapper'],
-      install_requires=['bottle'],
+      install_requires=['bottle', 'bottle_mysql', 'MySQL-python'],
       license='MPL2',
       )
