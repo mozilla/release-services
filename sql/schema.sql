1845623dd3b2e0d442550d59f33f320f7df00699
Aki Sasaki <aki@escapewindow.com>
2013-11-15 00:55:22 -0800

    schema fix

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,25 +1,19 @@
 CREATE TABLE IF NOT EXISTS projects (
-   id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-   name VARCHAR(255) NOT NULL,
-   PRIMARY KEY (id),
-   UNIQUE KEY (name)
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (name)
 );
 
 CREATE TABLE IF NOT EXISTS hashes (
-   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-   hg_changeset VARCHAR(40) NOT NULL,
-   git_changeset VARCHAR(40) NOT NULL,
-
-   PRIMARY KEY (id),
-   UNIQUE INDEX (hg_changeset),
-   UNIQUE INDEX (git_changeset)
-);
-
-CREATE TABLE IF NOT EXISTS projects_to_hashes (
-    project_id INT UNSIGNED NOT NULL,
-    hash_id INT UNSIGNED NOT NULL,
-    date_added DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    KEY (project_id),
-    KEY (hash_id),
-    KEY (date_added)
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    hg_changeset VARCHAR(40) NOT NULL,
+    git_changeset VARCHAR(40) NOT NULL,
+    project_id SMALLINT UNSIGNED NOT NULL,
+    date_added int UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (hg_changeset),
+    INDEX (git_changeset),
+    INDEX (project_id),
+    UNIQUE INDEX (hg_changeset, git_changeset, project_id)
 );
