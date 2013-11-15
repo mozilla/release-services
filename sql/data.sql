3d543692c9a5c72bfd6afe5be5f119264f570235
Aki Sasaki <aki@escapewindow.com>
2013-11-15 00:14:56 -0800

    data.sql

---
+++ sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO projects values (NULL, 'gecko');
+INSERT INTO projects values (NULL, 'gecko-dev');
+INSERT INTO projects values (NULL, 'gecko-projects');
+INSERT INTO projects values (NULL, 'gecko-combined');
