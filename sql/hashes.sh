29d21097bdfb73e85c6f9f591dbd530cea02c601
Aki Sasaki <aki@escapewindow.com>
2013-11-16 17:35:56 -0800

    change hashes

--- sql/hashes.sh
+++ sql/hashes.sh
@@ -3,5 +3,11 @@
 while read line; do
     hg_sha=`echo $line | sed -e 's/ .*//'`
     git_sha=`echo $line | sed -e 's/.* //'`
-    echo "insert into hashes values (NULL, '$hg_sha', '$git_sha', 3, unix_timestamp());"
-done < ~/Desktop/combined_gecko_mapfile
+    echo "insert into hashes values ('$hg_sha', '$git_sha', 3, unix_timestamp());"
+done < ~/Desktop/project-branches-mapfile
+
+while read line; do
+    hg_sha=`echo $line | sed -e 's/ .*//'`
+    git_sha=`echo $line | sed -e 's/.* //'`
+    echo "insert into hashes values ('$hg_sha', '$git_sha', 2, unix_timestamp());"
+done < ~/Desktop/gecko-mapfile
