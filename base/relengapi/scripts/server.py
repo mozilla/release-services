b0f257b85b83cca26c7aa2d40d663d86bfe98b0a
Dustin J. Mitchell <dustin@mozilla.com>
2013-11-26 18:04:08 -0500

    allow --port

--- base/relengapi/scripts/server.py
+++ base/relengapi/scripts/server.py
@@ -5,6 +5,8 @@ def main():
     parser = argparse.ArgumentParser(description="Run a dev instance of relengapi")
     parser.add_argument("-a", "--all-interfaces", action='store_true',
                         help='Run on all interfaces, not just localhost')
+    parser.add_argument("-p", "--port", type=int, default=5000,
+                        help='Port on which to serve')
     parser.add_argument("--no-debug", action='store_true',
                         help="Don't run in debug mode")
     args = parser.parse_args()
@@ -13,5 +15,6 @@ def main():
     if args.all_interfaces:
         kwargs['host'] = '0.0.0.0'
     kwargs['debug'] = not args.no_debug
+    kwargs['port'] = args.port
 
     create_app(cmdline=True).run(**kwargs)
