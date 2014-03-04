b865f47f72cab34e2a886b65851cd17965b340c7
Dustin J. Mitchell <dustin@mozilla.com>
2014-03-04 17:55:18 -0500

    add license

--- jacuzzi-allocator/relengapi/blueprints/jacuzzi_allocator.py
+++ jacuzzi-allocator/relengapi/blueprints/jacuzzi_allocator.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from sqlalchemy import Column, Integer, String, Table, ForeignKey, Index
 from sqlalchemy.orm import relationship
 from flask import Blueprint
