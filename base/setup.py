336af4bf625f42df97f5383631fc8d6abf455d6e
Dustin J. Mitchell <dustin@mozilla.com>
2014-04-28 16:25:56 -0400

    bump to 0.1.5

--- base/setup.py
+++ base/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='relengapi',
-    version='0.1.4',
+    version='0.1.5',
     description='The code behind https://api.pub.build.mozilla.org',
     author='Dustin J. Mitchell',
     author_email='dustin@mozilla.com',
