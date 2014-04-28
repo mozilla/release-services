d0a50901aeb7ec5887f8737261c62cea0a45c885
Dustin J. Mitchell <dustin@mozilla.com>
2014-04-28 16:05:12 -0400

    ship 0.1.4

--- base/setup.py
+++ base/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='relengapi',
-    version='0.1.3',
+    version='0.1.4',
     description='The code behind https://api.pub.build.mozilla.org',
     author='Dustin J. Mitchell',
     author_email='dustin@mozilla.com',
--- docs/setup.py
+++ docs/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='relengapi-docs',
-    version='0.1.1',
+    version='0.1.4',
     description='Documentation blueprint for relengapi',
     author='Dustin J. Mitchell',
     author_email='dustin@mozilla.com',
