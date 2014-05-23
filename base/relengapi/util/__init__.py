fd7faedad70335303c3ad32be15b6d2ff8050264
Justin Wood <Callek@gmail.com>
2014-05-23 00:23:26 -0400

    refactor util to its own module, and add timezone helpers as its own file rather than a fake-datetime module

--- base/relengapi/util/__init__.py
+++ base/relengapi/util/__init__.py
@@ -2,9 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import datetime as _datetime
 import importlib
-import pytz
 import wrapt
 
 
@@ -29,23 +27,3 @@ def make_support_class(app, module_path, mechanisms, config_key, default):
     mech_module = importlib.import_module(module_name, module_path)
     mech_class = getattr(mech_module, class_name)
     return mech_class(app)
-
-class datetime():
-    class datetime(_datetime.datetime):
-        @classmethod
-        def utcnow(cls):
-            return _datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
-        @classmethod
-        def utcfromtimestamp(cls, timestamp):
-            return _datetime.datetime.utcfromtimestamp(timestamp).replace(tzinfo=pytz.UTC)
-    # The following are just pointers to facilitate code sanity
-    date = _datetime.date
-    time = _datetime.time
-    timedelta = _datetime.timedelta
-    tzinfo = _datetime.tzinfo
-def dt_as_timezone(obj, dest_tzinfo):
-    if not issubclass(obj, _datetime):
-        raise ValueError("Must pass a datetime object")
-    if obj.tzinfo is None:
-        raise ValueError("Must pass a timezone aware datetime object")
-    return dest_tzinfo.normalize(obj.astimezone(dest_tzinfo))
---
+++ base/relengapi/util/tz.py
@@ -0,0 +1,18 @@
+import datetime
+import pytz
+
+
+def utcnow():
+    return datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+
+
+def utcfromtimestamp(timestamp):
+    return datetime.datetime.utcfromtimestamp(timestamp).replace(tzinfo=pytz.UTC)
+
+
+def dt_as_timezone(obj, dest_tzinfo):
+    if not issubclass(obj, datetime):
+        raise ValueError("Must pass a datetime object")
+    if obj.tzinfo is None:
+        raise ValueError("Must pass a timezone aware datetime object")
+    return dest_tzinfo.normalize(obj.astimezone(dest_tzinfo))
