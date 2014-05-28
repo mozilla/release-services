585480c01a866c4c9694310250efab9f862a0a45
Pete Moore <pmoore@mozilla.com>
2014-05-28 21:29:09 +0200

    Properly installs, before removing build-relengapi

--- install_relengapi.sh
+++ install_relengapi.sh
@@ -2,7 +2,7 @@
 
 git clone http://github.com/mozilla/build-relengapi
 cd build-relengapi
-pip install -e base[test]
+pip install base[test]
 cd ..
 mv build-relengapi/base/pep8rc .
 mv build-relengapi/base/pylintrc .
