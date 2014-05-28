fb5d350c4c4c6c34c3cb79f26c65ceb826be776e
Pete Moore <pmoore@mozilla.com>
2014-05-28 21:48:28 +0200

    Revert "Trying again to install as a package :)"
    This reverts commit 93b8fd8af0c1ecd33cca3ce47b6f0597c7344d9e.

--- install_relengapi.sh
+++ install_relengapi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -exv
 
 git clone http://github.com/mozilla/build-relengapi
-cd build-relengapi/base
-python setup.py install
-mv pep8rc ../..
-mv pylintrc ../..
-cd ../..
+cd build-relengapi
+pip install base[test]
+cd ..
+mv build-relengapi/base/pep8rc .
+mv build-relengapi/base/pylintrc .
 rm -rf build-relengapi
