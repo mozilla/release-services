1f7d7368a8af1ef4152b1f140b20e1e31d6e1c8e
Dustin J. Mitchell <dustin@mozilla.com>
2014-05-29 22:18:58 -0400

    remove unused 'make_support_class' method

--- base/relengapi/util/__init__.py
+++ base/relengapi/util/__init__.py
@@ -12,18 +12,3 @@ def synchronized(lock):
         with lock:
             return wrapper(*args, **kwargs)
     return wrap
-
-
-def make_support_class(app, module_path, mechanisms, config_key, default):
-    mechanism = app.config.get(config_key, {}).get('type', default)
-    try:
-        module_name, class_name = mechanisms[mechanism]
-    except KeyError:
-        raise RuntimeError("no such %s type '%s'" % (config_key, mechanism))
-
-    # stash this for easy access from the templates
-    app.config[config_key + '_TYPE'] = mechanism
-
-    mech_module = importlib.import_module(module_name, module_path)
-    mech_class = getattr(mech_module, class_name)
-    return mech_class(app)
