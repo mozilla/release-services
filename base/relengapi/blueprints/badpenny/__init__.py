6697f676bc66dc927ca3b58d2f92afd3aed3766b
Dustin J. Mitchell <dustin@mozilla.com>
2014-09-04 09:17:24 -0400

    refactor badpenny blueprint into several modules

--- base/relengapi/blueprints/badpenny/__init__.py
+++ base/relengapi/blueprints/badpenny/__init__.py
@@ -2,11 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from relengapi import db
-import wsme.types
 import logging
-import datetime
-import sqlalchemy as sa
 from flask import url_for
 from flask import Blueprint
 from relengapi import p
@@ -15,6 +11,8 @@ from relengapi.lib import permissions
 from relengapi.lib import angular
 from relengapi.lib import api
 from relengapi.lib import badpenny
+from relengapi.blueprints.badpenny import tables
+from relengapi.blueprints.badpenny import rest
 from werkzeug.exceptions import NotFound
 
 logger = logging.getLogger(__name__)
@@ -31,134 +29,6 @@ bp.root_widget_template(
     'badpenny_root_widget.html', priority=100, condition=permitted)
 
 
-class BadpennyJob(db.declarative_base('relengapi')):
-    __tablename__ = 'badpenny_jobs'
-
-    id = sa.Column(sa.Integer, primary_key=True)
-    task_id = sa.Column(sa.Integer, sa.ForeignKey('badpenny_tasks.id'))
-    task = sa.orm.relationship('BadpennyTask')
-
-    created_at = sa.Column(sa.DateTime(), nullable=False)
-    started_at = sa.Column(sa.DateTime(), nullable=True)
-    completed_at = sa.Column(sa.DateTime(), nullable=True)
-    successful = sa.Column(sa.Boolean())
-
-    # 'result' is JSON data
-    result = sa.Column(sa.Text())
-
-    # 'logs' is free-form, hopefully brief, log text
-    logs = sa.Column(sa.Text())
-
-    def to_jsonjob(self):
-        return JsonJob(id=self.id,
-                       task_name=self.task.name,
-                       created_at=self.created_at,
-                       started_at=self.started_at,
-                       completed_at=self.completed_at,
-                       successful=self.successful,
-                       result=self.result,
-                       logs=self.logs)
-
-
-class JsonJob(wsme.types.Base):
-
-    """A job is a single occurrence of a task."""
-
-    _name = "BadpennyJob"
-
-    #: unique job id
-    id = wsme.types.wsattr(int, mandatory=True)
-
-    #: name of the task that created this job
-    task_name = wsme.types.wsattr(unicode, mandatory=True)
-
-    #: time at which this job was created
-    created_at = wsme.types.wsattr(datetime.datetime, mandatory=True)
-
-    #: time at which this job started executing
-    started_at = wsme.types.wsattr(datetime.datetime, mandatory=False)
-
-    #: time at which this job finished executing
-    completed_at = wsme.types.wsattr(datetime.datetime, mandatory=False)
-
-    #: true if the job was successful
-    successful = wsme.types.wsattr(bool, mandatory=False)
-
-    #: arbitrary JSON-formatted string containing output from the job
-    result = wsme.types.wsattr(unicode, mandatory=False)
-
-    #: text log from the job
-    logs = wsme.types.wsattr(unicode, mandatory=False)
-
-
-class BadpennyTask(db.declarative_base('relengapi'), db.UniqueMixin):
-    __tablename__ = 'badpenny_tasks'
-
-    id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.Text, nullable=False)
-
-    # all other data about a task is determined from runtime information (that
-    # is, from the decorated function itself)
-
-    jobs = sa.orm.relationship('BadpennyJob')
-
-    @property
-    def last_success(self):
-        # TODO: get this value as part of the DB query for the task
-        job = BadpennyJob.query. \
-            filter(BadpennyJob.task_id == self.id). \
-            order_by(sa.desc(BadpennyJob.created_at)). \
-            limit(1). \
-            first()
-        if not job:
-            return -1
-        elif job.successful:
-            return 1
-        else:
-            return 0
-
-    @classmethod
-    def unique_filter(cls, query, name):
-        return query.filter(BadpennyTask.name == name)
-
-    @classmethod
-    def unique_hash(cls, name):
-        return name
-
-    def to_jsontask(self, with_jobs=False):
-        runtime_task = badpenny.Task.get(self.name)
-        task = JsonTask(name=self.name, last_success=self.last_success,
-                        active=bool(runtime_task))
-        if runtime_task:
-            task.schedule = runtime_task.schedule
-        if with_jobs:
-            task.jobs = [j.to_jsonjob() for j in self.jobs]
-        return task
-
-
-class JsonTask(wsme.types.Base):
-
-    """A task describes an operation that occurs periodically."""
-
-    _name = "BadpennyTask"
-
-    #: unique task name (based on the qualified Python function name)
-    name = wsme.types.wsattr(unicode, mandatory=True)
-
-    #: last success of the task: -1 (never run), 0 (failed), or 1 (succeeded)
-    last_success = wsme.types.wsattr(int, mandatory=True)
-
-    #: all recent jobs for this task; this is only returned when a single task
-    # is requested.
-    jobs = wsme.types.wsattr([JsonJob], mandatory=False)
-
-    #: true if the task is active (that is, if it is defined in the code).
-    active = wsme.types.wsattr(bool, mandatory=True)
-
-    #: a pretty description of the task's schedule, if active
-    schedule = wsme.types.wsattr(unicode, mandatory=False)
-
-
 @bp.route('/')
 @p.base.badpenny.view.require()
 def root():
@@ -169,42 +39,44 @@ def root():
 
 
 @bp.route('/tasks')
-@apimethod([JsonTask], bool)
+@apimethod([rest.BadpennyTask], bool)
 @p.base.badpenny.view.require()
 def list_tasks(all=False):
     """List all badpenny tasks.  With "?all=1", include inactive tasks."""
-    rv = [t.to_jsontask() for t in BadpennyTask.query.all()]
+    rv = [t.to_jsontask() for t in tables.BadpennyTask.query.all()]
     if not all:
         rv = [t for t in rv if t.active]
     return rv
 
 
 @bp.route('/tasks/<task_name>')
-@apimethod(JsonTask, unicode)
+@apimethod(rest.BadpennyTask, unicode)
 @p.base.badpenny.view.require()
 def get_task(task_name):
     """Get information on a badpenny task by name."""
-    t = BadpennyTask.query.filter(BadpennyTask.name == task_name).first()
+    t = tables.BadpennyTask.query.filter(
+        tables.BadpennyTask.name == task_name).first()
     if not t:
         raise NotFound
     return t.to_jsontask(with_jobs=True)
 
 
 @bp.route('/jobs')
-@apimethod([JsonJob])
+@apimethod([rest.BadpennyJob])
 @p.base.badpenny.view.require()
 def list_jobs():
     """List all badpenny jobs."""
-    return [t.to_jsonjob() for t in BadpennyJob.query.all()]
+    return [t.to_jsonjob() for t in tables.BadpennyJob.query.all()]
 
 
 @bp.route('/jobs/<job_id>')
-@apimethod(JsonJob, int)
+@apimethod(rest.BadpennyJob, int)
 @p.base.badpenny.view.require()
 def get_job(job_id):
     """Get information on a badpenny job by its ID.  Use this to poll for job
     completion."""
-    j = BadpennyJob.query.filter(BadpennyJob.id == job_id).first()
+    j = tables.BadpennyJob.query.filter(
+        tables.BadpennyJob.id == job_id).first()
     if not j:
         raise NotFound
     return j.to_jsonjob()
@@ -214,5 +86,5 @@ def sync_tasks(state):  # not used yet
     """Synchronize tasks defined in code into the DB"""
     with state.app.app_context():
         for task in badpenny.Task.list():
-            BadpennyTask.as_unique(
+            tables.BadpennyTask.as_unique(
                 state.app.db.session('relengapi'), name=task.name)
--- base/relengapi/blueprints/badpenny/test_badpenny.py
+++ base/relengapi/blueprints/badpenny/test_badpenny.py
@@ -9,10 +9,8 @@ from flask import json
 from nose.tools import eq_
 from relengapi.testing import TestContext
 from relengapi.lib.permissions import p
-from relengapi.blueprints.badpenny import BadpennyTask
-from relengapi.blueprints.badpenny import JsonTask
-from relengapi.blueprints.badpenny import BadpennyJob
-from relengapi.blueprints.badpenny import JsonJob
+from relengapi.blueprints.badpenny import tables
+from relengapi.blueprints.badpenny import rest
 
 
 dt = datetime.datetime
@@ -23,7 +21,7 @@ test_context = TestContext(databases=['relengapi'],
 
 def insert_job(app, task_id, created_at, **kwargs):
     session = app.db.session('relengapi')
-    j = BadpennyJob(task_id=task_id, created_at=created_at, **kwargs)
+    j = tables.BadpennyJob(task_id=task_id, created_at=created_at, **kwargs)
     session.add(j)
     session.commit()
     return j
@@ -31,13 +29,13 @@ def insert_job(app, task_id, created_at, **kwargs):
 
 def insert_task(app, name):
     session = app.db.session('relengapi')
-    t = BadpennyTask(name=name)
+    t = tables.BadpennyTask(name=name)
     session.add(t)
     session.commit()
     return t
 
-cleanup_task = JsonTask(active=False, name="cleanup", last_success=0)
-cleanup_job_1 = JsonJob(
+cleanup_task = rest.BadpennyTask(active=False, name="cleanup", last_success=0)
+cleanup_job_1 = rest.BadpennyJob(
     id=1,
     result=json.dumps({"deleted": 6}),
     successful=True,
@@ -46,7 +44,7 @@ cleanup_job_1 = JsonJob(
     created_at=dt(1978, 6, 15),
     started_at=dt(1978, 6, 15, 12),
     completed_at=dt(1978, 6, 15, 13))
-cleanup_job_2 = JsonJob(
+cleanup_job_2 = rest.BadpennyJob(
     id=2,
     result=json.dumps({"error": "ETIMEOUT"}),
     successful=False,
@@ -56,8 +54,8 @@ cleanup_job_2 = JsonJob(
     started_at=dt(1978, 6, 16, 12),
     completed_at=dt(1978, 6, 16, 13))
 
-report_task = JsonTask(active=False, name="report", last_success=1)
-report_job_1 = JsonJob(
+report_task = rest.BadpennyTask(active=False, name="report", last_success=1)
+report_job_1 = rest.BadpennyJob(
     id=3,
     result=json.dumps({}),
     successful=True,
@@ -67,7 +65,7 @@ report_job_1 = JsonJob(
     started_at=dt(1978, 6, 2, 12),
     completed_at=dt(1978, 6, 2, 13))
 
-check_task = JsonTask(active=False, name="check", last_success=-1)
+check_task = rest.BadpennyTask(active=False, name="check", last_success=-1)
 
 
 def add_data(app):
@@ -119,21 +117,23 @@ def active_tasks(*tasks):
 
 @test_context.specialize(app_setup=add_data)
 def test_to_jsontask(app):
-    """Converting a BadpennyTask to a JsonTask produces well-formed output"""
+    """Converting a tables.BadpennyTask to a rest.BadpennyTask produces well-formed output"""
     with app.test_request_context():
-        t = BadpennyTask.query.filter(BadpennyTask.name == 'cleanup').first()
+        t = tables.BadpennyTask.query.filter(
+            tables.BadpennyTask.name == 'cleanup').first()
         eq_(json.dumps(t.to_jsontask()), json.dumps(cleanup_task))
-        t = BadpennyTask.query.filter(BadpennyTask.name == 'report').first()
+        t = tables.BadpennyTask.query.filter(
+            tables.BadpennyTask.name == 'report').first()
         eq_(json.dumps(t.to_jsontask(with_jobs=True)),
-            json.dumps(JsonTask(active=False, name="report", last_success=1,
-                                jobs=[report_job_1])))
+            json.dumps(rest.BadpennyTask(active=False, name="report", last_success=1,
+                                         jobs=[report_job_1])))
 
 
 @test_context.specialize(app_setup=add_data)
 def test_last_success(app):
     """The last_success property returns -1 for never, 0 for failure, and 1 for success"""
-    q = lambda name: BadpennyTask.query.filter(
-        BadpennyTask.name == name).first()
+    q = lambda name: tables.BadpennyTask.query.filter(
+        tables.BadpennyTask.name == name).first()
     with app.test_request_context():
         eq_(q('cleanup').last_success, 0)
         eq_(q('report').last_success, 1)
@@ -145,21 +145,21 @@ def test_unique_task(app):
     """Finding a task with `as_unique` either creates a new task row or finds an existing row"""
     with app.test_request_context():
         sess = app.db.session('relengapi')
-        check_id = BadpennyTask.query.filter(
-            BadpennyTask.name == 'check').first().id
-        existing_task = BadpennyTask.as_unique(sess, name='check')
+        check_id = tables.BadpennyTask.query.filter(
+            tables.BadpennyTask.name == 'check').first().id
+        existing_task = tables.BadpennyTask.as_unique(sess, name='check')
         eq_(existing_task.id, check_id)
-        new_task = BadpennyTask.as_unique(sess, name='update')
-        assert new_task.id == BadpennyTask.query.filter(
-            BadpennyTask.name == 'update').first().id
+        new_task = tables.BadpennyTask.as_unique(sess, name='update')
+        assert new_task.id == tables.BadpennyTask.query.filter(
+            tables.BadpennyTask.name == 'update').first().id
 
 
 @test_context.specialize(app_setup=add_data)
 def test_to_jsonjob(app):
-    """Converting a BadpennyJob to a JsonJob produces output with matching fields"""
+    """Converting a tables.BadpennyJob to a rest.BadpennyJob produces output with matching fields"""
     with app.test_request_context():
-        j = BadpennyJob.query.filter(
-            BadpennyJob.id == cleanup_job_2.id).first()
+        j = tables.BadpennyJob.query.filter(
+            tables.BadpennyJob.id == cleanup_job_2.id).first()
         eq_(json.dumps(j.to_jsonjob()), json.dumps(cleanup_job_2))
 
 
