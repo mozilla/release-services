03190e24c865a2a67cb8f5be5391ce9a33913540
Justin Wood <Callek@gmail.com>
2014-09-25 01:11:30 -0400

    Merge to slaveloan changes from 0e9446b

--- fiximports.py
+++ fiximports.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 '''Check and sort import statement from a python file '''
-# code from buildbot source code https://github.com/buildbot/buildbot/blob/master/common/fiximports.py
+
+# code from buildbot source code
+# https://github.com/buildbot/buildbot/blob/master/common/fiximports.py
+# permission granted by authors @tardyp and @Stibbons in
+# https://github.com/mozilla/build-relengapi/pull/116
 
 import re
 import sys
@@ -153,7 +157,8 @@ class FixImports(object):
 
 
 def main():
-    '''I am the main method'''
+    '''I am the main method. Exit status 1 means failure; exit status 2 means
+    changes were made, and exit status 0 means no changes.'''
     if len(sys.argv) != 2:
         print "usage: %s <python file>" % (sys.argv[0])
         sys.exit(1)
@@ -169,7 +174,7 @@ def main():
     with open(filename, 'w') as filedesc:
         filedesc.write(content)
     if data != content:
-        print "import successfully reordered for file: %s" % (filename)
+        sys.exit(2)
     sys.exit(0)
 
 if __name__ == "__main__":
