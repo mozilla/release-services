0a589ee2a9aa1db724e1c1f9507f060127bc7664
Justin Wood <Callek@gmail.com>
2014-09-25 00:27:35 -0400

    Add Basic Task History (still includes debug info)

--- relengapi/blueprints/slaveloan/tasks.py
+++ relengapi/blueprints/slaveloan/tasks.py
@@ -15,21 +15,72 @@ from relengapi.blueprints.slaveloan import slave_mappings
 from relengapi.blueprints.slaveloan.model import Machines, Humans, Loans, History
 from redo import retry
 import datetime
+from functools import wraps
+from relengapi.util import tz
+
+from celery import signals
+import celery
+
+
+def add_task_to_history(loanid, msg):
+    session = current_app.db.session('relengapi')
+    l = session.query(Loans).get(loanid)
+    history = History(for_loan=l,
+                      timestamp=tz.utcnow(),
+                      msg=msg)
+    session.add(history)
+    session.commit()
+    print "DEBUG:", repr(l.to_json())
+    print "DEBUG:", repr(history.to_json())
+    print "DEBUG: Log_line: %s" % msg
+
+
+def add_to_history(before=None, after=None):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            bound_task = None
+            loanid = None
+            if isinstance(args[0], celery.Task):
+                bound_task = args[0]
+                loanid = args[1]
+            else:
+                loanid = args[0]
+            if before:
+                print "DEBUG: Locals: %s" % repr(locals())
+                print "DEBUG: Log_line: %s" % before.format(**locals())
+                add_task_to_history(loanid, before.format(**locals()))
+            retval = f(*args, **kwargs)
+            if after:
+                print "DEBUG: Locals: %s" % repr(locals())
+                print "DEBUG: AFTER: Log_line: %s" % after.format(**locals())
+                add_task_to_history(loanid, after.format(**locals()))
+            return retval
+        return wrapper
+    return decorator
 
 
 @task()
+@add_to_history(
+    before="Initialising Loan for slaveclass {args[1]!s}",
+    after="Initialising Loan Process Complete for slaveclass {args[1]!s}, using flow {retval!s}")
 def init_loan(loanid, loan_class):
     print "Init Loan2", datetime.datetime.utcnow().isoformat(sep=" ")
     print "Loan Class = %s" % loan_class
     if slave_mappings.is_aws_serviceable(loan_class):
         print "aws host"
         # do_aws_loan.delay()
+        return "AWS"
     else:
         print "physical host: %s" % loan_class
         choose_inhouse_machine.delay(loanid, loan_class)
+        return "inhouse"
 
 
 @task(bind=True)
+@add_to_history(
+    before="Choosing an inhouse machine based on slavealloc",
+    after="Chose inhouse machine {retval!s}")
 def choose_inhouse_machine(self, loanid, loan_class):
     print "Choosing inhouse machine"
     url = furl("http://slavealloc.pvt.build.mozilla.org/api/slaves")
@@ -48,9 +99,13 @@ def choose_inhouse_machine(self, loanid, loan_class):
     print "Chosen Slave = %s" % chosen
     fixup_machine.delay(loanid, chosen['name'])
     start_disable_slave.delay(loanid, chosen['name'])
+    return chosen['name']
 
 
 @task(bind=True, max_retries=None)
+@add_to_history(
+    before="Identifying FQDN and IP of {args[2]}",
+    after="Aquired FQDN and IP")
 def fixup_machine(self, loanid, machine):
     try:
         print "FIXUP MACHINE"
@@ -76,6 +131,9 @@ def fixup_machine(self, loanid, machine):
 
 
 @task(bind=True, max_retries=None)
+@add_to_history(
+    before="Calling slaveapi's disable method",
+    after="Disable request sent")
 def start_disable_slave(self, loanid, machine):
     try:
         print "START DISABLE SLAVE"
