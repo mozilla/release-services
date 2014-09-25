04441759c7598d3bf7d9d41d1838ed5195fec293
Justin Wood <Callek@gmail.com>
2014-09-25 00:35:02 -0400

    changes for Merge djmitche/build-relengapi:simplify-validate (PR #114)

--- validate.sh
+++ validate.sh
@@ -4,8 +4,57 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 set -e
+
+# some colors
+
+_ESC=$'\e'
+GREEN="$_ESC[0;32m"
+MAGENTA="$_ESC[0;35m"
+RED="$_ESC[0;31m"
+LTCYAN="$_ESC[1;36m"
+YELLOW="$_ESC[1;33m"
+NORM="$_ESC[0;0m"
+
+fail() {
+    echo "${RED}${@}${NORM}"
+    exit 1
+}
+
+status() {
+    echo "${LTCYAN}-- ${*} --${NORM}"
+}
+
+ok=true
+problem_summary=""
+
+not_ok() {
+    ok=false
+    echo "${RED}** ${*} **${NORM}"
+    problem_summary="$problem_summary"$'\n'"${RED}**${NORM} ${*}"
+}
+
+warning() {
+    echo "${YELLOW}** ${*} **${NORM}"
+    problem_summary="$problem_summary"$'\n'"${YELLOW}**${NORM} ${*} (warning)"
+}
+
+show_results() {
+    echo ""
+    if $ok; then
+        if [ -z "${problem_summary}" ]; then
+            echo "${GREEN}GOOD!${NORM}"
+        else
+            echo "${YELLOW}WARNINGS${NORM}${problem_summary}"
+        fi
+    else
+        echo "${RED}NO GOOD!${NORM}${problem_summary}"
+        return 1
+    fi
+}
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-source ./validate-common.sh
+
+[ -z "$VIRTUAL_ENV" ] && fail "Need an activated virtualenv with relengapi installed"
 
 status "running pep8 -- slaveloan"
 pep8 --config=../base/pep8rc relengapi || not_ok "pep8 failed"
