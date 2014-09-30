6d030ece9c1c6d785624997e85ce4a9d69361797
Pete Moore <pmoore@mozilla.com>
2014-09-30 18:03:39 +0200

    Bumped version number from 0.2.2 to 0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='relengapi-mapper',
-      version='0.2.2',
+      version='0.2.3',
       description='hg to git mapper',
       author='Chris AtLee',
       author_email='chris@atlee.ca',
