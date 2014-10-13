f6b6e3968fabfcce11a1c7bec035ceab51a09359
Morgan Phillips <winter2718@gmail.com>
2014-10-13 19:49:50 +0000

    Add indexes to models to speed lookup times.

--- relengapi/blueprints/clobberer/models.py
+++ relengapi/blueprints/clobberer/models.py
@@ -14,10 +14,10 @@ class ClobbererBase(db.declarative_base(DB_DECLARATIVE_BASE)):
     __abstract__ = True
 
     id = sa.Column(sa.Integer, primary_key=True)
-    branch = sa.Column(sa.String(50))
-    master = sa.Column(sa.String(50))  # TODO: Remove this field
-    slave = sa.Column(sa.String(50))
-    builddir = sa.Column(sa.String(100))
+    branch = sa.Column(sa.String(50), index=True)
+    master = sa.Column(sa.String(50), index=True)  # TODO: Remove this field
+    slave = sa.Column(sa.String(30), index=True)
+    builddir = sa.Column(sa.String(100), index=True)
 
 
 class Build(ClobbererBase, db.UniqueMixin):
@@ -50,10 +50,15 @@ class ClobberTime(ClobbererBase):
     "A clobber request."
 
     __tablename__ = 'clobber_times'
+    __table_args__ = (
+        # Index to speed up lastclobber lookups
+        sa.Index('ix_get_clobber_times', 'slave', 'builddir', 'branch'),
+    )
 
     lastclobber = sa.Column(
         sa.Integer,
         nullable=False,
-        default=int(time.time())
+        default=int(time.time()),
+        index=True
     )
     who = sa.Column(sa.String(50))
