2e453e2a32b3d88fabaf35832d0d07df27b831e7
Morgan Phillips <winter2718@gmail.com>
2014-10-19 00:36:13 +0000

    Adds a new interface for retreiving clobber times.

--- relengapi/blueprints/clobberer/rest.py
+++ relengapi/blueprints/clobberer/rest.py
@@ -6,8 +6,15 @@ import wsme.types
 
 
 class ClobberRequest(wsme.types.Base):
-    "Represents a clobber request for a list of branch build directories."
+    "Represents a clobber request for a branch and build directory."
 
-    id = int
-    branch = unicode  #: The branch for this clobber request
-    builddirs = [unicode]  #: The build directories to be clobbered
+    branch = unicode  #: The branch for this clobber request.
+    builddir = unicode  #: The build directory to be clobbered.
+
+
+class ClobberTime(wsme.types.Base):
+    "Represents the most recent data pertaining to a particular clobber."
+    branch = unicode  #: The branch associated with this clobber.
+    builddir = unicode  #: The clobbered directory.
+    lastclobber = int  #: Timestamp associated with the last clobber request.
+    who = unicode  #: User who initiated the last clobber.
