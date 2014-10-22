c6d718b9a1b9c53633e0a143e01b8992b407b8fb
Justin Wood <Callek@gmail.com>
2014-10-21 21:56:02 -0400

    Fix pep8 line length, by using a different formatting style

--- relengapi/blueprints/slaveloan/rest.py
+++ relengapi/blueprints/slaveloan/rest.py
@@ -7,42 +7,67 @@ import wsme.types
 
 class Machine(wsme.types.Base):
     "Represents a single loanable Machine"
-    id = int  #: Unique ID for this entry
-    fqdn = unicode  #: The machines Fully Qualified Domain Name
-    ipaddr = unicode  #: The machines IP Address
+
+    #: Unique ID for this entry
+    id = int
+    #: The machines Fully Qualified Domain Name
+    fqdn = unicode
+    #: The machines IP Address
+    ipaddr = unicode
 
 
 class Human(wsme.types.Base):
     "Represents a single Human requesting a loan"
-    id = int  #: Unique ID for this entry
-    ldap = unicode  #: The humans full LDAP name
-    bugzilla = unicode  #: The humans full bugzilla e-mail address
+
+    #: Unique ID for this entry
+    id = int
+    #: The humans full LDAP name
+    ldap = unicode
+    #: The humans full bugzilla e-mail address
+    bugzilla = unicode
 
 
 class Loan(wsme.types.Base):
     "Represents a singe Loan Entry"
-    id = int  #: Unique ID for this entry
-    status = unicode  #: Current status of the loan. Valid values ("PENDING", "READY")
-    bug_id = int  #: Bugzilla Bug number of the Loan Request
-    human = Human  #: The :api:type:`Human` using this loan
-    machine = Machine  #: The :api:type:`Machine` assigned to this loan
+
+    #: Unique ID for this entry
+    id = int
+    #: Current status of the loan. Valid values ("PENDING", "READY")
+    status = unicode
+    #: Bugzilla Bug number of the Loan Request
+    bug_id = int
+    #: The :api:type:`Human` using this loan
+    human = Human
+    #: The :api:type:`Machine` assigned to this loan
+    machine = Machine
 
 
 class HistoryEntry(wsme.types.Base):
     "Represents a single log line of History Loan Entry"
-    id = int  #: Unique ID for this line
-    loan_id = int  #: Unique ID of the loan this log line is for
-    timestamp = unicode  #: Datetime string (in UTC) of this entry. (e.g. "2014-06-06T20:02:46.937065+00:00" )
-    msg = unicode  #: The actual log message
+
+    #: Unique ID for this line
+    id = int
+    #: Unique ID of the loan this log line is for
+    loan_id = int
+    #: Datetime string (in UTC) of this entry. (e.g. "2014-06-06T20:02:46.937065+00:00")
+    timestamp = unicode
+    #: The actual log message
+    msg = unicode
 
 
 class LoanRequest(wsme.types.Base):
     "Represents a new loan request"
-    status = unicode  #: Initial Status
-    LDAP = unicode  #: Users LDAP username
-    bugzilla = unicode  #: Users Bugzilla e-mail
-    fqdn = unicode  #: If known in advance, fqdn of the machine to loan
-    ipaddress = unicode  #: If known in advance, ip address of the machine to loan
+
+    #: Initial Status
+    status = unicode
+    #: Users LDAP username
+    LDAP = unicode
+    #: Users Bugzilla e-mail
+    bugzilla = unicode
+    #: If known in advance, fqdn of the machine to loan
+    fqdn = unicode
+    #: If known in advance, ip address of the machine to loan
+    ipaddress = unicode
 
 
 MachineClassMapping = wsme.types.DictType(key_type=unicode,
