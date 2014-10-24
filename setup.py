b0d7fc049f420afaac9aad652b52aabc109f9da3
Morgan Phillips <winter2718@gmail.com>
2014-10-24 14:05:09 -0500

    Fixes setup.py so that installing clobberer doesn't blow away relengapi's docs.

--- setup.py
+++ setup.py
@@ -28,6 +28,10 @@ setup(name='relengapi-clobberer',
     data_files=[
         ('relengapi-' + dirpath, [os.path.join(dirpath, f) for f in files])
         for dirpath, _, files in os.walk('docs')
+        # don't include directories not containing any files, as they will be
+        # included in installed-filestxt, and deleted (rm -rf) on uninstall;
+        # see https://bugzilla.mozilla.org/show_bug.cgi?id=1088676
+        if files
     ],
     package_data={  # NOTE: these files must *also* be specified in MANIFEST.in
         'relengapi.blueprints.clobberer': data_patterns,
