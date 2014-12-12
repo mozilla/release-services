bd6f3cf37bd7ff0b9792e345eeac18b316223ed2
Dustin J. Mitchell <dustin@mozilla.com>
2014-12-12 16:39:58 -0500

    Fix up styling -- sort of -- for badpenny

--- relengapi/blueprints/badpenny/static/badpenny.js
+++ relengapi/blueprints/badpenny/static/badpenny.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-angular.module('badpenny', ['relengapi', 'initial_data']);
+angular.module('badpenny', ['relengapi', 'initial_data', 'angularMoment']);
 
 angular.module('badpenny').controller('TasksController',
                                     function($scope, restapi, initial_data) {
@@ -15,44 +15,50 @@ angular.module('badpenny').controller('TasksController',
 
     $scope.tasks = tasksByName(initial_data.tasks);
     $scope.expandedTask = null;
-    $scope.expandedLogs = null;
 
-    $scope.expandTask = function(name) {
-        $scope.expandedTask = name;
-
-        restapi.get('/badpenny/tasks/' + name, {while: 'fetching task'})
+    $scope.expandTask = function(task) {
+        restapi.get('/badpenny/tasks/' + task.name, {while: 'fetching task'})
         .then(function (data, status, headers, config) {
-            $scope.tasks[name] = data.data.result;
+            $scope.tasks[task.name] = data.data.result;
         });
     };
 
     $scope.expandJob = function(job) {
-        $scope.expandedLogs = 'loading';
+        job.logs = 'loading';
         restapi.get('/badpenny/jobs/' + job.id + '/logs',
                     {while: 'fetching logs', expected_status: 404})
         .then(function (data, status, headers, config) {
-            $scope.expandedLogs = data.data.result.content;
+            job.logs = data.data.result.content;
         }, function (data, status, header, config) {
             if (data.status == 404) {
-                $scope.expandedLogs = '(no logs)';
+                job.logs = '(no logs)';
             }
         });
     };
 
-    $scope.closeJob = function() {
-        $scope.expandedLogs = null;
+    $scope.humanJobDuration = function(job) {
+        var dur = moment.duration(moment(job.started_at).diff(job.completed_at));
+        if (dur < 100) {
+            return "instantly";
+        } else if (dur < 10000) {
+            // humanize just says "a few seconds", which isn't good enough
+            return "in " + (dur / 1000).toFixed(2) + ' seconds';
+        } else {
+            return "in " + moment.duration(diff).humanize();
+        }
     };
 
     $scope.refresh = function() {
         restapi.get('/badpenny/tasks', {while: 'refreshing tasks'})
         .then(function (data, status, headers, config) {
-            // copy over the job from the expanded task
-            var newTasks = tasksByName(data.data.result);
-            var expandedTask = $scope.tasks[$scope.expandedTask];
-            if (expandedTask) {
-                newTasks[expandedTask.name].jobs = expandedTask.jobs;
-            }
-            $scope.tasks = newTasks;
+            // re-request any existing job data, and start refreshing it
+            angular.forEach(data.data.result, function(task) {
+                if ($scope.tasks[task.name].jobs) {
+                    task.jobs = $scope.tasks[task.name].jobs;
+                    $scope.expandTask(task);
+                }
+            });
+            $scope.tasks = tasksByName(data.data.result);
         });
 
         // reload the currently expanded task simultaneously
