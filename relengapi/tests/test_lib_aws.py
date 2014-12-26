408338ff0edcd7df1a5c82488577084b3c57aea0
Dustin J. Mitchell <dustin@mozilla.com>
2014-12-26 11:46:02 -0500

    test relengapi.lib.aws

---
+++ relengapi/tests/test_lib_aws.py
@@ -0,0 +1,93 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import boto
+import json
+import mock
+
+from moto import mock_sqs
+from nose.tools import assert_raises
+from nose.tools import eq_
+from relengapi.lib.testing.context import TestContext
+
+test_context = TestContext(reuse_app=False)
+
+aws_cfg = {
+    'AWS': {
+        'access_key_id': 'aa',
+        'secret_access_key': 'ss',
+        'sqs': {
+            'myq': ('us-east-1', 'my-sqs-queue'),
+        },
+    },
+}
+
+
+@mock_sqs
+@test_context.specialize(config=aws_cfg)
+def test_connect_to(app):
+    with mock.patch('boto.connect_sqs', return_value='sqs_conn') as connect_sqs:
+        eq_(app.aws.connect_to('sqs', 'us-east-1'), 'sqs_conn')
+        connect_sqs.assert_called_with(
+            aws_access_key_id='aa',
+            aws_secret_access_key='ss',
+            region=mock.ANY)
+    # connection is cached
+    eq_(app.aws.connect_to('sqs', 'us-east-1'), 'sqs_conn')
+
+
+@mock_sqs
+@test_context
+def test_connect_to_no_creds(app):
+    with mock.patch('boto.connect_sqs', return_value='sqs_conn') as connect_sqs:
+        eq_(app.aws.connect_to('sqs', 'us-east-1'), 'sqs_conn')
+        connect_sqs.assert_called_with(
+            # the None here will cause boto to look in ~/.boto, etc.
+            aws_access_key_id=None,
+            aws_secret_access_key=None,
+            region=mock.ANY)
+
+
+@mock_sqs
+@test_context
+def test_connect_to_invalid_region(app):
+    assert_raises(RuntimeError, lambda:
+                  app.aws.connect_to('sqs', 'us-canada-17'))
+
+
+@mock_sqs
+@test_context
+def test_get_sqs_queue_no_config(app):
+    assert_raises(RuntimeError, lambda:
+                  app.aws.get_sqs_queue('foo'))
+
+
+@mock_sqs
+@test_context.specialize(config=aws_cfg)
+def test_get_sqs_queue_no_queue(app):
+    assert_raises(RuntimeError, lambda:
+                  app.aws.get_sqs_queue('myq'))
+
+
+@mock_sqs
+@test_context.specialize(config=aws_cfg)
+def test_get_sqs_queue(app):
+    conn = boto.connect_sqs()
+    conn.create_queue('my-sqs-queue')
+    queue = app.aws.get_sqs_queue('myq')
+    # check it's a queue
+    assert hasattr(queue, 'get_messages')
+    # check caching
+    assert app.aws.get_sqs_queue('myq') is queue
+
+
+@mock_sqs
+@test_context.specialize(config=aws_cfg)
+def test_sqs_write(app):
+    conn = boto.connect_sqs()
+    queue = conn.create_queue('my-sqs-queue')
+    app.aws.sqs_write('myq', {'a': 'b'})
+    msgs = queue.get_messages()
+    assert len(msgs) == 1
+    eq_(json.loads(msgs[0].get_body()), {'a': 'b'})
