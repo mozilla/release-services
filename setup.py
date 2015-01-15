198e7e818d12889b6086714523b9275ecec2e3bc
Dustin J. Mitchell <dustin@mozilla.com>
2015-01-15 11:38:29 -0500

    bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ data_patterns = [
 
 setup(
     name='relengapi',
-    version='1.0.1',
+    version='1.1.0',
     description='The code behind https://api.pub.build.mozilla.org',
     author='Dustin J. Mitchell',
     author_email='dustin@mozilla.com',
