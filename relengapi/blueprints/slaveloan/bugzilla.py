19789e9e3a4e4fa490cebf347e148f439ed3f514
Justin Wood <Callek@gmail.com>
2015-03-12 19:56:05 -0400

    Use getattr properly

--- relengapi/blueprints/slaveloan/bugzilla.py
+++ relengapi/blueprints/slaveloan/bugzilla.py
@@ -18,7 +18,7 @@ DEFAULT_BUGZILLA_URL = "https://bugzilla-dev.allizom.org/rest/"
 
 
 def _bzclient():
-    if current_app.getattr('bzclient', None):
+    if getattr(current_app, 'bzclient', None):
         return current_app.bzclient
     # XXX: Todo log warning if user or pass is "None"
     bzclient = BugzillaClient()
