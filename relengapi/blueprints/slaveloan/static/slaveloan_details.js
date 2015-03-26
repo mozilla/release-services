4d9b4540f2a91cf92da9f5f3ed41a3aaffeda0fe
Justin Wood <Callek@gmail.com>
2015-03-26 01:38:44 -0400

    basic UI for actions-to-do

--- relengapi/blueprints/slaveloan/static/slaveloan_details.js
+++ relengapi/blueprints/slaveloan/static/slaveloan_details.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+
 function reload_loans() {
     $('#loans-history tbody').empty();
     $.ajax({
@@ -14,3 +16,56 @@ function reload_loans() {
     });
 }
 $(reload_loans);
+
+function complete_action(evt){
+    evt.target.disabled = true;
+    var good = function(action) {
+        alertify.success("Marked action as complete");
+        reload_actions();
+    };
+    var bad = function(errmsg) {
+        /* re-enable the button so users can try again */
+        evt.target.disabled=false;
+        alertify.error(errmsg);
+    };
+
+    var action_id = evt.target.value;
+    var result = $.ajax({
+        url: _get_manual_actions_base + '/' + action_id,
+        type: 'PUT',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({complete: true})
+    }).done(function(data) {
+        good(data.result);
+    }).fail(function(jqhxr, err) {
+        bad("error from server: " + jqhxr.responseText );
+    });
+}
+
+function reload_actions() {
+    $('#actions_wrapper').hide();
+    $('#loans-actions tbody').empty();
+    $.ajax({
+        url: _get_manual_actions_url,
+        dataType: 'json',
+        success: function(data) {
+            if (data.result.length > 0) {
+                $('#actions_wrapper').show();
+            }
+            $.each(data.result,function(i,row){
+                $('<tr>').append(
+                    $('<td>').text(row.timestamp_start),
+                    $('<td>').text(row.msg),
+                    $('<td>').append(
+                        $('<button type="button" value="' + row.id + '">COMPLETE</button>.').click(
+                            complete_action
+                        )
+                    )
+                ).appendTo('#loans-actions tbody');
+            });
+        }
+    });
+}
+$(reload_actions);
+
+});
