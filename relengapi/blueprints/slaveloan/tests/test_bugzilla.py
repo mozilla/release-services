3f087605f3e3db3dced11190de77372738f91a35
Justin Wood <Callek@gmail.com>
2015-03-30 01:19:29 -0400

    Moar bugzilla testing

--- relengapi/blueprints/slaveloan/tests/test_bugzilla.py
+++ relengapi/blueprints/slaveloan/tests/test_bugzilla.py
@@ -4,12 +4,15 @@
 
 import mock
 
+from nose.tools import assert_raises
 from nose.tools import assert_not_equal
 from nose.tools import eq_
+from nose.tools import ok_
 from relengapi.blueprints.slaveloan import bugzilla
 from relengapi.lib.testing.context import TestContext
 
 test_context = TestContext()
+test_context_servername = TestContext(config={'SERVER_NAME': "allizom.org"})
 
 
 @test_context
@@ -115,3 +118,92 @@ def test_problem_tracking_bug_alias_length(app):
                 mockcreatebug.side_effect = create_bug_alias_long
                 bug = bugzilla.ProblemTrackingBug(slavename, loadInfo=False)
                 bug.create()
+
+
+@test_context
+def test_problem_tracking_bug_create_comment(app):
+    "Test problem tracking bug creation includes comment"
+    # The slave name doesn't actually matter for this test
+    slavename = "foobar"
+
+    def create_bug_comment(data):
+        data['id'] = 12345
+        eq_(data['comment'], "some comment should be here")
+        return data
+
+    with app.app_context():
+        with mock.patch("bzrest.client.BugzillaClient.create_bug") as mockcreatebug:
+            mockcreatebug.side_effect = create_bug_comment
+            bug = bugzilla.ProblemTrackingBug(slavename, loadInfo=False)
+            bug.create(comment="some comment should be here")
+
+
+@test_context
+def test_problem_tracking_bug_create_deps(app):
+    "Test problem tracking bug creation includes depend"
+    # The slave name doesn't actually matter for this test
+    slavename = "foobar"
+
+    def create_bug_comment(data):
+        data['id'] = 12345
+        eq_(data['depends_on'], 54321)
+        return data
+
+    with app.app_context():
+        with mock.patch("bzrest.client.BugzillaClient.create_bug") as mockcreatebug:
+            mockcreatebug.side_effect = create_bug_comment
+            bug = bugzilla.ProblemTrackingBug(slavename, loadInfo=False)
+            bug.create(depends_on=54321)
+
+
+@test_context
+def test_create_loan_bug_valueerror(app):
+    "Test that varying missing input combinations to create_loan_bug raises an exception"
+    clb = bugzilla.create_loan_bug
+    with app.app_context():
+        assert_raises(ValueError, clb)
+        assert_raises(ValueError, clb, loan_id=12345)
+        assert_raises(ValueError, clb, slavetype="foobar")
+        assert_raises(ValueError, clb, bugzilla_username="me@example.com")
+        assert_raises(ValueError, clb, loan_id=12345, slavetype="foobar")
+        assert_raises(ValueError, clb, loan_id=12345, bugzilla_username="me@example.com")
+        assert_raises(ValueError, clb, slavetype="foobar", bugzilla_username="me@example.com")
+
+
+@test_context
+def test_create_loan_bug(app):
+    "Test problem loan bug creation requires SERVER_NAME set"
+    # The slave name doesn't actually matter for this test
+    slavetype = "foobar"
+    who = "me@example.com"
+    loan_id = 10
+
+    with app.app_context():
+        assert_raises(
+            RuntimeError, bugzilla.create_loan_bug,
+            loan_id=10, slavetype=slavetype, bugzilla_username=who)
+
+
+@test_context_servername
+def test_create_loan_bug(app):
+    "Test problem loan bug creation includes stuff"
+    # The slave name doesn't actually matter for this test
+    slavetype = "foobar"
+    who = "me@example.com"
+    loan_id = 123456789
+
+    def create_bug_comment(data):
+        data['id'] = 12345
+        ok_("summary" in data, msg="creation includes bug summary")
+        ok_(slavetype in data['summary'], msg="bug summary includes slavetype")
+        ok_(who in data['summary'], msg="bug summary includes who")
+        ok_("comment" in data, msg="creation includes initial comment")
+        ok_(slavetype in data['comment'], msg="comment includes slavetype")
+        ok_(who in data['comment'], msg="comment includes who")
+        ok_(loan_id in data['comment'], msg="the loan id must appear in the comment")
+        return data
+
+    with app.app_context():
+        with mock.patch("bzrest.client.BugzillaClient.create_bug") as mockcreatebug:
+            mockcreatebug.side_effect = create_bug_comment
+            bugzilla.create_loan_bug(loan_id=10, slavetype=slavetype, bugzilla_username=who)
