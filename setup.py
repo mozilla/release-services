1624e976641fcd36ca27e6e8d4fa4bf2a7f1f094
Dustin J. Mitchell <dustin@mozilla.com>
2015-04-01 16:35:54 -0400

    Bump to version relengapi-2.0.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ data_patterns = [
 
 setup(
     name='relengapi',
-    version='2.0.0',
+    version='2.0.1',
     description='The code behind https://api.pub.build.mozilla.org',
     author='Dustin J. Mitchell',
     author_email='dustin@mozilla.com',
