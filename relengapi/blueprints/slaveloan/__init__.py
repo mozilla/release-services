aee87e627dcb735d4779ebb0881fe28dd287128b
Justin Wood <Callek@gmail.com>
2015-04-15 02:17:03 -0400

    Don't ignore bugzilla e-mail entry

--- relengapi/blueprints/slaveloan/__init__.py
+++ relengapi/blueprints/slaveloan/__init__.py
@@ -115,6 +115,8 @@ def new_loan_from_admin(body):
                                    ipaddress=body.ipaddress)
         h = Humans.as_unique(session, ldap=body.LDAP,
                              bugzilla=body.bugzilla)
+        if h.bugzilla != body.bugzilla:
+            h.bugzilla = body.bugzilla
     except sa.exc.IntegrityError:
         raise InternalServerError("Integrity Error from Database, please retry.")
 
@@ -157,6 +159,8 @@ def new_loan_request(body):
     try:
         h = Humans.as_unique(session, ldap=body.ldap_email,
                              bugzilla=body.bugzilla_email)
+        if h.bugzilla != body.bugzilla_email:
+            h.bugzilla = body.bugzilla_email
     except sa.exc.IntegrityError:
         raise InternalServerError("Integrity Error from Database, please retry.")
 
