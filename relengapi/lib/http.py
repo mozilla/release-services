5c68c14a4566d68da64450ecf1e1791acb05ae50
Dustin J. Mitchell <dustin@mozilla.com>
2015-07-27 10:03:31 -0400

    Add status code filtering, defaulting to all

--- relengapi/lib/http.py
+++ relengapi/lib/http.py
@@ -5,18 +5,56 @@
 import wrapt
 
 from flask import make_response
+from werkzeug.exceptions import HTTPException
 
 
-def response_headers(*headers):
+_status_ranges = {
+    '2xx': lambda c: 200 <= c < 300,
+    '3xx': lambda c: 300 <= c < 400,
+    '4xx': lambda c: 400 <= c < 500,
+    '5xx': lambda c: 500 <= c < 600,
+}
+
+
+def response_headers(*headers, **kwargs):
+    assert set(kwargs.keys()) <= set(['status_codes'])
+    status_codes = kwargs.pop('status_codes', None)
+    if not status_codes:
+        def status_match(c):
+            return True
+    elif callable(status_codes):
+        status_match = status_codes
+    elif isinstance(status_codes, (unicode, str)):
+        if status_codes in _status_ranges:
+            status_match = _status_ranges[status_codes]
+        else:
+            raise ValueError("invalid status_codes " + status_codes)
+    elif isinstance(status_codes, int):
+        code = status_codes
+
+        def status_match(c):
+            return c == code
+    else:
+        codeset = set(status_codes)
+
+        def status_match(c):
+            return c in codeset
+
     @wrapt.decorator
     def wrapper(wrapped, instance, args, kwargs):
         # skip apimethod __data_only__ calls
         if '_data_only_' in kwargs:
             return wrapped(*args, **kwargs)
-        # mark the response to avoid caching; insert this decorator just after
-        # the route decorators
-        resp = make_response(wrapped(*args, **kwargs))
-        if 200 <= resp.status_code < 400:
+        try:
+            resp = make_response(wrapped(*args, **kwargs))
+        except HTTPException as e:
+            if status_match(e.code):
+                resp = e.get_response()
+                for h, v in headers:
+                    resp.headers[h] = v
+                return resp
+            raise
+        if status_match(resp.status_code):
             for h, v in headers:
                 resp.headers[h] = v
         return resp
--- relengapi/tests/test_lib_http.py
+++ relengapi/tests/test_lib_http.py
@@ -4,17 +4,19 @@
 
 import werkzeug
 
+from nose.tools import assert_raises
 from nose.tools import eq_
 
 from relengapi.lib import api
 from relengapi.lib import http
 from relengapi.lib.testing.context import TestContext
+from werkzeug.exceptions import BadRequest
 
 test_context = TestContext()
 
 
 @test_context
-def test_response(app, client):
+def test_response_headers_response(app, client):
     @app.route('/test')
     @http.response_headers(('foo', 'bar'))
     def response_view_func():
@@ -23,7 +25,7 @@ def test_response(app, client):
 
 
 @test_context
-def test_tuple(app, client):
+def test_response_headers_tuple(app, client):
     @app.route('/test')
     @http.response_headers(('foo', 'bar'))
     def response_view_func():
@@ -32,7 +34,7 @@ def test_tuple(app, client):
 
 
 @test_context
-def test_string(app, client):
+def test_response_headers_string(app, client):
     @app.route('/test')
     @http.response_headers(('foo', 'bar'))
     def response_view_func():
@@ -41,16 +43,78 @@ def test_string(app, client):
 
 
 @test_context
-def test_error(app, client):
+def test_response_headers_error_response(app, client):
     @app.route('/test')
     @http.response_headers(('foo', 'bar'))
     def response_view_func():
         return "Hello, world", 400
-    assert 'foo' not in client.get('/test').headers
+    assert 'foo' in client.get('/test').headers
 
 
 @test_context
-def test_api_get_data(app, client):
+def test_response_headers_error_raised(app, client):
+    @app.route('/test')
+    @http.response_headers(('foo', 'bar'))
+    def response_view_func():
+        raise BadRequest("oh noes")
+    assert 'foo' in client.get('/test').headers
+
+
+def _add_numeric_route(app, status_codes):
+    @app.route('/test/<int:c>')
+    @http.response_headers(('foo', 'bar'), status_codes=status_codes)
+    def view(c):
+        return 'RESPONSE', c
+
+
+@test_context
+def test_response_headers_status_codes_func(app, client):
+    """response_headers(.., status_codes=lambda..) attaches headers only to
+    responses for which the lambda returns True"""
+    _add_numeric_route(app, status_codes=lambda c: c == 201)
+    assert 'foo' not in client.get('/test/200').headers
+    assert 'foo' in client.get('/test/201').headers
+
+
+@test_context
+def test_response_headers_status_codes_range(app, client):
+    """response_headers(.., status_codes='3xx') attaches headers only to
+    responses in the 300's"""
+    _add_numeric_route(app, status_codes='3xx')
+    assert 'foo' not in client.get('/test/400').headers
+    assert 'foo' in client.get('/test/303').headers
+
+
+@test_context
+def test_response_headers_status_codes_str():
+    """response_headers(.., status_codes='300') raises ValueError"""
+    assert_raises(ValueError, lambda:
+                  http.response_headers(('foo', 'bar'), status_codes='300'))
+
+
+@test_context
+def test_response_headers_status_codes_int(app, client):
+    """response_headers(.., status_codes=302) attaches headers only to
+    responses with code 302"""
+    _add_numeric_route(app, status_codes=303)
+    assert 'foo' not in client.get('/test/302').headers
+    assert 'foo' in client.get('/test/303').headers
+
+
+@test_context
+def test_response_headers_status_codes_iterable(app, client):
+    """response_headers(.., status_codes=[..]) attaches headers only to
+    responses listed in the iterable"""
+    _add_numeric_route(app, status_codes=[401, 404])
+    assert 'foo' not in client.get('/test/400').headers
+    assert 'foo' in client.get('/test/401').headers
+    assert 'foo' not in client.get('/test/4u3').headers
+
+
+@test_context
+def test_response_headers_api_get_data(app, client):
+    """View functions decorated with response_headers can still be
+    used in api.get_data calls"""
     @app.route('/test/api')
     @http.response_headers(('foo', 'bar'))
     @api.apimethod([unicode])
