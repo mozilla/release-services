b05b963f4deafccb9fae9586a728854497d9b9e6
Justin Wood <Callek@gmail.com>
2015-09-14 15:34:01 -0400

    Bump to version relengapi-3.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='relengapi',
-    version='3.2.1',
+    version='3.3.0',
     description='The code behind https://api.pub.build.mozilla.org',
     author='Dustin J. Mitchell',
     author_email='dustin@mozilla.com',
