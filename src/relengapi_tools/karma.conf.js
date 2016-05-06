4d83a52b18671dc9d5e101ee1d200446098b7a3a
Rok Garbas <rok@garbas.si>
2016-05-06 16:36:38 +0200

    relengapi_tools: using neo for creating development env

--- src/relengapi_tools/karma.conf.js
+++ src/relengapi_tools/karma.conf.js
@@ -1,60 +1,10 @@
-var glob = require("glob")
-var merge = require('webpack-merge');
-var webpackConfig = require('./webpack.config');
+'use strict';
 
-var files = glob.sync("tests/*-test.js")
-var preprocessors = files.reduce(function(i, j) { i[j] = [ 'webpack', 'sourcemap' ]; return i; }, {});
+const karmaConfig = require('neo/karma');
+const webpackConfig = require('./webpack.config');
 
-module.exports = function (config) {
-  config.set({
-    browsers: [ 'Chrome' ],
-    singleRun: true,
-    frameworks: [ 'mocha' ],
-    plugins: [
-      'karma-chai',
-      'karma-chrome-launcher',
-      'karma-coverage',
-      'karma-mocha',
-      'karma-mocha-reporter',
-      'karma-sourcemap-loader',
-      'karma-webpack'
-    ],
-    files: files,
-    preprocessors: preprocessors,
-    reporters: [ 'mocha', 'coverage' ],
-    webpack: merge(webpackConfig, {
-      // https://github.com/airbnb/enzyme/issues/47#issuecomment-207498885
-      externals: {
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': true,
-        'react/addons': true
-      },
-      devtool: 'inline-source-map',
-      module: {
-        // https://github.com/airbnb/enzyme/issues/47#issuecomment-171953666
-        loaders: [
-          { test: /\.json$/,
-            loader: "json" 
-          }
-        ],
-        postLoaders: [
-          { test: /\.js$/,
-            exclude: /(tests|node_modules)\//,
-            loader: 'istanbul-instrumenter'
-          }
-        ]
-      }
-    }),
-    webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
-    },
-    coverageReporter: {
-      dir: 'coverage/',
-      reporters: [
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' },
-        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' }
-      ]
-    }
-  });
+module.exports = config => {
+  config.set(karmaConfig(webpackConfig, {
+    // ... custom karma configuration
+  }));
 };
--- src/relengapi_tools/src/index.js
+++ src/relengapi_tools/src/index.js
@@ -1,26 +1,43 @@
 require('expose?$!expose?jQuery!jquery')
 require('expose?Tether!tether')
 require('bootstrap')
-require("!style!css!sass!./index.scss")
+require("./index.scss")
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { Router, Route } from 'react-router'
+import {Link} from 'react-router'
+import {renderApp, createApp} from 'neo'
 
-import { store, history, Layout, routes } from './app'
+import Home from './home'
 
+const Routes = [
+  { path: '/', title: 'Home', component: Home }
+];
 
-ReactDOM.render((
-    <Provider store={store}>
-        <Router history={history}>
-            <Route component={Layout}>
-            {
-              routes.map(function(route) {
-                return <Route key={route.path} path={route.path} component={route.component} />;
-              })
-            }
-            </Route>
-        </Router>
-    </Provider>
-), document.getElementById('app'))
+const Layout = ({ children }) => (
+  <div className="container">
+    <nav id="navbar" className="navbar navbar-light bg-faded">
+      <button className="navbar-toggler hidden-sm-up" type="button"
+              data-toggle="collapse" data-target="#navbar-header"
+              aria-controls="navbar-header">&#9776;</button>
+      <div className="collapse navbar-toggleable-xs" id="navbar-header">
+        <ul className="nav navbar-nav">
+        {
+          Routes.map((route) => (
+            <li key={route.path} className="nav-item active">
+              <Link className="nav-link" to={route.path}>{route.title}</Link>
+            </li>
+          ))
+        }
+        </ul>
+      </div>
+    </nav>
+    <main id="content">{children}</main>
+  </div>
+)
+
+const App = createApp(
+  {}, // <- reducers
+  {}  // <- initial state
+)
+
+renderApp(App, Layout, Routes)
