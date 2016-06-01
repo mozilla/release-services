5633c32023e26d8d85fac6e996ea6d7831c6ec5b
Rok Garbas <rok@garbas.si>
2016-06-01 11:20:10 +0200

    common: fix, missed checking in cache extension

---
+++ src/relengapi_common/relengapi_common/cache.py
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+from flask_cache import Cache
+
+
+def init_app(app):
+    cache_config = app.config.get('CACHE', {'CACHE_TYPE': 'simple'})
+    cache = Cache(app, config=cache_config)
+    return cache
