31a34a502dda6ac2c281125fb11bfb074b3d0e2c
Rok Garbas <rok@garbas.si>
2016-06-01 13:26:15 +0200

    setup: use redis for cache

--- settings.py
+++ settings.py
@@ -12,6 +12,10 @@ CACHE = {x: os.environ.get(x) for x in os.environ.keys() if x.startswith('CACHE_
 if 'CACHE_DEFAULT_TIMEOUT' not in CACHE:
     CACHE['CACHE_DEFAULT_TIMEOUT'] = 60 * 5
 
+if 'REDIS_URL' in os.environ:
+    CACHE['CACHE_TYPE'] = 'redis'
+    CACHE['CACHE_REDIS_URL'] = os.environ['REDIS_URL']
+
 if not DATABASE_URL:
     raise Exception("You need to specify DATABASE_URL variable.")
 
