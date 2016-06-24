79c12b5eed5b38f8ac36fa7e124227617d7f4762
Rok Garbas <rok@garbas.si>
2016-06-24 14:56:12 +0200

    setup: nixification

---
+++ default.nix
@@ -0,0 +1,30 @@
+{}:
+
+let
+  pkgs = import <nixpkgs> {};
+  version = "1.0.0";
+in pkgs.stdenv.mkDerivation {
+  name = "relengapi-${version}";
+  buildInputs = with pkgs; [
+    pythonPackages.virtualenv
+    libxslt
+    libxml2
+    zlib
+    libffi
+    openssl
+    postgresql
+    nodejs-6_x
+    phantomjs2
+    sassc
+    mysql.lib
+    zlib
+    openssl
+  ];
+  shellHook = ''
+    export CACHE_DEFAULT_TIMEOUT=3600
+    export CACHE_TYPE=filesystem
+    export CACHE_DIR=$TMPDIR/clobberer
+    export DATABASE_URL=sqlite:///$PWD/app.db
+    export PATH=$PWD/src/relengapi_tools//node_modules/.bin:$PATH
+  '';
+}
