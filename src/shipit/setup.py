7c805128ca05ebfb572c35b5e03a0da5b0f2fe06
Rok Garbas <rok@garbas.si>
2016-08-01 17:47:58 +0200

    inital shipit code

---
+++ src/shipit/setup.py
@@ -0,0 +1,37 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+here = os.path.dirname(__file__)
+
+setup(
+    name='shipit',
+    version=open(os.path.join(here, 'VERSION')).read().strip(),
+    description='The code behind https://ship-it.mozilla-releng.net',
+    author='Mozilla RelEng',
+    author_email='release@mozilla.com',
+    url='https://ship-it.mozilla-releng.net',
+    install_requires=[
+        "relengapi_common",
+        "taskcluster",
+    ],
+    extras_require={
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            'relengapi = relengapi.cmd:main',
+        ],
+    },
+    license='MPL2',
+)
+
---
+++ src/shipit/shipit/__init__.py
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+
+from relengapi_common import create_app, db
+from relengapi_clobberer import _flask
+
+app = create_app(__name__, [db, _flask])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
