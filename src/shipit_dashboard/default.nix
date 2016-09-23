cfa14912f2f079b64808b6fefe18c542ed553135
Bastien Abadie <bastien@nextcairn.com>
2016-09-23 12:31:19 +0200

    Shipit: fix default.nix hook

--- src/shipit_dashboard/default.nix
+++ src/shipit_dashboard/default.nix
@@ -3,52 +3,53 @@
 
 let
 
-  inherit (releng_pkgs.lib) mkBackend;
+  inherit (releng_pkgs.lib) mkBackend mkTaskclusterHook;
   inherit (releng_pkgs.pkgs) writeScript;
   inherit (releng_pkgs.pkgs.lib) removeSuffix;
   inherit (releng_pkgs.tools) pypi2nix;
 
-in mkBackend rec {
-  name = "shipit_dashboard";
-  version = removeSuffix "\n" (builtins.readFile ./../../VERSION);
-  python = import ./requirements.nix { inherit (releng_pkgs) pkgs; };
-  src = ./.;
-  srcs = [
-    "./../releng_common"
-    "./../${name}"
-  ];
-  buildRequirements =
-    [ ./requirements-dev.txt
-      ./requirements-setup.txt
+  self = mkBackend rec {
+    name = "shipit_dashboard";
+    version = removeSuffix "\n" (builtins.readFile ./../../VERSION);
+    python = import ./requirements.nix { inherit (releng_pkgs) pkgs; };
+    src = ./.;
+    srcs = [
+      "./../releng_common"
+      "./../${name}"
     ];
-  propagatedRequirements =
-    [ ./../releng_common/requirements.txt
-      ./requirements.txt
-      ./requirements-prod.txt
-    ];
-  passthru = {
-    taskclusterHooks = {
-      taskcluster_analysis = mkTaskclusterHook {
-        name = "update bugzilla analysis";
-        owner = "bastien@nextcairn.com";
-        schedule = [ "0 */2 * * * *" ];
-        taskImage = self.docker;
-        taskCommand = [ "flask" "run_workflow_local" ];
+    buildRequirements =
+      [ ./requirements-dev.txt
+        ./requirements-setup.txt
+      ];
+    propagatedRequirements =
+      [ ./../releng_common/requirements.txt
+        ./requirements.txt
+        ./requirements-prod.txt
+      ];
+    passthru = {
+      taskclusterHooks = {
+        taskcluster_analysis = mkTaskclusterHook {
+          name = "update bugzilla analysis";
+          owner = "bastien@nextcairn.com";
+          schedule = [ "0 */2 * * * *" ];
+          taskImage = self.docker;
+          taskCommand = [ "flask" "run_workflow_local" ];
+        };
       };
+      update = writeScript "update-${name}" ''
+        pushd src/${name}
+        ${pypi2nix}/bin/pypi2nix -v \
+         -V 3.5 \
+         -E "postgresql libffi openssl" \
+         -r requirements.txt \
+         -r requirements-setup.txt \
+         -r requirements-dev.txt \
+         -r requirements-prod.txt 
+        popd
+      '';
     };
-    update = writeScript "update-${name}" ''
-      pushd src/${name}
-      ${pypi2nix}/bin/pypi2nix -v \
-       -V 3.5 \
-       -E "postgresql libffi openssl" \
-       -r requirements.txt \
-       -r requirements-setup.txt \
-       -r requirements-dev.txt \
-       -r requirements-prod.txt 
-      popd
-    '';
   };
-}
+in self
 
 # Update the database with bugs analysis
 # CACHE_TYPE=filesystem \
