bdf1bfb27613e6b81d31d2126b6ab5697e400f18
Rok Garbas <rok@garbas.si>
2016-11-03 03:07:44 +0100

    releng_tooltool: init

---
+++ src/releng_tooltool/releng_tooltool/__init__.py
@@ -0,0 +1,41 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+
+import os
+
+import releng_common
+import releng_common.db
+
+
+DEBUG = bool(os.environ.get('DEBUG', __name__ == '__main__'))
+HERE = os.path.dirname(os.path.abspath(__file__))
+APP_SETTINGS = os.path.abspath(os.path.join(HERE, '..', 'settings.py'))
+
+
+def init_app(app):
+    return app.api.register(
+        os.path.join(os.path.dirname(__file__), 'swagger.yml'))
+
+
+if DEBUG and not os.environ.get('DATABASE_URL'):
+    os.environ['DATABASE_URL'] = 'sqlite:///%s' % (
+        os.path.abspath(os.path.join(HERE, '..', 'app.db')))
+
+if not os.environ.get('APP_SETTINGS') and \
+       os.path.isfile(APP_SETTINGS):
+    os.environ['APP_SETTINGS'] = APP_SETTINGS
+
+
+app = releng_common.create_app(
+    "releng_tooltool",
+    extensions=[init_app, releng_common.db],
+    debug=DEBUG,
+    debug_src=HERE,
+)
+
+
+if __name__ == "__main__":
+    app.run(**app.run_options())
