592db3469de1a136cc6f488c2cc32fd57bd7cce7
Rok Garbas <rok@garbas.si>
2016-11-03 05:20:25 +0100

    releng_tooltool: correct swagger file

---
+++ src/releng_tooltool/releng_tooltool/api.py
@@ -0,0 +1,24 @@
+
+
+def search_batches():
+    pass
+def upload_batch():
+    pass
+
+def get_batch():
+    pass
+
+def upload_complete():
+    pass
+
+def search_files():
+    pass
+
+def get_file():
+    pass
+
+def download_file():
+    pass
+
+def patch_file():
+    pass
