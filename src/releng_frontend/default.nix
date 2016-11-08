364fb9b0d7dcff9cf3afdf33a419e6e162964118
Rok Garbas <rok@garbas.si>
2016-11-08 15:21:36 +0100

    nix: bring makeElmStuff function to services (not exposed in nixpkgs)

--- src/releng_frontend/default.nix
+++ src/releng_frontend/default.nix
@@ -3,10 +3,10 @@
 let
   inherit (builtins) readFile;
   inherit (releng_pkgs.lib) mkFrontend;
-  inherit (releng_pkgs.pkgs.lib) removeSuffix;
+  inherit (releng_pkgs.pkgs.lib) fileContents;
 in mkFrontend {
   name = "releng_frontend";
-  version = removeSuffix "\n" (readFile ./../../VERSION);
+  version = fileContents ./../../VERSION;
   src = ./.;
   node_modules = import ./node-modules.nix { inherit (releng_pkgs) pkgs; };
   elm_packages = import ./elm-packages.nix;
