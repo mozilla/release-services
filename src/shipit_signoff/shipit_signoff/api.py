e88ac2ae9152a5c1c77dc7d03ec97d63a65470cf
Chris AtLee <catlee@mozilla.com>
2016-11-16 17:59:41 +0100

    shipit_signoff: add basic logging

--- src/shipit_signoff/shipit_signoff/api.py
+++ src/shipit_signoff/shipit_signoff/api.py
@@ -4,39 +4,51 @@
 
 from __future__ import absolute_import
 
+import logging
+
+log = logging.getLogger(__name__)
+
 STEPS = {}
 SIGNOFFS = {}
 
 
 def list_steps():
+    log.info('listing steps')
     return list(STEPS.keys())
 
 
 def get_step(uid):
+    log.info('getting step %s', uid)
     if uid not in STEPS:
         return None, 404
     return dict(uid=uid, input={}, parameters={})
 
 
 def get_step_status(uid):
+    log.info('getting step status %s', uid)
     return dict(
         state=STEPS[uid]
     )
 
 
 def create_step(uid):
+    log.info('creating step %s', uid)
     STEPS[uid] = 'running'
     SIGNOFFS[uid] = False
     return None
 
 
 def delete_step(uid):
+    log.info('deleting step %s', uid)
     del STEPS[uid]
     del SIGNOFFS[uid]
     return None
 
 
 def signoff(uid):
+    log.error('signing off step %s', uid)
+    if uid not in STEPS:
+        return None, 404
     SIGNOFFS[uid] = True
     STEPS[uid] = 'completed'
     return None
