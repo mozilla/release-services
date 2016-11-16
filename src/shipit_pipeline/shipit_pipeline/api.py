1ebd6985fcf63b07fc7a549625aa24e5b1e8df25
Chris AtLee <catlee@mozilla.com>
2016-11-16 18:39:09 +0100

    dummy pipeline service

--- src/shipit_pipeline/shipit_pipeline/api.py
+++ src/shipit_pipeline/shipit_pipeline/api.py
@@ -4,6 +4,44 @@
 
 from __future__ import absolute_import
 
+import logging
 
-def example():
-    return {}
+log = logging.getLogger(__name__)
+
+STEPS = {}
+
+
+def list_steps():
+    log.info('listing steps')
+    return list(STEPS.keys())
+
+
+def get_step(uid):
+    log.info('getting step %s', uid)
+    if uid not in STEPS:
+        return None, 404
+    return dict(uid=uid, input={}, parameters={})
+
+
+def get_step_status(uid):
+    log.info('getting step status %s', uid)
+    return dict(
+        state=STEPS[uid]
+    )
+
+
+def create_step(uid):
+    log.info('creating step %s', uid)
+    STEPS[uid] = 'running'
+    return None
+
+
+def delete_step(uid):
+    log.info('deleting step %s', uid)
+    del STEPS[uid]
+    return None
+
+
+def ticktock():
+    log.info('refreshing pipelines')
+    return None
