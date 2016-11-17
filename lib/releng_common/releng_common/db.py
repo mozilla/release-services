316b771cb30ec383a8aee426ef1aa26eb7ba7812
Bastien Abadie <bastien@nextcairn.com>
2016-11-17 11:36:15 +0100

    Releng Common: Do not init migrations automatically.

--- lib/releng_common/releng_common/db.py
+++ lib/releng_common/releng_common/db.py
@@ -15,6 +15,35 @@ db = flask_sqlalchemy.SQLAlchemy()
 migrate = flask_migrate.Migrate(db=db)
 
 
+def init_database(app):
+    """
+    Run Migrations through Alembic
+    """
+    migrations_dir = os.path.abspath(
+        os.path.join(app.root_path, '..', 'migrations'))
+
+    with app.app_context():
+
+        # Needed to init potential migrations later on
+        # Use a separate alembic_version table per app
+        options = {
+            'version_table': '{}_alembic_version'.format(app.name),
+        }
+        migrate.init_app(app, directory=migrations_dir, **options)
+
+        if os.path.isdir(migrations_dir):
+            logger.info('Starting migrations', app=app.name)
+            try:
+                flask_migrate.upgrade()
+                logger.info('Completed migrations', app=app.name)
+            except Exception as e:
+                logger.error('Migrations failure', app=app.name, error=e)
+
+        else:
+            logger.info('No migrations: creating full DB', app=app.name)
+            db.create_all()
+
+
 def init_app(app):
     db.init_app(app)
 
@@ -23,26 +52,9 @@ def init_app(app):
         if not table_name.startswith(app.name):
             raise Exception('DB table {} should start with {}'.format(table_name, app.name))  # noqa
 
-    # Setup migrations
-    migrations_dir = os.path.abspath(
-        os.path.join(app.root_path, '..', 'migrations'))
-
-    # Setup migrations
-    with app.app_context():
-        options = {
-            # Use a separate alembic_version table per app
-            'version_table': '{}_alembic_version'.format(app.name),
-        }
-        migrate.init_app(app, directory=migrations_dir, **options)
-        logger.info('Starting migrations', app=app.name)
-        if os.path.isdir(migrations_dir):
-            try:
-                flask_migrate.upgrade()
-                logger.info('Completed migrations', app=app.name)
-            except Exception as e:
-                logger.error('Migrations failure', app=app.name, error=e)
-        else:
-            flask_migrate.init()
+    # Try to run migrations on the app
+    # or direct db creation
+    init_database(app)
 
     @app.before_request
     def setup_request():
