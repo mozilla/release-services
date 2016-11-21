9b261ab06c88e5c86e7a99bf98ee5a845de8c78d
Bastien Abadie <bastien@nextcairn.com>
2016-11-21 19:16:20 +0100

    Shipit frontend: Use TaskclusterLogin from elm-common.

--- src/shipit_frontend/src/AppTest.elm
+++ src/shipit_frontend/src/AppTest.elm
@@ -3,13 +3,17 @@ port module AppTest exposing (..)
 import Html exposing (..)
 import Html.App
 import RouteUrl exposing ( UrlChange )
+import RouteUrl.Builder as Builder exposing ( Builder, builder, replacePath )
 import Navigation exposing ( Location )
 
 import BugzillaLogin as Bugzilla
+import TaskclusterLogin as User
 
 type Msg = BugzillaMsg Bugzilla.Msg
+  | UserMsg User.Msg
 
 type alias Model = {
+  user : User.Model,
   bugzilla : Bugzilla.Model
 }
 
@@ -22,14 +26,17 @@ init : Flags -> (Model, Cmd Msg)
 init flags =
     let
         (bz, bzCmd) = Bugzilla.init flags.bugzilla_url
+        (user, userCmd) = User.init
     in
     (
       {
-         bugzilla = bz
+        bugzilla = bz,
+        user = user
       },
       -- Follow through with sub parts init
       Cmd.batch [
-        Cmd.map BugzillaMsg bzCmd
+        Cmd.map BugzillaMsg bzCmd,
+        Cmd.map UserMsg userCmd
       ]
     )
 
@@ -45,16 +52,42 @@ update msg model =
           Cmd.map BugzillaMsg bzCmd
         )
 
+    UserMsg userMsg ->
+      let
+        (newUser, userCmd) = User.update userMsg model.user
+      in
+        (
+          { model | user = newUser },
+          Cmd.map UserMsg userCmd
+        )
+
 -- Empty view
 view model =
     div [] [ 
+      h1 [] [text "Taskcluster"],
+      Html.App.map UserMsg (User.view model.user),
+      h1 [] [text "Bugzilla"],
       Html.App.map BugzillaMsg (Bugzilla.view model.bugzilla)
     ]
 
 -- Empty Routing
 location2messages : Location -> List Msg
 location2messages location =
-  []
+  let
+    builder = Builder.fromUrl location.href
+  in
+    case Builder.path builder of
+      first :: rest ->
+        case first of
+          "login" -> [
+            Builder.query builder
+            |> User.convertUrlQueryToUser
+            |> User.Logging
+            |> UserMsg
+          ]
+          _ -> []
+
+      _ -> []
 
 delta2url : Model -> Model -> Maybe UrlChange
 delta2url previous current =
@@ -64,6 +97,6 @@ delta2url previous current =
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.batch [
-    Sub.map BugzillaMsg (Bugzilla.bugzillalogin_get (Bugzilla.Logged))
-
+    Sub.map BugzillaMsg (Bugzilla.bugzillalogin_get (Bugzilla.Logged)),
+    Sub.map UserMsg (User.taskclusterlogin_get (User.Logged))
   ]
