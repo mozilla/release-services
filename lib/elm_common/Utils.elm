b94917380c8b78e37974dbbab40e3d8d48ae4de7
Bastien Abadie <bastien@nextcairn.com>
2016-11-21 19:16:20 +0100

    Shipit frontend: Use TaskclusterLogin from elm-common.

---
+++ lib/elm_common/Utils.elm
@@ -0,0 +1,20 @@
+module Utils exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events as Events
+import Json.Decode as JsonDecode
+
+onClick msg =
+  Events.onWithOptions
+    "click"
+    (Events.Options False True)
+    (JsonDecode.succeed msg)
+
+onChange : (String -> msg) -> Attribute msg
+onChange handler = 
+  Events.on "change" <| JsonDecode.map handler <| JsonDecode.at ["target", "value"] JsonDecode.string
+
+
+eventLink msg attributes =
+  a ([ onClick <| msg, href "#"  ] ++ attributes)
