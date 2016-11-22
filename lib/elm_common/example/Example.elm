2779c300b5b2b41f2e379231c97896256fa796b0
Bastien Abadie <bastien@nextcairn.com>
2016-11-22 13:24:46 +0100

    Elm common: Hawk integration on a single request

--- lib/elm_common/example/Example.elm
+++ lib/elm_common/example/Example.elm
@@ -1,20 +1,30 @@
 port module Example exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Html.App
+import Http
+import Json.Decode as JsonDecode exposing ((:=))
 import RouteUrl exposing ( UrlChange )
 import RouteUrl.Builder as Builder exposing ( Builder, builder, replacePath )
+import RemoteData exposing ( WebData , RemoteData(..))
 import Navigation exposing ( Location )
 
 import BugzillaLogin as Bugzilla
 import TaskclusterLogin as User
+import Hawk
 
 type Msg = BugzillaMsg Bugzilla.Msg
   | UserMsg User.Msg
+  | HawkMsg Hawk.Msg
+  | LoadScopes
+  | FetchedScopes (WebData (List String))
 
 type alias Model = {
   user : User.Model,
-  bugzilla : Bugzilla.Model
+  bugzilla : Bugzilla.Model,
+  scopes : WebData (List String)
 }
 
 type alias Flags = {
@@ -30,7 +40,8 @@ init flags =
     (
       {
         bugzilla = bz,
-        user = user
+        user = user,
+        scopes = NotAsked
       },
       -- Follow through with sub parts init
       Cmd.batch [
@@ -60,14 +71,69 @@ update msg model =
           Cmd.map UserMsg userCmd
         )
 
--- Empty view
+    LoadScopes ->
+      case model.user.credentials of
+        Just credentials ->
+          let
+            -- Build Taskcluster http request
+            url = "https://auth.taskcluster.net/v1/scopes/current"
+            request = Http.Request "GET" [] url Http.empty
+          in
+            (
+              model,
+              Cmd.map HawkMsg (Hawk.add_header request credentials)
+            )
+        Nothing ->
+            (model, Cmd.none)
+
+    FetchedScopes scopes ->
+      -- Save scopes on model
+      ( { model | scopes = scopes }, Cmd.none)
+
+    HawkMsg hawkMsg ->
+      let
+
+        -- TODO: support multiple decoders
+        decoder = JsonDecode.at ["scopes"] (JsonDecode.list JsonDecode.string)
+
+        (hawkCmd, responses) = Hawk.update decoder hawkMsg
+      in
+        (
+          model,
+          Cmd.batch [
+            Cmd.map HawkMsg hawkCmd,
+            Cmd.map FetchedScopes responses 
+          ]
+        )
+
+-- Demo view
 view model =
-    div [] [ 
-      h1 [] [text "Taskcluster"],
-      Html.App.map UserMsg (User.view model.user),
-      h1 [] [text "Bugzilla"],
-      Html.App.map BugzillaMsg (Bugzilla.view model.bugzilla)
-    ]
+  div [] [ 
+    h1 [] [text "Taskcluster"],
+    Html.App.map UserMsg (User.view model.user),
+    h1 [] [text "Hawk"],
+    viewHawk model,
+    h1 [] [text "Bugzilla"],
+    Html.App.map BugzillaMsg (Bugzilla.view model.bugzilla)
+  ]
+
+viewHawk model = 
+  div [] [
+    case model.user.credentials of
+      Just credentials ->
+        button [onClick LoadScopes] [text "Request Taskcluster scopes"]
+      Nothing ->
+        span [class "text-warning"] [text "Login on Taskcluster first."]
+    , case model.scopes of
+      Success scopes ->
+        ul [] (List.map (\s -> li [] [text s]) scopes)
+      Failure err ->
+        p [class "text-danger"] [text ("Error: "++(toString err))]
+      Loading ->
+        p [class "text-info"] [text "Loading scopes..."]
+      NotAsked ->
+        p [class "text-info"] [text "Scopes not fetched yet."]
+  ]
 
 -- Empty Routing
 location2messages : Location -> List Msg
@@ -97,5 +163,6 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.batch [
     Sub.map BugzillaMsg (Bugzilla.bugzillalogin_get (Bugzilla.Logged)),
-    Sub.map UserMsg (User.taskclusterlogin_get (User.Logged))
+    Sub.map UserMsg (User.taskclusterlogin_get (User.Logged)),
+    Sub.map HawkMsg (Hawk.hawk_send_request (Hawk.SendRequest))
   ]
