35c57956db78fbfb1b607fee7dd678f3dfc2dceb
Rok Garbas <rok@garbas.si>
2016-11-23 04:12:41 +0100

    releng_frontend: link extensions from elm_common

--- src/releng_frontend/default.nix
+++ src/releng_frontend/default.nix
@@ -8,12 +8,9 @@ in mkFrontend {
   name = "releng_frontend";
   version = fileContents ./../../VERSION;
   src = ./.;
+  src_elm_common = ./../../lib/elm_common;
   node_modules = import ./node-modules.nix { inherit (releng_pkgs) pkgs; };
   elm_packages = import ./elm-packages.nix;
-  patchPhase = ''
-    rm src/user.js
-    cp ${./../../lib/elm_common/user.js} src/user.js
-  '';
   postInstall = ''
     mkdir -p $out/trychooser
     cp src/trychooser/* $out/trychooser/
