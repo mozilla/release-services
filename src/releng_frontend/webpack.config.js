0c0094bf889984079756814ea65bc25fe2ce98a5
Rok Garbas <rok@garbas.si>
2016-11-23 04:35:37 +0100

    fixing webpack.config.js in all frontend services

--- src/releng_frontend/webpack.config.js
+++ src/releng_frontend/webpack.config.js
@@ -1,1 +1,26 @@
-../../lib/elm_common/webpack.config.js
+const fs = require('fs');
+const config_type = process.argv.indexOf('start') !== -1 ? 'dev' : 'prod';
+const config = require('mozilla-neo/config/webpack.' + config_type);
+const ELM_EXT = /\.elm$/;
+
+// Remove react-hot from config
+config.module.loaders = config.module.loaders.filter(function(loader){
+  return !(loader['loaders'] && loader['loaders'].indexOf('react-hot') > -1);
+});
+
+config.module.loaders.push({
+  test: ELM_EXT,
+  exclude: /(node_modules|elm-stuff)/,
+  loader: 'elm-webpack'
+});
+
+config.module.noParse = ELM_EXT;
+
+if (config_type === 'dev') {
+  config.devServer.https = true;
+  config.devServer.cacert = fs.readFileSync(process.env.SSL_CACERT);
+  config.devServer.cert = fs.readFileSync(process.env.SSL_CERT);
+  config.devServer.key= fs.readFileSync(process.env.SSL_KEY);
+}
+
+module.exports = config;
