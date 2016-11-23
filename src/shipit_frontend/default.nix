207c1dbadea5c4808d861a20174a082a24b8e37d
Rok Garbas <rok@garbas.si>
2016-11-23 04:23:42 +0100

    shipit_frontend: include elm_common links into the project

--- src/shipit_frontend/default.nix
+++ src/shipit_frontend/default.nix
@@ -8,6 +8,7 @@ in mkFrontend {
   name = "shipit_frontend";
   version = fileContents ./../../VERSION;
   src = ./.;
+  src_elm_common = ./../../lib/elm_common;
   node_modules = import ./node-modules.nix { inherit (releng_pkgs) pkgs; };
   elm_packages = import ./elm-packages.nix;
 }
