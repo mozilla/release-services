97da5c92888d09e9df0adc6270f249be77405b41
Bastien Abadie <bastien@nextcairn.com>
2016-12-05 15:03:48 +0100

    Shipit dashboard: update to libmozdata 0.21.0

--- src/shipit_dashboard/requirements_override.nix
+++ src/shipit_dashboard/requirements_override.nix
@@ -43,4 +43,12 @@ self: super: {
     patches = [ (pkgs.fetchurl { url = "https://github.com/taskcluster/taskcluster-client.py/pull/56.patch"; sha256 = "1k29primpv3fa62b1wq52shwvjamcja1m6ph66vykxab5ywmfkfw"; }) ];
   });
 
+  "libmozdata" = python.overrideDerivation super."libmozdata" (old: {
+		# Remove useless depencies
+    preConfigure = ''
+      sed -i -e "s|mercurial>=3.9.1; python_version < '3.0'||" requirements.txt
+      sed -i -e "s|setuptools>=28.6.1||" requirements.txt
+    '';
+  });
+
 }
