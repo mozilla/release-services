79d640c5027b73e3edf36320a1eee19523826675
Bastien Abadie <bastien@nextcairn.com>
2016-12-12 15:33:53 +0100

    Shipit dashboard: Add unit tests for contributors/karma

--- src/shipit_dashboard/tests/conftest.py
+++ src/shipit_dashboard/tests/conftest.py
@@ -3,6 +3,7 @@ import pickle
 import os
 import glob
 import json
+from releng_common.mocks import build_header
 
 FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
 
@@ -46,7 +47,9 @@ def bugs(app):
     """
     Add an analysis and some bugs
     """
-    from shipit_dashboard.models import BugAnalysis, BugResult
+    from shipit_dashboard.models import (
+        BugAnalysis, BugResult, Contributor, BugContributor
+    )
     from releng_common.db import db
 
     # Add an analysis
@@ -64,4 +67,56 @@ def bugs(app):
         analysis.bugs.append(bug)
         db.session.add(bug)
 
+        # Add creator & link to bug
+        user = payload['analysis']['users']['creator']
+        contrib = Contributor(bugzilla_id=user['id'])
+        contrib.name = user.get('real_name', user['name'])
+        contrib.email = user['email']
+        contrib.avatar_url = 'gravatar_url_here'
+        contrib.karma = 1
+        contrib.comment_private = 'hidden comment'
+        contrib.comment_public = 'Top Contributor'
+        link = BugContributor(bug=bug, contributor=contrib, roles='creator')
+        db.session.add(contrib)
+        db.session.add(link)
+
     db.session.commit()
+
+
+def hawk_header(scopes):
+    """"
+    Helper to build an Hawk header
+    for a set of TC scopes
+    """
+    client_id = 'test/shipit-user@mozilla.com'
+    ext_data = {
+        'scopes': scopes,
+    }
+    return build_header(client_id, ext_data)
+
+
+@pytest.yield_fixture(scope='module')
+def header_user(app):
+    """
+    Build an Hawk header for user role
+    """
+    from shipit_dashboard import SCOPES_USER
+    return hawk_header(SCOPES_USER)
+
+
+@pytest.yield_fixture(scope='module')
+def header_admin(app):
+    """
+    Build an Hawk header for admin role
+    """
+    from shipit_dashboard import SCOPES_ADMIN
+    return hawk_header(SCOPES_ADMIN)
+
+
+@pytest.yield_fixture(scope='module')
+def header_bot(app):
+    """
+    Build an Hawk header for bot role
+    """
+    from shipit_dashboard import SCOPES_BOT
+    return hawk_header(SCOPES_BOT)
