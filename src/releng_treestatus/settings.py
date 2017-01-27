58101591238462c01a32f6a9597bba50e19e5e61
Rok Garbas <rok@garbas.si>
2017-01-27 10:09:14 +0100

    releng_treestatus: prepare for redis cache integration

--- src/releng_treestatus/settings.py
+++ src/releng_treestatus/settings.py
@@ -9,9 +9,26 @@ import os
 
 SWAGGER_BASE_URL = os.environ.get('SWAGGER_BASE_URL')
 DATABASE_URL = os.environ.get('DATABASE_URL')
+CACHE = {
+    x: os.environ.get(x)
+    for x in os.environ.keys()
+    if x.startswith('CACHE_')
+}
+
+
+if 'REDIS_URL' in os.environ:
+    CACHE['CACHE_TYPE'] = 'redis'
+    CACHE['CACHE_REDIS_URL'] = os.environ['REDIS_URL']
+
+if 'CACHE_DEFAULT_TIMEOUT' not in CACHE:
+    CACHE['CACHE_DEFAULT_TIMEOUT'] = 60 * 5
+
+if 'CACHE_KEY_PREFIX' not in CACHE:
+    CACHE['CACHE_KEY_PREFIX'] = "releng_treestatus-"
 
 if not DATABASE_URL:
     raise Exception("You need to specify DATABASE_URL variable.")
+
 if not DATABASE_URL.startswith('postgresql://'):
     raise Exception('Shipit dashboard needs a postgresql:// DATABASE_URL')
 
