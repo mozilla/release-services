8900f07c04d7b26667aad713ab63239269b456a9
Rok Garbas <rok@garbas.si>
2017-02-01 14:15:41 +0100

    releng_tooltool: get tooltool in running state
    * use postgres (instead of sqlite) in development, in all services
    * add missing dependencies: boto, pytz
    * fix models to work with naming changes of databases
    * enable deploying to production and staging

--- src/releng_tooltool/releng_tooltool/api.py
+++ src/releng_tooltool/releng_tooltool/api.py
@@ -5,20 +5,18 @@
 from __future__ import absolute_import
 
 import datetime
-from cli_common import log
 import time
+import flask
+import flask_login
 import random
+import sqlalchemy as sa
+import time
+import werkzeug
+import werkzeug.exceptions
 
-from flask import current_app
-from flask import g
-from flask import redirect
-from flask_login import current_user
-from releng_tooltool.aws import AWS
-from sqlalchemy import or_
-from werkzeug import Response
-from werkzeug.exceptions import BadRequest, NotFound
-
+from cli_common import log
 from releng_tooltool.models import Batch, File, PendingUpload, BatchFile
+from releng_tooltool.aws import AWS
 from releng_tooltool.utils import (
     get_region_and_bucket, now, is_valid_sha512, keyname
 )
@@ -38,8 +36,8 @@ def search_batches(query):
     return [
         row.to_json()
         for row in Batch.query.filter(
-                        or_(Batch.author.contains(query),
-                            Batch.message.contains(query))).all()
+                        sa.or_(Batch.author.contains(query),
+                               Batch.message.contains(query))).all()
     ]
 
 
@@ -47,19 +45,25 @@ def upload_batch(region, body):
     region, bucket = get_region_and_bucket(region)
 
     if not body.message:
-        raise BadRequest("message must be non-empty")
+        raise werkzeug.exceptions.BadRequest(
+            "message must be non-empty"
+        )
 
     if not body.files:
-        raise BadRequest("a batch must include at least one file")
+        raise werkzeug.exceptions.BadRequest(
+            "a batch must include at least one file"
+        )
 
     if body.author:
-        raise BadRequest("Author must not be specified for upload")
+        raise werkzeug.exceptions.BadRequest(
+            "Author must not be specified for upload"
+        )
     try:
-        body.author = current_user.authenticated_email
+        body.author = flask_login.current_user.authenticated_email
     except AttributeError:
         # no authenticated_email -> use the stringified user (probably a token
         # ID)
-        body.author = str(current_user)
+        body.author = str(flask_login.current_user)
 
     # verify permissions based on visibilities
     visibilities = set(f.visibility for f in body.files.itervalues())
@@ -70,7 +74,7 @@ def upload_batch(region, body):
         #     raise Forbidden("no permission to upload {} files".format(v))
         pass
 
-    session = g.db.session
+    session = flask.g.db.session
 
     batch = Batch(
         uploaded=now(),
@@ -78,7 +82,7 @@ def upload_batch(region, body):
         message=body.message,
     )
 
-    aws = AWS(current_app.config.get('AWS', {}))
+    aws = AWS(flask.current_app.config.get('AWS', {}))
     s3 = aws.connect_to('s3', region)
 
     for filename, info in body.files.iteritems():
@@ -89,16 +93,24 @@ def upload_batch(region, body):
             mozdef=True,
         )
         if info.algorithm != 'sha512':
-            raise BadRequest("'sha512' is the only allowed digest algorithm")
+            raise werkzeug.exceptions.BadRequest(
+                "'sha512' is the only allowed digest algorithm"
+            )
         if not is_valid_sha512(info.digest):
-            raise BadRequest("Invalid sha512 digest")
+            raise werkzeug.exceptions.BadRequest(
+                "Invalid sha512 digest"
+            )
         digest = info.digest
         file = File.query.filter(File.sha512 == digest).first()
         if file and file.visibility != info.visibility:
-            raise BadRequest("Cannot change a file's visibility level")
+            raise werkzeug.exceptions.BadRequest(
+                "Cannot change a file's visibility level"
+            )
         if file and file.instances != []:
             if file.size != info.size:
-                raise BadRequest("Size mismatch for {}".format(filename))
+                raise werkzeug.exceptions.BadRequest(
+                    "Size mismatch for {}".format(filename)
+                )
         else:
             if not file:
                 file = File(
@@ -110,7 +122,7 @@ def upload_batch(region, body):
                 "generating signed S3 PUT URL to {} for {}; expiring in "
                 "{}s".format(
                     info.digest[:10],
-                    current_user,
+                    flask_login.current_user,
                     UPLOAD_EXPIRES_IN,
                 )
             )
@@ -147,13 +159,13 @@ def upload_batch(region, body):
 def get_batch(id):
     row = Batch.query.filter(Batch.id == id).first()
     if not row:
-        raise NotFound
+        raise werkzeug.exceptions.NotFound
     return row.to_json()
 
 
 def upload_complete(digest):
     if not is_valid_sha512(digest):
-        raise BadRequest("Invalid sha512 digest")
+        raise werkzeug.exceptions.BadRequest("Invalid sha512 digest")
 
     # if the pending upload is still valid, then we can't check this file
     # yet, so return 409 Conflict.  If there is no PU, or it's expired,
@@ -165,7 +177,7 @@ def upload_complete(digest):
             if until > datetime.timedelta(0):
                 # add 1 second to avoid rounding / skew errors
                 hdr = {'X-Retry-After': str(1 + int(until.total_seconds()))}
-                return Response(status=409, headers=hdr)
+                return werkzeug.Response(status=409, headers=hdr)
 
     # start a celery task in the background and return immediately
     # TODO: grooming.check_file_pending_uploads.delay(digest)
@@ -173,32 +185,32 @@ def upload_complete(digest):
 
 
 def search_files(query):
-    session = g.db.session
+    session = flask.g.db.session
     _query = session.query(File).join(BatchFile)
-    _query = query.filter(or_(BatchFile.filename.contains(query),
-                              File.sha512.startswith(query)))
+    _query = query.filter(sa.or_(BatchFile.filename.contains(query),
+                                 File.sha512.startswith(query)))
     return [row.to_json() for row in _query.all()]
 
 
 def get_file(digest):
     row = File.query.filter(File.sha512 == digest).first()
     if not row:
-        raise NotFound
+        raise werkzeug.exceptions.NotFound
     return row.to_json(include_instances=True)
 
 
 def download_file(digest, region):
     log = logger.bind(tooltool_sha512=digest, tooltool_operation='download')
     if not is_valid_sha512(digest):
-        raise BadRequest("Invalid sha512 digest")
+        raise werkzeug.exceptions.BadRequest("Invalid sha512 digest")
 
     # see where the file is..
     file_row = File.query.filter(File.sha512 == digest).first()
     if not file_row or not file_row.instances:
-        raise NotFound
+        raise werkzeug.exceptions.NotFound
 
     # check visibility
-    allow_pub_dl = current_app.config.get('TOOLTOOL_ALLOW_ANONYMOUS_PUBLIC_DOWNLOAD')  # noqa
+    allow_pub_dl = flask.current_app.config.get('TOOLTOOL_ALLOW_ANONYMOUS_PUBLIC_DOWNLOAD')  # noqa
     if file_row.visibility != 'public' or not allow_pub_dl:
         # TODO: check scope
         # if not p.get('tooltool.download.{}'.format(file_row.visibility)).can():  # noqa
@@ -206,7 +218,7 @@ def download_file(digest, region):
         pass
 
     # figure out which region to use, and from there which bucket
-    cfg = current_app.config['TOOLTOOL_REGIONS']
+    cfg = flask.current_app.config['TOOLTOOL_REGIONS']
     selected_region = None
     for inst in file_row.instances:
         if inst.region == region:
@@ -220,37 +232,37 @@ def download_file(digest, region):
 
     key = keyname(digest)
 
-    s3 = current_app.aws.connect_to('s3', selected_region)
+    s3 = flask.current_app.aws.connect_to('s3', selected_region)
     log.info("generating signed S3 GET URL for {}.. expiring in {}s".format(
         digest[:10], GET_EXPIRES_IN))
     signed_url = s3.generate_url(
         method='GET', expires_in=GET_EXPIRES_IN, bucket=bucket, key=key)
 
-    return redirect(signed_url)
+    return flask.redirect(signed_url)
 
 
 def patch_file(digest, body):
-    session = current_app.db.session
+    session = flask.current_app.db.session
     file = session.query(File).filter(File.sha512 == digest).first()
     if not file:
-        raise NotFound
+        raise werkzeug.exceptions.NotFound
 
     for change in body:
         if 'op' not in change:
-            raise BadRequest("no op")
+            raise werkzeug.exceptions.BadRequest("no op")
         if change['op'] == 'delete_instances':
             key_name = keyname(digest)
-            cfg = current_app.config.get('TOOLTOOL_REGIONS')
+            cfg = flask.current_app.config.get('TOOLTOOL_REGIONS')
             for i in file.instances:
-                conn = current_app.aws.connect_to('s3', i.region)
+                conn = flask.current_app.aws.connect_to('s3', i.region)
                 bucket = conn.get_bucket(cfg[i.region])
                 bucket.delete_key(key_name)
                 session.delete(i)
         elif change['op'] == 'set_visibility':
             if change['visibility'] not in ('internal', 'public'):
-                raise BadRequest("bad visibility level")
+                raise werkzeug.exceptions.BadRequest("bad visibility level")
             file.visibility = change['visibility']
         else:
-            raise BadRequest("unknown op")
+            raise werkzeug.exceptions.BadRequest("unknown op")
     session.commit()
     return file.to_json(include_instances=True)
