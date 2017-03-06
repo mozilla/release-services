cd2257d2f98397dad37c53ea5a87aa161ef949e2
Rok Garbas <rok@garbas.si>
2017-03-06 13:43:49 +0100

    setup: tool.push updated

--- nix/tools/push.nix
+++ nix/tools/push.nix
@@ -1,4 +1,4 @@
-# generated using pypi2nix tool (version: 1.5.0.dev0)
+# generated using pypi2nix tool (version: 1.8.0)
 # See more at: https://github.com/garbas/pypi2nix
 #
 # COMMAND:
@@ -13,11 +13,11 @@ let
   inherit (pkgs) makeWrapper;
   inherit (pkgs.stdenv.lib) fix' extends inNixShell;
 
-  pythonPackages = import "${toString pkgs.path}/pkgs/top-level/python-packages.nix" {
+  pythonPackages =
+  import "${toString pkgs.path}/pkgs/top-level/python-packages.nix" {
     inherit pkgs;
     inherit (pkgs) stdenv;
     python = pkgs.python35;
-    self = pythonPackages;
   };
 
   commonBuildInputs = [];
@@ -31,11 +31,13 @@ let
         buildInputs = [ makeWrapper ] ++ (builtins.attrValues pkgs);
         buildCommand = ''
           mkdir -p $out/bin
-          ln -s ${pythonPackages.python.interpreter} $out/bin/${pythonPackages.python.executable}
-          for dep in ${builtins.concatStringsSep " " (builtins.attrValues pkgs)}; do
+          ln -s ${pythonPackages.python.interpreter}               $out/bin/${pythonPackages.python.executable}
+          for dep in ${builtins.concatStringsSep " "               (builtins.attrValues pkgs)}; do
             if [ -d "$dep/bin" ]; then
               for prog in "$dep/bin/"*; do
-                ln -s $prog $out/bin/`basename $prog`
+                if [ -f $prog ]; then
+                  ln -s $prog $out/bin/`basename $prog`
+                fi
               done
             fi
           done
@@ -61,7 +63,39 @@ let
 
   python = withPackages {};
 
-  generated = import ./push_generated.nix { inherit pkgs python commonBuildInputs commonDoCheck; };
+  generated = self: {
+
+    "push" = python.mkDerivation {
+      name = "push-0.0.1";
+      src = pkgs.fetchurl { url = "https://github.com/garbas/push/archive/master.tar.gz"; sha256 = "7fa5cce8617c59e96234a8ad78ea475b647342f294505b286ea41966a3cb642a"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."requests"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = "";
+        description = "Utility to push tar.gz docker images to v2 registry";
+      };
+    };
+
+
+
+    "requests" = python.mkDerivation {
+      name = "requests-2.13.0";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/16/09/37b69de7c924d318e51ece1c4ceb679bf93be9d05973bb30c35babd596e2/requests-2.13.0.tar.gz"; sha256 = "5722cd09762faa01276230270ff16af7acf7c5c45d623868d9ba116f15791ce8"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.asl20;
+        description = "Python HTTP for Humans.";
+      };
+    };
+
+  };
   overrides = import ./push_override.nix { inherit pkgs python; };
 
 in python.withPackages (fix' (extends overrides generated))
