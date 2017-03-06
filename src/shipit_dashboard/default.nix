5e0d58178cdd6d06cfe9d8aee0fad54461cb3a2e
Rok Garbas <rok@garbas.si>
2017-03-06 20:59:46 +0100

    shipit_dashboard: updated

--- src/shipit_dashboard/default.nix
+++ src/shipit_dashboard/default.nix
@@ -3,40 +3,31 @@
 
 let
 
-  inherit (releng_pkgs.lib) mkBackend mkTaskclusterHook filterSource;
+  inherit (releng_pkgs.lib) mkBackend fromRequirementsFile filterSource;
   inherit (releng_pkgs.pkgs) writeScript;
   inherit (releng_pkgs.pkgs.lib) fileContents;
   inherit (releng_pkgs.tools) pypi2nix;
 
   python = import ./requirements.nix { inherit (releng_pkgs) pkgs; };
-  releng_common = import ./../../lib/releng_common {
-    inherit releng_pkgs python;
-    extras = ["api" "auth" "cors" "log" "db" "security" ];
-  };
+  name = "mozilla-shipit-dashboard";
 
   self = mkBackend rec {
-    inherit python releng_common;
-    production = true;
-    name = "shipit_dashboard";
+    inherit python name;
+    inProduction = true;
     version = fileContents ./../../VERSION;
     src = filterSource ./. { inherit name; };
     buildInputs =
-      [ python.packages."flake8"
-        python.packages."pytest"
-        python.packages."ipdb"
-        python.packages."responses"
-      ];
+      fromRequirementsFile ./requirements-dev.txt python.packages;
+    propagatedBuildInputs =
+      fromRequirementsFile ./requirements.txt python.packages;
     passthru = {
       update = writeScript "update-${name}" ''
-        pushd src/${name}
+        pushd ${self.src_path}
         ${pypi2nix}/bin/pypi2nix -v \
-         -V 3.5 \
-         -E "postgresql" \
-				 -s "six packaging appdirs" \
-         -r ../../lib/releng_common/requirements-dev.txt \
-         -r requirements.txt \
-         -r requirements-dev.txt \
-         -r requirements-nix.txt
+          -V 3.5 \
+          -E "postgresql" \
+          -r requirements.txt \
+          -r requirements-dev.txt
         popd
       '';
     };
