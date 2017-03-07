9c5d870761c4ef26db1b76351c5074ba880cf2ec
Rok Garbas <rok@garbas.si>
2017-03-07 02:10:13 +0100

    shipit_frontend: elm packages updated

--- src/shipit_frontend/default.nix
+++ src/shipit_frontend/default.nix
@@ -5,11 +5,11 @@ let
   inherit (releng_pkgs.lib) mkFrontend;
   inherit (releng_pkgs.pkgs.lib) fileContents;
 in mkFrontend {
+  inProduction = true;
   name = "shipit_frontend";
   version = fileContents ./../../VERSION;
   csp = "default-src 'none'; img-src 'self' data: *.gravatar.com; script-src 'self'; style-src 'self'; font-src 'self';";
   src = ./.;
   node_modules = import ./node-modules.nix { inherit (releng_pkgs) pkgs; };
   elm_packages = import ./elm-packages.nix;
-  production = true;
 }
