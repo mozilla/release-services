821a02796112a6d70d90dbe61f38f8149aec0dd1
Rok Garbas <rok@garbas.si>
2017-03-07 02:10:00 +0100

    releng_frontend: elm packages updated

--- src/releng_frontend/elm-packages.nix
+++ src/releng_frontend/elm-packages.nix
@@ -1,50 +1,42 @@
 {
   "krisajenkins/remotedata" = {
-    version = "1.0.1";
-    sha256 = "1qdpyah75l2nmkh5yk0415i2d5la8mdgxz2j2g0pmp65liv3w1wa";
-  };
-  "etaque/elm-simple-form" = {
-    version = "5.0.1";
-    sha256 = "188b9bz91ivdkpyqlpmbnbgjz8v75r1llavw2l4fbj0ymdaky42h";
-  };
-  "elm-community/elm-test" = {
-    version = "1.1.0";
-    sha256 = "0rf0k1s3pvqk1hfpqrfbahlrpvbxzyyp5h6n8w6pcm4za1sql45p";
-  };
-  "sporto/hop" = {
-    version = "6.0.1";
-    sha256 = "00750jxdfki8llbcgi2xa2naf3wkhpiw9r9gbz45lmls7059d9sx";
+    version = "4.3.0";
+    sha256 = "0k07s964ahq0mfgvwl8yp93ziqfzz3r38h4xawbl44g7pfajlz3v";
   };
   "elm-lang/navigation" = {
-    version = "1.0.0";
-    sha256 = "013mndac19z01802rcwc0fbzfq3sf923m7sx065ak7viaiclnlay";
+    version = "2.1.0";
+    sha256 = "0xyamkcw3misjfyc523g0bh97lffrikaxk2hklwmla9i7j39ivf3";
   };
   "elm-lang/virtual-dom" = {
-    version = "1.1.1";
-    sha256 = "0mihz195qd8lilbdja0552k16jrdv2h66b7kx0waylhswpw6ml1b";
+    version = "2.0.4";
+    sha256 = "13kc3jim6js827h91m6wx6n16qb3b6p6ms6789ks106zpwmhfwxv";
   };
   "evancz/url-parser" = {
-    version = "1.0.0";
-    sha256 = "1grk47m7b93ahckmjbynrf7jplq9zd8b65p64b0jsyfda8a992gk";
+    version = "2.0.1";
+    sha256 = "0c9vxd7m11mmshlqi3s2arsr4jjkzxqcb907fvamxhaxvygkd4i7";
   };
-  "evancz/elm-http" = {
-    version = "3.0.1";
-    sha256 = "0bw8ppazywjljbdbph7319fnlcr8qnxdpd8556b50fzhy57a4bjq";
+  "etaque/elm-form" = {
+    version = "2.0.0";
+    sha256 = "0dbqjf0ddjhi48gjjc5wz5amzzvbk64dgwnq7g06pawp208bkcbw";
   };
   "elm-lang/dom" = {
-    version = "1.1.0";
-    sha256 = "1l8yn7g2ikpxc0qylzmv18hpiafgyip77nz886fjm77qpys7ibbw";
+    version = "1.1.1";
+    sha256 = "1y6nm3np10l34mhpc9c88vv7kc7qgaxim4cmagf4ig82rwi9mpl1";
   };
   "elm-lang/html" = {
-    version = "1.1.0";
-    sha256 = "1v7pwxxd81qrfywb4rr199p2i9z77vjkbwjwm5gy1nxdpi8mb50y";
+    version = "2.0.0";
+    sha256 = "05sqjd5n8jnq4lv5v0ipcg98b8im1isnnl4wns1zzn4w5nbrjjzi";
+  };
+  "elm-lang/http" = {
+    version = "1.0.0";
+    sha256 = "1fh9k278q4i2y447wkk8jd4n12px1nsmycd0fql8k0i3ypcibm8h";
   };
   "evancz/focus" = {
-    version = "2.0.1";
-    sha256 = "05qm4vx0pxa7s721sjm78kvrk69n4rcbg9vr77ii33jpa28xbkn3";
+    version = "2.0.2";
+    sha256 = "1680pbkx67cdk1fp7pdciiv00grq86kk15amf30pma9zmysp938p";
   };
   "elm-lang/core" = {
-    version = "4.0.5";
-    sha256 = "14ayra6w474qppg2xmbbyg4ai0p2l134k0vl22drhpqmay852060";
+    version = "5.1.1";
+    sha256 = "1ndw8ffn2la48gvs7xf3zrc5d7zqrq4j6scbrb60mb2ml1a5hhry";
   };
 }
