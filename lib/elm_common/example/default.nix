ae7421b086e5db0edec95a0c6834c9abbcf50c84
Rok Garbas <rok@garbas.si>
2017-03-09 04:29:58 +0100

    elm_common_example: initial webpack config

--- lib/elm_common/example/default.nix
+++ lib/elm_common/example/default.nix
@@ -4,11 +4,19 @@ let
   inherit (builtins) readFile;
   inherit (releng_pkgs.lib) mkFrontend;
   inherit (releng_pkgs.pkgs.lib) fileContents;
+
+  nodejs = releng_pkgs.pkgs."nodejs-6_x";
+  node_modules = import ./node-modules.nix {
+    inherit nodejs;
+    inherit (releng_pkgs) pkgs;
+  };
+
 in mkFrontend {
+  inherit nodejs node_modules;
   name = "elm_common_example";
   version = fileContents ./../../../VERSION;
   src = ./.;
   src_path = "lib/elm_common/example";
-  node_modules = import ./node-modules.nix { inherit (releng_pkgs) pkgs; };
   elm_packages = import ./elm-packages.nix;
 }
+#    "build": "rimraf dist && webpack && mv dist/*.eot dist/static/css/ && mv dist/*.woff* dist/static/css/ && mv dist/*.svg dist/static/css/ && mv dist/*.ttf dist/static/css/"
---
+++ lib/elm_common/example/src/BugzillaLogin.elm
@@ -0,0 +1,1 @@
+../../BugzillaLogin.elm
