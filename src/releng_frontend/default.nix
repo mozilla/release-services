0e26020732b75eba18d4b5c25b18a5a8d6bc1419
Rok Garbas <rok@garbas.si>
2017-03-09 13:50:37 +0100

    releng_frontend: updated node packages and webpack config added

--- src/releng_frontend/default.nix
+++ src/releng_frontend/default.nix
@@ -4,13 +4,20 @@ let
   inherit (builtins) readFile;
   inherit (releng_pkgs.lib) mkFrontend;
   inherit (releng_pkgs.pkgs.lib) fileContents;
+
+  nodejs = releng_pkgs.pkgs."nodejs-6_x";
+  node_modules = import ./node-modules.nix {
+    inherit nodejs;
+    inherit (releng_pkgs) pkgs;
+  };
+  elm_packages = import ./elm-packages.nix;
+
 in mkFrontend {
   inProduction = true;
-  name = "releng_frontend";
+  name = "mozilla-releng-frontend";
+  inherit nodejs node_modules elm_packages;
   version = fileContents ./../../VERSION;
   src = ./.;
-  node_modules = import ./node-modules.nix { inherit (releng_pkgs) pkgs; };
-  elm_packages = import ./elm-packages.nix;
   postInstall = ''
     cp -R src/static/* $out/
   '';
