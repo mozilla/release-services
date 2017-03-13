5e10122cab461a4606288a04308bfddb36a47852
Rok Garbas <rok@garbas.si>
2017-03-13 15:38:58 +0100

    setup: pin "nixos/nix" docker image and install cacert

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -194,7 +194,7 @@ in rec {
         workerType: "{{ taskcluster.docker.workerType }}"
         payload:
           maxRunTime: 7200 # seconds (i.e. two hours)
-          image: "nixos/nix:latest"
+          image: "nixos/nix:1.11"
           features:
             taskclusterProxy: true
           env:
@@ -203,7 +203,7 @@ in rec {
           command:
             - "/bin/bash"
             - "-c"
-            - "nix-env -iA nixpkgs.gnumake nixpkgs.curl && mkdir /src && cd /src && curl -L https://github.com/mozilla-releng/services/archive/$GITHUB_HEAD_SHA.tar.gz -o $GITHUB_HEAD_SHA.tar.gz && tar zxf $GITHUB_HEAD_SHA.tar.gz && cd services-$GITHUB_HEAD_SHA && ./.taskcluster.sh"
+            - "nix-env -iA nixpkgs.gnumake nixpkgs.curl nixpkgs.cacert && export SSL_CERT_FILE="$HOME/.nix-profile/etc/ssl/certs/ca-bundle.crt" && mkdir /src && cd /src && curl -L https://github.com/mozilla-releng/services/archive/$GITHUB_HEAD_SHA.tar.gz -o $GITHUB_HEAD_SHA.tar.gz && tar zxf $GITHUB_HEAD_SHA.tar.gz && cd services-$GITHUB_HEAD_SHA && ./.taskcluster.sh"
     '';
 
   fromRequirementsFile = files: pkgs':
