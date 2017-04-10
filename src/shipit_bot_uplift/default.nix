4ba723d37d451ce48c51b33b2c8f9501aa2190b2
Bastien Abadie <bastien@nextcairn.com>
2017-04-10 20:05:19 +0200

    shipit_bot_uplift: Use shared mercurial + robustcheckout.

--- src/shipit_bot_uplift/default.nix
+++ src/shipit_bot_uplift/default.nix
@@ -4,47 +4,14 @@
 let
 
   inherit (releng_pkgs.lib) mkTaskclusterHook mkPython fromRequirementsFile filterSource;
-  inherit (releng_pkgs.pkgs) writeScript makeWrapper mercurial cacert fetchurl;
+  inherit (releng_pkgs.pkgs) writeScript makeWrapper fetchurl;
   inherit (releng_pkgs.pkgs.stdenv) mkDerivation;
   inherit (releng_pkgs.pkgs.lib) fileContents optional licenses;
-  inherit (releng_pkgs.tools) pypi2nix;
+  inherit (releng_pkgs.tools) pypi2nix mercurial;
 
   python = import ./requirements.nix { inherit (releng_pkgs) pkgs; };
   name = "mozilla-shipit-bot-uplift";
   dirname = "shipit_bot_uplift";
-
-  robustcheckout = mkDerivation {
-    name = "robustcheckout";
-    src = fetchurl { 
-      url = "https://hg.mozilla.org/hgcustom/version-control-tools/archive/1a8415be17e8.tar.bz2";
-      sha256 = "005n7ar8cn7162s1qx970x1aabv263zp7mxm38byxc23nzym37kn";
-    };
-    installPhase = ''
-      mkdir -p $out
-      cp -rf hgext/robustcheckout $out
-    '';
-    doCheck = false;
-    buildInputs = [];
-    propagatedBuildInputs = [ ];
-    meta = {
-      homepage = "https://hg.mozilla.org/hgcustom/version-control-tools";
-      license = licenses.mit;
-      description = "Mozilla Version Control Tools: robustcheckout";
-    };
-  };
-
-  mercurial' = mercurial.overrideDerivation (old: {
-    postInstall = old.postInstall + ''
-      cat > $out/etc/mercurial/hgrc <<EOF
-[web]
-cacerts = ${cacert}/etc/ssl/certs/ca-bundle.crt
-
-[extensions]
-purge =
-robustcheckout = ${robustcheckout}/robustcheckout/__init__.py
-EOF
-    '';
-  });
 
   mkBot = branch:
     let
@@ -93,10 +60,10 @@ EOF
       fromRequirementsFile ./requirements.txt python.packages;
     postInstall = ''
       mkdir -p $out/bin
-      ln -s ${mercurial'}/bin/hg $out/bin
+      ln -s ${mercurial}/bin/hg $out/bin
     '';
 		shellHook = ''
-			export PATH="${mercurial'}/bin:$PATH"
+			export PATH="${mercurial}/bin:$PATH"
 		'';
     passthru = {
       taskclusterHooks = {
