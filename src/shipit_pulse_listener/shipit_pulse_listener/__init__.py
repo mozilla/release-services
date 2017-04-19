25671aa84236f6ce20cb8ede26be88d3eb2d8fb1
Bastien Abadie <bastien@nextcairn.com>
2017-04-19 17:43:05 +0200

    shipit_pulse_listener: Create a new Taskcluster Task set for every bugzilla message on pulse

---
+++ src/shipit_pulse_listener/shipit_pulse_listener/__init__.py
@@ -0,0 +1,5 @@
+from cli_common.log import init_logger
+
+
+# Setup logger for bot execution
+init_logger()
--- src/shipit_pulse_listener/shipit_pulse_listener/cli.py
+++ src/shipit_pulse_listener/shipit_pulse_listener/cli.py
@@ -1,12 +1,18 @@
 import click
+from shipit_pulse_listener.listener import PulseListener
 
 
 @click.command()
+@click.argument('hooks', nargs=-1)
 @click.option('--secrets', required=True, help='Taskcluster Secrets path')
 @click.option('--client-id', help='Taskcluster Client ID')
 @click.option('--client-token', help='Taskcluster Client token')
-def main(secrets, client_id, client_token):
-    pass
+def main(hooks, secrets, client_id, client_token):
+    if not hooks:
+        raise Exception('Specify at least one hook (hookGroupId:hookId)')
+
+    pl = PulseListener(secrets, client_id, client_token)
+    pl.run(hooks)
 
 
 if __name__ == '__main__':
