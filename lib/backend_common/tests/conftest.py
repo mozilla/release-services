1b9a796aa655848186750404000914088015d201
Simon Fraser <sfraser@mozilla.com>
2017-04-27 22:19:23 +0100

    Add auth0 accept_token support in common library
    Cleaned up tests for existing taskcluster auth, which included using jsonify and also preventing it trying to json-serialize a set()
    
    Added tests/client_secrets.json for oidc testing, rather than try and mock the different locations where open() uses it.

--- lib/backend_common/tests/conftest.py
+++ lib/backend_common/tests/conftest.py
@@ -1,7 +1,27 @@
+"""Configure a mock application to run queries against"""
+
 import pytest
 from flask_login import current_user
 from flask import jsonify
-from backend_common import create_app, auth, mocks
+from backend_common import create_app, auth, auth0, mocks
+from os.path import join, dirname
+
+FAKE_CLIENT_SECRETS = """
+{
+    "web": {
+        "auth_uri": "https://auth.mozilla.auth0.com/authorize",
+        "issuer": "https://auth.mozilla.auth0.com/",
+        "client_id": "some-id-string",
+        "client_secret": "my-super-secret",
+        "redirect_uris": [
+            "https://signoff.shipit.mozilla.com/oidc_callback"
+        ],
+        "token_uri": "https://auth.mozilla.auth0.com/oauth/token",
+        "token_introspection_uri": "https://test/oauth/token",
+        "userinfo_uri": "https://auth.mozilla.auth0.com/userinfo"
+    }
+}
+"""
 
 
 @pytest.fixture(scope='module')
@@ -13,30 +33,39 @@ def app():
     # Use unique auth instance
     config = {
         'DEBUG': True,
+        'OIDC_CLIENT_SECRETS': join(dirname(__file__), 'client_secrets.json'),
+        'OIDC_RESOURCE_SERVER_ONLY': True
     }
-    app = create_app('test', extensions=[auth], config=config)
+
+    app = create_app('test', extensions=[auth, auth0], config=config)
 
     @app.route('/')
     def index():
         return app.response_class('OK')
 
-    @app.route('/test-login')
+    @app.route('/test-auth-login')
     @auth.auth.require_login
     def logged_in():
         data = {
             'auth': True,
             'user': current_user.get_id(),
-            'scopes': current_user.permissions,
+            # permissions is a set, not serializable
+            'scopes': list(current_user.permissions),
         }
         return jsonify(data)
 
-    @app.route('/test-scopes')
+    @app.route('/test-auth-scopes')
     @auth.auth.require_scopes([
         ['project/test/A', 'project/test/B'],
         ['project/test-admin/*'],
     ])
     def scopes():
         return app.response_class('Your scopes are ok.')
+
+    @app.route('/test-auth0-userinfo')
+    @auth0.accept_token()
+    def auth0_token():
+        return app.response_class('OK')
 
     # Add fake swagger url, used by redirect
     app.api.swagger_url = '/'
