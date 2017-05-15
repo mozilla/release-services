6bac8962d4b1e1561a9b9b2cda01da660d7a2d0b
Bastien Abadie <bastien@nextcairn.com>
2017-05-15 15:28:31 +0200

    shipit_risk_assessment: Use REVISION env variable straight from python.

--- src/shipit_risk_assessment/shipit_risk_assessment/cli.py
+++ src/shipit_risk_assessment/shipit_risk_assessment/cli.py
@@ -4,7 +4,7 @@ from shipit_risk_assessment.bot import Bot
 
 @click.command()
 @click.argument('work_dir')
-@click.argument('merge_revision')
+@click.argument('merge_revision', envvar='REVISION')
 @click.option('--client-id', help='Taskcluster Client ID')
 @click.option('--client-token', help='Taskcluster Client token')
 def main(work_dir, merge_revision, client_id, client_token):
