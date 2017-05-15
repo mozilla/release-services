c6dd37f55745248cf6eca90cd4ebecdfa7df6d59
Simon Fraser <sfraser@mozilla.com>
2017-05-15 13:00:02 +0100

    Refactor taskcluster.sh and .yml
    Finding the command payload hard to read with the very long lines, and
    wondering whether they're all the same. After finding they are, have moved all
    the contents into the script that's run at the end.
    
    Have also updated .taskcluster.sh with increased bash safety.

--- .taskcluster.sh
+++ .taskcluster.sh
@@ -1,5 +1,14 @@
-#/bin/bash
+#!/bin/bash
 set -e
+
+nix-env -iA nixpkgs.gnumake nixpkgs.curl nixpkgs.cacert
+export SSL_CERT_FILE="$HOME/.nix-profile/etc/ssl/certs/ca-bundle.crt"
+mkdir -p /src 
+cd /src 
+curl -L "https://github.com/mozilla-releng/services/archive/${GITHUB_HEAD_SHA}.tar.gz" -o "${GITHUB_HEAD_SHA}.tar.gz"
+tar zxf "${GITHUB_HEAD_SHA}.tar.gz"
+cd "services-${GITHUB_HEAD_SHA}"
+
 
 if [[ -z "$APP" ]] ||
    [[ -z "$TASKCLUSTER_SECRETS" ]]; then
@@ -19,32 +28,32 @@ echo 'binary-caches = https://s3.amazonaws.com/releng-cache/ https://cache.nixos
 
 mkdir -p tmp
 rm -f tmp/taskcluster_secrets
-wget $TASKCLUSTER_SECRETS -O tmp/taskcluster_secrets
+wget "$TASKCLUSTER_SECRETS" -O tmp/taskcluster_secrets
 
-make build-pkgs-jq build-app APP=$APP
+make build-pkgs-jq build-app APP="$APP"
 
 if [[ "$GITHUB_BASE_BRANCH" = "staging" ]] ||
    [[ "$GITHUB_BASE_BRANCH" = "production" ]]; then
-    make deploy-$GITHUB_BASE_BRANCH-$APP \
-        APP=$APP \
-        AWS_ACCESS_KEY_ID=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.AWS_ACCESS_KEY_ID'` \
-        AWS_SECRET_ACCESS_KEY=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.AWS_SECRET_ACCESS_KEY'` \
-        HEROKU_USERNAME=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.HEROKU_USERNAME'` \
-        HEROKU_PASSWORD=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.HEROKU_PASSWORD'`
+    make "deploy-${GITHUB_BASE_BRANCH}-${APP}" \
+        APP="$APP" \
+        AWS_ACCESS_KEY_ID="$(./result-pkgs-jq/bin/jq -r '.secret.AWS_ACCESS_KEY_ID' < tmp/taskcluster_secrets)" \
+        AWS_SECRET_ACCESS_KEY="$(./result-pkgs-jq/bin/jq -r '.secret.AWS_SECRET_ACCESS_KEY' < tmp/taskcluster_secrets)" \
+        HEROKU_USERNAME="$(./result-pkgs-jq/bin/jq -r '.secret.HEROKU_USERNAME' < tmp/taskcluster_secrets)" \
+        HEROKU_PASSWORD="$(./result-pkgs-jq/bin/jq -r '.secret.HEROKU_PASSWORD' < tmp/taskcluster_secrets)"
 fi
 
 if [[ "$GITHUB_PULL_REQUEST" = "" ]]; then
     make taskcluster-hooks \
-        APP=$APP \
-        BRANCH=$GITHUB_BASE_BRANCH \
-        HOOKS_URL=http://`cat /etc/hosts | grep taskcluster | awk '{ print $1 }'`/hooks/v1 \
-        DOCKER_USERNAME=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.DOCKER_USERNAME'` \
-        DOCKER_PASSWORD=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.DOCKER_PASSWORD'`
+        APP="$APP" \
+        BRANCH="$GITHUB_BASE_BRANCH" \
+        HOOKS_URL="http://$(grep taskcluster /etc/hosts | awk '{ print $1 }')/hooks/v1" \
+        DOCKER_USERNAME="$(./result-pkgs-jq/bin/jq -r '.secret.DOCKER_USERNAME' < tmp/taskcluster_secrets)" \
+        DOCKER_PASSWORD="$(./result-pkgs-jq/bin/jq -r '.secret.DOCKER_PASSWORD' < tmp/taskcluster_secrets)"
 fi
 
 make deploy-cache \
-    CACHE_BUCKET=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.CACHE_BUCKET'` \
-    AWS_ACCESS_KEY_ID=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.AWS_ACCESS_KEY_ID'` \
-    AWS_SECRET_ACCESS_KEY=`cat tmp/taskcluster_secrets | ./result-pkgs-jq/bin/jq -r '.secret.AWS_SECRET_ACCESS_KEY'`
+    CACHE_BUCKET="$(./result-pkgs-jq/bin/jq -r '.secret.CACHE_BUCKET' < tmp/taskcluster_secrets)" \
+    AWS_ACCESS_KEY_ID="$(./result-pkgs-jq/bin/jq -r '.secret.AWS_ACCESS_KEY_ID' < tmp/taskcluster_secrets)" \
+    AWS_SECRET_ACCESS_KEY="$(./result-pkgs-jq/bin/jq -r '.secret.AWS_SECRET_ACCESS_KEY' < tmp/taskcluster_secrets)"
 
 echo "Success!"
