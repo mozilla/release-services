537ba8fe55524dc01a6ba3fa741fae77ce3dfd16
Jordan Lund <jlund@mozilla.com>
2017-05-23 12:51:33 -0700

    re-add backend postgres db

--- src/shipit_taskcluster/settings.py
+++ src/shipit_taskcluster/settings.py
@@ -12,14 +12,14 @@ SWAGGER_BASE_URL = os.environ.get('SWAGGER_BASE_URL')
 
 # -- DATABASE -----------------------------------------------------------------
 
-# DATABASE_URL = os.environ.get('DATABASE_URL')
-#
-# if not DATABASE_URL:
-#     raise Exception("You need to specify DATABASE_URL variable.")
-#
-# if not DATABASE_URL.startswith('postgresql://'):
-#     raise Exception('Shipit dashboard needs a postgresql:// DATABASE_URL')
-#
-#
-# SQLALCHEMY_DATABASE_URI = DATABASE_URL
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if not DATABASE_URL:
+    raise Exception("You need to specify DATABASE_URL variable.")
+
+if not DATABASE_URL.startswith('postgresql://'):
+    raise Exception('Shipit dashboard needs a postgresql:// DATABASE_URL')
+
+
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
+SQLALCHEMY_TRACK_MODIFICATIONS = False
