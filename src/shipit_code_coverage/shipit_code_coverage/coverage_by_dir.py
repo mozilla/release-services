7c452df6e71ce896d9615bdf3145a16d1d7b52f2
Marco <mcastelluccio@mozilla.com>
2017-06-30 14:29:31 -0700

    shipit_code_coverage: Support using codecov.io to generate coverage-by-dir report (#467)

--- src/shipit_code_coverage/shipit_code_coverage/coverage_by_dir.py
+++ src/shipit_code_coverage/shipit_code_coverage/coverage_by_dir.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from enum import Enum
 import fnmatch
 import json
 import os
@@ -7,6 +8,12 @@ import requests
 from libmozdata.bugzilla import Bugzilla
 
 
+class CoverageService(Enum):
+    COVERALLS = 1
+    CODECOV = 2
+
+
+COVERAGE_SERVICE = CoverageService.CODECOV
 MAX_LEVEL = 3
 
 
@@ -47,17 +54,49 @@ def get_related_bugs(changesets, directory):
 
 
 def get_coverage_builds():
-    r = requests.get('https://coveralls.io/github/marco-c/gecko-dev.json?page=1')
-    return r.json()['builds']
+    if COVERAGE_SERVICE == CoverageService.COVERALLS:
+        r = requests.get('https://coveralls.io/github/marco-c/gecko-dev.json?page=1')
+        builds = r.json()['builds']
+
+        latest_commit = builds[0]['commit_sha']
+        previous_commit = builds[1]['commit_sha']
+    elif COVERAGE_SERVICE == CoverageService.CODECOV:
+        r = requests.get('https://codecov.io/api/gh/marco-c/gecko-dev')
+        commits = r.json()['commits']
+
+        latest_commit = commits[0]['commitid']
+        previous_commit = commits[1]['commitid']
+
+    return (latest_commit, previous_commit)
 
 
 def get_coverage(commit_sha, directory):
-    r = requests.get('https://coveralls.io/builds/' + commit_sha + '.json?paths=' + directory + '/*')
+    if COVERAGE_SERVICE == CoverageService.COVERALLS:
+        r = requests.get('https://coveralls.io/builds/' + commit_sha + '.json?paths=' + directory + '/*')
 
-    if r.status_code != requests.codes.ok:
-        raise Exception('Error while loading coverage data.')
+        if r.status_code != requests.codes.ok:
+            raise Exception('Error while loading coverage data.')
 
-    return r.json()
+        result = r.json()
+        files_num = result['selected_source_files_count']
+        cur = result['paths_covered_percent']
+        prev = result['paths_previous_covered_percent']
+    elif COVERAGE_SERVICE == CoverageService.CODECOV:
+        r = requests.get('https://codecov.io/api/gh/marco-c/gecko-dev/tree/' + commit_sha + '/' + directory)
+
+        if r.status_code != requests.codes.ok:
+            raise Exception('Error while loading coverage data.')
+
+        result = r.json()
+        files_num = result['commit']['folder_totals']['files']
+        cur = result['commit']['totals']['c']
+        prev = result['commit']['parent_totals']['c']
+
+    return {
+      'files_num': files_num,
+      'cur': cur,
+      'prev': prev,
+    }
 
 
 def get_directories(directory, rootDir, curLevel=0):
@@ -74,10 +113,8 @@ def get_directories(directory, rootDir, curLevel=0):
 
 
 def generate(rootDir):
-    builds = get_coverage_builds()
+    latest_commit, previous_commit = get_coverage_builds()
 
-    latest_commit = builds[0]['commit_sha']
-    previous_commit = builds[1]['commit_sha']
     latest_mercurial_commit = get_mercurial_commit(latest_commit)
     previous_mercurial_commit = get_mercurial_commit(previous_commit)
 
@@ -91,12 +128,12 @@ def generate(rootDir):
     for directory in directories:
         d = get_coverage(latest_commit, directory)
 
-        if d['selected_source_files_count'] == 0:
+        if d['files_num'] == 0:
             continue
 
         data[directory] = {}
-        data[directory]['cur'] = d['paths_covered_percent']
-        data[directory]['prev'] = d['paths_previous_covered_percent']
+        data[directory]['cur'] = d['cur']
+        data[directory]['prev'] = d['prev']
 
         # Add bugs with structure for libmozdata updates
         directory_bugs = get_related_bugs(changesets, directory)
