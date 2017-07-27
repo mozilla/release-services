adb546836b6a070a0d9fa4fe3d6bb60be3b3337b
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-07-27 21:10:23 +0200

    shipit_pulse_listener: Use deadline defined in the hook

---
+++ src/shipit_pulse_listener/tests/test_hook.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from shipit_pulse_listener.hook import Hook
+from datetime import timedelta
+
+
+def test_parse_deadline():
+    hook = Hook('gid', 'hid', 'q', 'r')
+    assert hook.parse_deadline('1 second') == timedelta(seconds=1)
+    assert hook.parse_deadline('2 seconds') == timedelta(seconds=2)
+    assert hook.parse_deadline('1 minute') == timedelta(minutes=1)
+    assert hook.parse_deadline('2 minutes') == timedelta(minutes=2)
+    assert hook.parse_deadline('1 hour') == timedelta(minutes=60)
+    assert hook.parse_deadline('2 hours') == timedelta(minutes=120)
+    assert hook.parse_deadline('1 day') == timedelta(days=1)
+    assert hook.parse_deadline('2 days') == timedelta(days=2)
