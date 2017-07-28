faf50dfe6084f97e3f620f993396c3a61f2815b7
Marco <mcastelluccio@mozilla.com>
2017-07-28 07:31:55 +0200

    shipit_pulse_listener: Use deadline defined in the hook (#532)

--- src/shipit_pulse_listener/shipit_pulse_listener/hook.py
+++ src/shipit_pulse_listener/shipit_pulse_listener/hook.py
@@ -32,7 +32,7 @@ class Hook(object):
         try:
             service = get_service('hooks', client_id, access_token)
             hook_payload = service.hook(self.group_id, self.hook_id)
-            self.task_definition = hook_payload['task']
+            self.hook_definition = hook_payload
         except Exception as e:
             logger.warn('Failed to fetch task definition', hook=self.hook_id, group=self.group_id, err=e)  # noqa
             return False
@@ -83,14 +83,31 @@ class Hook(object):
         '''
         raise NotImplementedError
 
-    async def create_task(self, ttl=5, extra_env={}):
+    def parse_deadline(self, deadline):
+        parts = deadline.split(' ')
+
+        num = int(parts[0])
+        unit = parts[1]
+
+        if unit.startswith('second'):
+            return timedelta(seconds=num)
+        elif unit.startswith('minute'):
+            return timedelta(minutes=num)
+        elif unit.startswith('hour'):
+            return timedelta(minutes=num * 60)
+        elif unit.startswith('day'):
+            return timedelta(days=num)
+        else:
+            raise Exception('Error while parsing: ' % deadline)
+
+    async def create_task(self, extra_env={}):
         '''
         Create a new task on Taskcluster
         '''
         assert self.queue is not None
 
         # Update the env in task
-        task_definition = copy.deepcopy(self.task_definition)
+        task_definition = copy.deepcopy(self.hook_definition['task'])
         task_definition['payload']['env'].update(extra_env)
 
         # Build task id
@@ -99,7 +116,7 @@ class Hook(object):
         # Set dates
         now = datetime.utcnow()
         task_definition['created'] = now
-        task_definition['deadline'] = now + timedelta(seconds=ttl * 3600)
+        task_definition['deadline'] = now + self.parse_deadline(self.hook_definition['deadline'])
         logger.info('Creating a new task', id=task_id, name=task_definition['metadata']['name'])  # noqa
 
         # Create a new task
