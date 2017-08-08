e49f96e55f802d81114c6e93b034afa0d386adab
Bastien Abadie <bastien@nextcairn.com>
2017-08-08 08:54:05 +0200

    shipit_static_analysis: Use MOZCONFIG from gecko-env, on a per project basis.

--- src/shipit_static_analysis/shipit_static_analysis/workflow.py
+++ src/shipit_static_analysis/shipit_static_analysis/workflow.py
@@ -34,6 +34,8 @@ class Workflow(object):
         self.cache_root = cache_root
         assert os.path.isdir(self.cache_root), \
             'Cache root {} is not a dir.'.format(self.cache_root)
+        assert 'MOZCONFIG' in os.environ, \
+            'Missing MOZCONFIG in environment'
 
         # Load TC services & secrets
         self.notify = get_service(
@@ -93,12 +95,10 @@ class Workflow(object):
             modified_files += [f.decode('utf-8') for _, f in status]
         logger.info('Modified files', files=modified_files)
 
-        env = os.environ.copy()
-        env['MOZCONFIG'] = env['CLANG_MOZCONFIG']
 
         # mach configure with mozconfig
         logger.info('Mach configure...')
-        run_check(['gecko-env', './mach', 'configure'], cwd=self.repo_dir, env=env)
+        run_check(['gecko-env', './mach', 'configure'], cwd=self.repo_dir)
 
         # Build CompileDB backend
         logger.info('Mach build backend...')
@@ -107,8 +107,8 @@ class Workflow(object):
 
         # Build exports
         logger.info('Mach build exports...')
-        run_check(['gecko-env', './mach', 'build', 'pre-export'], cwd=self.repo_dir, env=env)
-        run_check(['gecko-env', './mach', 'build', 'export'], cwd=self.repo_dir, env=env)
+        run_check(['gecko-env', './mach', 'build', 'pre-export'], cwd=self.repo_dir)
+        run_check(['gecko-env', './mach', 'build', 'export'], cwd=self.repo_dir)
 
         # Run static analysis through run-clang-tidy.py
         logger.info('Run clang-tidy...')
