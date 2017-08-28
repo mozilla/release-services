4b8f53d7883881afcf67fe0e7cd0580bd56d183d
Connor Sheehan <csheehan@mozilla.com>
2017-08-28 04:34:36 -0400

    releng_clobberer: Migrate releng_clobberer from relengapi (#572)

--- lib/backend_common/backend_common/db.py
+++ lib/backend_common/backend_common/db.py
@@ -17,6 +17,61 @@ db = flask_sqlalchemy.SQLAlchemy()
 migrate = flask_migrate.Migrate(db=db)
 
 
+def _unique(session, cls, hashfunc, queryfunc, constructor, arg, kw, _test_hook=None):
+    # Based on
+    # https://bitbucket.org/zzzeek/sqlalchemy/wiki/UsageRecipes/UniqueObject
+    cache = session.info.get('_unique_cache', None)
+    if cache is None:
+        session.info['_unique_cache'] = cache = {}
+
+        # Setup to clear session cache on rollback
+        @db.event.listens_for(session, 'after_rollback', once=True)
+        def _clear_session_cache(s):
+            if s.info.get('_unique_cache', None):
+                del s.info['_unique_cache']
+
+    key = (cls, hashfunc(*arg, **kw))
+    if key in cache:
+        return cache[key]
+    else:
+        with session.no_autoflush:
+            q = session.query(cls)
+            q = queryfunc(q, *arg, **kw)
+            obj = q.first()
+            if _test_hook:
+                _test_hook()
+            if not obj:
+                obj = constructor(*arg, **kw)
+                session.add(obj)
+                session.flush()
+        cache[key] = obj
+        return obj
+
+
+class UniqueMixin(object):
+    # Based on
+    # https://bitbucket.org/zzzeek/sqlalchemy/wiki/UsageRecipes/UniqueObject
+
+    @classmethod
+    def unique_filter(cls, query, *arg, **kw):
+        raise NotImplementedError()
+
+    @classmethod
+    def unique_hash(cls, *arg, **kw):
+        raise NotImplementedError()
+
+    @classmethod
+    def as_unique(cls, session, *arg, **kw):
+        return _unique(
+            session,
+            cls,
+            cls.unique_hash,
+            cls.unique_filter,
+            cls,
+            arg, kw
+        )
+
+
 def init_database(app):
     '''
     Run Migrations through Alembic
--- src/releng_clobberer/releng_clobberer/models.py
+++ src/releng_clobberer/releng_clobberer/models.py
@@ -5,32 +5,33 @@
 
 from __future__ import absolute_import
 
-import re
-import sqlalchemy as sa
 import time
 
-from backend_common.db import db
+import sqlalchemy as sa
+
+from backend_common.db import UniqueMixin, db
+
+from .config import PROJECT_PATH_NAME
 
 
-BUILDBOT_BUILDDIR_REL_PREFIX = 'rel-'
-BUILDBOT_BUILDER_REL_PREFIX = 'release-'
-
-
-class Build(db.Model):
-    '''
-    A clobberable builds.
-    '''
-
-    __tablename__ = 'releng_clobberer_builds'
+class ClobbererBase(db.Model):
+    __abstract__ = True
 
     id = sa.Column(sa.Integer, primary_key=True)
     branch = sa.Column(sa.String(50), index=True)
     builddir = sa.Column(sa.String(100), index=True)
+
+
+class Build(ClobbererBase, UniqueMixin):
+    '''A clobberable build.'''
+
+    __tablename__ = PROJECT_PATH_NAME + '_builds'
+
     buildername = sa.Column(sa.String(100))
     last_build_time = sa.Column(
         sa.Integer,
         nullable=False,
-        default=lambda: int(time.time())
+        default=int(time.time())
     )
 
     @classmethod
@@ -38,8 +39,7 @@ class Build(db.Model):
         return '{}:{}:{}'.format(branch, builddir, buildername)
 
     @classmethod
-    def unique_filter(cls, query, branch, builddir, buildername,
-                      *args, **kwargs):
+    def unique_filter(cls, query, branch, builddir, buildername, *args, **kwargs):
         return query.filter(
             cls.branch == branch,
             cls.builddir == builddir,
@@ -47,24 +47,16 @@ class Build(db.Model):
         )
 
 
-class ClobberTime(db.Model):
+class ClobberTime(ClobbererBase, UniqueMixin):
+    '''A clobber request.'''
 
-    __tablename__ = 'releng_clobberer_times'
+    __tablename__ = PROJECT_PATH_NAME + '_times'
     __table_args__ = (
         # Index to speed up lastclobber lookups
-        sa.Index('ix_get_clobberer_times', 'slave', 'builddir', 'branch'),
+        sa.Index('ix_get_clobber_times', 'slave', 'builddir', 'branch'),
     )
-
-    id = sa.Column(sa.Integer, primary_key=True)
-    branch = sa.Column(sa.String(50), index=True)
     slave = sa.Column(sa.String(30), index=True)
-    builddir = sa.Column(sa.String(100), index=True)
-    lastclobber = sa.Column(
-        sa.Integer,
-        nullable=False,
-        default=lambda: int(time.time()),
-        index=True,
-    )
+    lastclobber = sa.Column(sa.Integer, nullable=False, default=int(time.time()), index=True)
     who = sa.Column(sa.String(50))
 
     @classmethod
@@ -78,122 +70,3 @@ class ClobberTime(db.Model):
             cls.slave == slave,
             cls.builddir == builddir,
         )
-
-
-def buildbot_branches(db_session):
-    '''List of all buildbot branches.
-    '''
-
-    branches = db_session.query(
-        Build.branch
-    ).filter(
-        # Users shouldn't see any branch associated with a release builddir
-        sa.not_(
-            Build.builddir.startswith(BUILDBOT_BUILDDIR_REL_PREFIX),
-        )
-    ).order_by(
-        Build.branch
-    ).distinct()
-
-    return [
-        dict(
-            name=branch[0] or '',
-            builders=[
-                dict(
-                    name=builder[0] or '',
-                    branch=builder[1] or '',
-                    slave=builder[2] or '',
-                    builddir=builder[3] or '',
-                    lastclobber=builder[4] or -1,
-                    who=builder[5] or '',
-                )
-                for builder in buildbot_branch_builders(db_session, branch[0])
-                if all([builder[1], builder[2], builder[3]])
-            ],
-        )
-        for branch in branches
-    ]
-
-
-def buildbot_branch_builders(db_session, branch):
-    '''Return a dictionary of most recent ClobberTime grouped by
-       buildername.
-    '''
-    # Isolates the maximum lastclobber for each builddir on a branch
-    max_ct_sub_query = db_session.query(
-        sa.func.max(ClobberTime.lastclobber).label('lastclobber'),
-        ClobberTime.branch,
-        ClobberTime.builddir,
-    ).group_by(
-        ClobberTime.branch,
-        ClobberTime.builddir,
-    ).filter(
-        ClobberTime.branch == branch
-    ).subquery()
-
-    # Finds the 'greatest n per group' by joining with the
-    # max_ct_sub_query
-    # This is necessary to get the correct 'who' values
-    sub_query = db_session.query(
-        ClobberTime
-    ).join(
-        max_ct_sub_query,
-        sa.and_(
-            ClobberTime.builddir == max_ct_sub_query.c.builddir,
-            ClobberTime.lastclobber == max_ct_sub_query.c.lastclobber,
-            ClobberTime.branch == max_ct_sub_query.c.branch,
-        ),
-    ).subquery()
-
-    # Attaches builddir, along with their max lastclobber to a
-    # buildername
-    return db_session.query(
-        Build.buildername,
-        sub_query.c.branch,
-        sub_query.c.slave,
-        sub_query.c.builddir,
-        sub_query.c.lastclobber,
-        sub_query.c.who
-    ).outerjoin(
-        sub_query,
-        Build.builddir == sub_query.c.builddir,
-    ).filter(
-        Build.branch == branch,
-        sa.not_(Build.buildername.startswith(BUILDBOT_BUILDER_REL_PREFIX))
-    ).order_by(
-        Build.buildername
-    ).distinct()
-
-
-def clobber_buildbot(db_session, who, branch=None, slave=None, builddir=None,
-                     log=None):
-    ''' TODO:
-    '''
-
-    builder = ClobberTime.unique_hash(branch, slave, builddir)
-
-    match = re.search('^' + BUILDBOT_BUILDDIR_REL_PREFIX + '.*', builddir)
-    if match is None:
-
-        if log:
-            log.debug('Clobbering builder: {}'.format(builder))
-
-        clobberer_time = ClobberTime.as_unique(
-            db_session,
-            branch=branch,
-            slave=slave,
-            builddir=builddir,
-        )
-        clobberer_time.lastclobber = int(time.time())
-        clobberer_time.who = who
-
-        db_session.add(clobberer_time)
-        db_session.commit()
-
-        if log:
-            log.debug('Clobbered builder: {}'.format(builder))
-
-        return clobberer_time
-
-    if log:
-        log.debug('Skipping clobbering of builder: {}'.format(builder))
