2c246c7fed2ed49b28ec972dbad7a8b0f64ea04b
Marco <mcastelluccio@mozilla.com>
2017-10-09 17:07:01 +0200

    shipit_code_coverage: Generate static per-test-suite reports weekly (#622)

--- src/shipit_code_coverage/shipit_code_coverage/codecov.py
+++ src/shipit_code_coverage/shipit_code_coverage/codecov.py
@@ -21,7 +21,9 @@ class CodeCov(object):
     def __init__(self, revision, cache_root, coveralls_token, codecov_token, gecko_dev_user, gecko_dev_pwd):
         # List of test-suite, sorted alphabetically.
         # This way, the index of a suite in the array should be stable enough.
-        self.suites = []
+        self.suites = [
+            'cppunit', 'gtest', 'web-platform-tests',
+        ]
 
         self.cache_root = cache_root
 
@@ -38,9 +40,11 @@ class CodeCov(object):
 
             task_data = taskcluster.get_task_details(self.task_id)
             self.revision = task_data['payload']['env']['GECKO_HEAD_REV']
+            self.from_pulse = False
         else:
             self.task_id = taskcluster.get_task('mozilla-central', revision)
             self.revision = revision
+            self.from_pulse = True
 
         self.build_finished = False
         self.build_finished_cv = Condition()
@@ -56,8 +60,6 @@ class CodeCov(object):
 
         task_data = taskcluster.get_task_details(build_task_id)
 
-        all_suites = set()
-
         def rewriting_task(path):
             return lambda: self.rewrite_jsvm_lcov(path)
 
@@ -70,8 +72,6 @@ class CodeCov(object):
                 if any(to_ignore in suite_name for to_ignore in ['awsy', 'talos']):
                     continue
 
-                all_suites.add(suite_name)
-
                 test_task_id = test_task['status']['taskId']
                 for artifact in taskcluster.get_task_artifacts(test_task_id):
                     if not any(n in artifact['name'] for n in ['code-coverage-grcov.zip', 'code-coverage-jsvm.zip']):
@@ -80,9 +80,6 @@ class CodeCov(object):
                     artifact_path = taskcluster.download_artifact(test_task_id, suite_name, artifact)
                     if 'code-coverage-jsvm.zip' in artifact['name']:
                         executor.submit(rewriting_task(artifact_path))
-
-            self.suites = list(all_suites)
-            self.suites.sort()
 
             logger.info('Code coverage artifacts downloaded')
 
@@ -133,7 +130,7 @@ class CodeCov(object):
         for lcov_out_file in lcov_out_files:
             os.rename(lcov_out_file, lcov_out_file[:-4])
 
-    def generate_info(self, commit_sha, coveralls_token, suite=None):
+    def generate_info(self, commit_sha, coveralls_token=None, suite=None, out_format='coveralls'):
         files = os.listdir('ccov-artifacts')
         ordered_files = []
         for fname in files:
@@ -145,30 +142,49 @@ class CodeCov(object):
             if suite is None or suite in fname:
                 ordered_files.append('ccov-artifacts/' + fname)
 
-        r = requests.get('https://hg.mozilla.org/mozilla-central/json-rev/%s' % self.revision)
-        push_id = r.json()['pushid']
-
         cmd = [
           'grcov',
-          '-t', 'coveralls',
+          '-t', out_format,
           '-s', self.repo_dir,
           '-p', '/home/worker/workspace/build/src/',
           '--ignore-dir', 'gcc',
           '--ignore-not-existing',
-          '--service-name', 'TaskCluster',
-          '--service-number', str(push_id),
-          '--commit-sha', commit_sha,
-          '--token', coveralls_token,
         ]
 
-        if suite is not None:
-            cmd.extend(['--service-job-number', str(self.suites.index(suite) + 1)])
-        else:
-            cmd.extend(['--service-job-number', '1'])
+        if out_format == 'coveralls':
+            r = requests.get('https://hg.mozilla.org/mozilla-central/json-rev/%s' % self.revision)
+            r.raise_for_status()
+            push_id = r.json()['pushid']
+
+            cmd.extend([
+              '--service-name', 'TaskCluster',
+              '--service-number', str(push_id),
+              '--commit-sha', commit_sha,
+              '--token', coveralls_token,
+            ])
+
+            if suite is not None:
+                cmd.extend(['--service-job-number', str(self.suites.index(suite) + 1)])
+            else:
+                cmd.extend(['--service-job-number', '1'])
 
         cmd.extend(ordered_files)
 
-        return run_check(cmd, cwd=os.getcwd())
+        return run_check(cmd)
+
+    def generate_report(self, output, suite):
+        info_file = '%s.info' % suite
+
+        with open(info_file, 'wb') as f:
+            f.write(output)
+
+        run_check([
+            'genhtml',
+            '-o', os.path.join(os.getcwd(), suite),
+            '--show-details', '--highlight', '--ignore-errors', 'source',
+            '--legend', os.path.join(os.getcwd(), info_file),
+            '--prefix', self.repo_dir
+        ], cwd=self.repo_dir)
 
     def clone_mozilla_central(self, revision):
         shared_dir = self.repo_dir + '-shared'
@@ -206,39 +222,7 @@ class CodeCov(object):
         with self.build_finished_cv:
             self.build_finished_cv.notify_all()
 
-    def go(self):
-        with ThreadPoolExecutorResult(max_workers=2) as executor:
-            # Thread 1 - Download coverage artifacts.
-            executor.submit(lambda: self.download_coverage_artifacts(self.task_id))
-
-            # Thread 2 - Clone and build mozilla-central
-            clone_future = executor.submit(lambda: self.clone_mozilla_central(self.revision))
-            clone_future.add_done_callback(lambda f: self.build_files())
-
-        if self.gecko_dev_user is not None and self.gecko_dev_pwd is not None:
-            self.update_github_repo()
-
-        commit_sha = self.get_github_commit(self.revision)
-        logger.info('GitHub revision', revision=commit_sha)
-
-        # TODO: Process suites in parallel.
-        # While we are uploading results for a suite, we can start to process the next one.
-        # TODO: Reenable when Coveralls and/or Codecov will be able to properly handle the load.
-        '''for suite in self.suites:
-            output = self.generate_info(commit_sha, self.coveralls_token, suite)
-
-            logger.info('Suite report generated', suite=suite)
-
-            uploader.coveralls(output)
-            uploader.codecov(output, commit_sha, self.codecov_token, [suite.replace('-', '_')])'''
-
-        output = self.generate_info(commit_sha, self.coveralls_token)
-        logger.info('Report generated successfully')
-
-        with ThreadPoolExecutorResult(max_workers=2) as executor:
-            executor.submit(lambda: uploader.coveralls(output))
-            executor.submit(lambda: uploader.codecov(output, commit_sha, self.codecov_token))
-
+    def prepopulate_cache(self, commit_sha):
         try:
             logger.info('Waiting for build to be ingested by Codecov...')
             # Wait until the build has been ingested by Codecov.
@@ -251,6 +235,7 @@ class CodeCov(object):
             # Get pushlog and ask the backend to generate the coverage by changeset
             # data, which will be cached.
             r = requests.get('https://hg.mozilla.org/mozilla-central/json-pushes?changeset=%s&version=2' % self.revision)
+            r.raise_for_status()
             data = r.json()
             changesets = data['pushes'][data['lastpushid']]['changesets']
 
@@ -258,3 +243,60 @@ class CodeCov(object):
                 requests.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/changeset/%s' % changeset)
         except Exception as e:
             logger.warn('Error while requesting coverage data: ' + str(e))
+
+    def go(self):
+        with ThreadPoolExecutorResult(max_workers=2) as executor:
+            # Thread 1 - Download coverage artifacts.
+            executor.submit(lambda: self.download_coverage_artifacts(self.task_id))
+
+            # Thread 2 - Clone and build mozilla-central
+            clone_future = executor.submit(lambda: self.clone_mozilla_central(self.revision))
+            clone_future.add_done_callback(lambda f: self.build_files())
+
+        if self.from_pulse:
+            if self.gecko_dev_user is not None and self.gecko_dev_pwd is not None:
+                self.update_github_repo()
+
+            commit_sha = self.get_github_commit(self.revision)
+            logger.info('GitHub revision', revision=commit_sha)
+
+            output = self.generate_info(commit_sha, self.coveralls_token)
+            logger.info('Report generated successfully')
+
+            with ThreadPoolExecutorResult(max_workers=2) as executor:
+                executor.submit(lambda: uploader.coveralls(output))
+                executor.submit(lambda: uploader.codecov(output, commit_sha, self.codecov_token))
+
+            self.prepopulate_cache(commit_sha)
+        else:
+            try:
+                os.mkdir('code-coverage-reports')
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise e
+
+            def generate_suite_report(suite):
+                output = self.generate_info(self.revision, suite=suite, out_format='lcov')
+
+                self.generate_report(output, suite)
+
+                run_check(['tar', '-cjf', 'code-coverage-reports/%s.tar.bz2' % suite, suite])
+
+                logger.info('Suite report generated', suite=suite)
+
+            def generate_suite_report_task(suite):
+                return lambda: generate_suite_report(suite)
+
+            for suite in self.suites:
+                with ThreadPoolExecutorResult() as executor:
+                    executor.submit(generate_suite_report_task(suite))
+
+            os.chdir('code-coverage-reports')
+            run_check(['git', 'config', '--global', 'http.postBuffer', '12M'])
+            run_check(['git', 'config', '--global', 'user.email', 'report@upload.it'])
+            run_check(['git', 'config', '--global', 'user.name', 'Report Uploader'])
+            repo_url = 'https://%s:%s@github.com/marco-c/code-coverage-reports' % (self.gecko_dev_user, self.gecko_dev_pwd)
+            run_check(['git', 'init'])
+            run_check(['git', 'add', '*'])
+            run_check(['git', 'commit', '-m', 'Coverage reports upload'])
+            retry(lambda: run_check(['git', 'push', repo_url, 'master', '--force']))
