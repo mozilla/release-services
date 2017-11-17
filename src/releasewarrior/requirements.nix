cd5f2df257bd2f1e8d69bd0d7ef8b430722fa0b6
Rok Garbas <rok@garbas.si>
2017-11-17 13:21:47 +0100

    releasewarrior: nixify and  hook with please_cli

---
+++ src/releasewarrior/requirements.nix
@@ -0,0 +1,284 @@
+# generated using pypi2nix tool (version: 1.8.1)
+# See more at: https://github.com/garbas/pypi2nix
+#
+# COMMAND:
+#   pypi2nix -v -V 3.5 -r requirements.txt -r requirements-dev.txt
+#
+
+{ pkgs ? import <nixpkgs> {}
+}:
+
+let
+
+  inherit (pkgs) makeWrapper;
+  inherit (pkgs.stdenv.lib) fix' extends inNixShell;
+
+  pythonPackages =
+  import "${toString pkgs.path}/pkgs/top-level/python-packages.nix" {
+    inherit pkgs;
+    inherit (pkgs) stdenv;
+    python = pkgs.python35;
+    # patching pip so it does not try to remove files when running nix-shell
+    overrides =
+      self: super: {
+        bootstrapped-pip = super.bootstrapped-pip.overrideDerivation (old: {
+          patchPhase = old.patchPhase + ''
+            sed -i               -e "s|paths_to_remove.remove(auto_confirm)|#paths_to_remove.remove(auto_confirm)|"                -e "s|self.uninstalled = paths_to_remove|#self.uninstalled = paths_to_remove|"                  $out/${pkgs.python35.sitePackages}/pip/req/req_install.py
+          '';
+        });
+      };
+  };
+
+  commonBuildInputs = [];
+  commonDoCheck = false;
+
+  withPackages = pkgs':
+    let
+      pkgs = builtins.removeAttrs pkgs' ["__unfix__"];
+      interpreter = pythonPackages.buildPythonPackage {
+        name = "python35-interpreter";
+        buildInputs = [ makeWrapper ] ++ (builtins.attrValues pkgs);
+        buildCommand = ''
+          mkdir -p $out/bin
+          ln -s ${pythonPackages.python.interpreter}               $out/bin/${pythonPackages.python.executable}
+          for dep in ${builtins.concatStringsSep " "               (builtins.attrValues pkgs)}; do
+            if [ -d "$dep/bin" ]; then
+              for prog in "$dep/bin/"*; do
+                if [ -f $prog ]; then
+                  ln -s $prog $out/bin/`basename $prog`
+                fi
+              done
+            fi
+          done
+          for prog in "$out/bin/"*; do
+            wrapProgram "$prog" --prefix PYTHONPATH : "$PYTHONPATH"
+          done
+          pushd $out/bin
+          ln -s ${pythonPackages.python.executable} python
+          ln -s ${pythonPackages.python.executable}               python3
+          popd
+        '';
+        passthru.interpreter = pythonPackages.python;
+      };
+    in {
+      __old = pythonPackages;
+      inherit interpreter;
+      mkDerivation = pythonPackages.buildPythonPackage;
+      packages = pkgs;
+      overrideDerivation = drv: f:
+        pythonPackages.buildPythonPackage (drv.drvAttrs // f drv.drvAttrs //                                            { meta = drv.meta; });
+      withPackages = pkgs'':
+        withPackages (pkgs // pkgs'');
+    };
+
+  python = withPackages {};
+
+  generated = self: {
+
+    "GitPython" = python.mkDerivation {
+      name = "GitPython-2.1.7";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/e1/ec/9f2bbc86a0188bf83221212be87fa4b70dc71a9385efcc95a25e9789894e/GitPython-2.1.7.tar.gz"; sha256 = "13c7cd99c2bf277fc99accfc25148752fa90e7cc6c6d08a3f01d229dacb461d9"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."gitdb2"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "https://github.com/gitpython-developers/GitPython";
+        license = licenses.bsdOriginal;
+        description = "Python Git Library";
+      };
+    };
+
+
+
+    "Jinja2" = python.mkDerivation {
+      name = "Jinja2-2.10";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/56/e6/332789f295cf22308386cf5bbd1f4e00ed11484299c5d7383378cf48ba47/Jinja2-2.10.tar.gz"; sha256 = "f84be1bb0040caca4cea721fcbbbbd61f9be9464ca236387158b0feea01914a4"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."MarkupSafe"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "http://jinja.pocoo.org/";
+        license = licenses.bsdOriginal;
+        description = "A small but fast and easy to use stand-alone template engine written in pure python.";
+      };
+    };
+
+
+
+    "MarkupSafe" = python.mkDerivation {
+      name = "MarkupSafe-1.0";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/4d/de/32d741db316d8fdb7680822dd37001ef7a448255de9699ab4bfcbdf4172b/MarkupSafe-1.0.tar.gz"; sha256 = "a6be69091dac236ea9c6bc7d012beab42010fa914c459791d627dad4910eb665"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "http://github.com/pallets/markupsafe";
+        license = licenses.bsdOriginal;
+        description = "Implements a XML/HTML/XHTML Markup safe string for Python";
+      };
+    };
+
+
+
+    "PyYAML" = python.mkDerivation {
+      name = "PyYAML-3.12";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/4a/85/db5a2df477072b2902b0eb892feb37d88ac635d36245a72a6a69b23b383a/PyYAML-3.12.tar.gz"; sha256 = "592766c6303207a20efc445587778322d7f73b161bd994f227adaa341ba212ab"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "http://pyyaml.org/wiki/PyYAML";
+        license = licenses.mit;
+        description = "YAML parser and emitter for Python";
+      };
+    };
+
+
+
+    "arrow" = python.mkDerivation {
+      name = "arrow-0.10.0";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/54/db/76459c4dd3561bbe682619a5c576ff30c42e37c2e01900ed30a501957150/arrow-0.10.0.tar.gz"; sha256 = "805906f09445afc1f0fc80187db8fe07670e3b25cdafa09b8d8ac264a8c0c722"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."python-dateutil"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "https://github.com/crsmithdev/arrow/";
+        license = licenses.asl20;
+        description = "Better dates and times for Python";
+      };
+    };
+
+
+
+    "click" = python.mkDerivation {
+      name = "click-6.7";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/95/d9/c3336b6b5711c3ab9d1d3a80f1a3e2afeb9d8c02a7166462f6cc96570897/click-6.7.tar.gz"; sha256 = "f15516df478d5a56180fbf80e68f206010e6d160fc39fa508b65e035fd75130b"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "http://github.com/mitsuhiko/click";
+        license = licenses.bsdOriginal;
+        description = "A simple wrapper around optparse for powerful command line utilities.";
+      };
+    };
+
+
+
+    "gitdb2" = python.mkDerivation {
+      name = "gitdb2-2.0.3";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/84/11/22e68bd46fd545b17d0a0b200cf75c20e9e7b817726a69ad5f3070fd0d3c/gitdb2-2.0.3.tar.gz"; sha256 = "b60e29d4533e5e25bb50b7678bbc187c8f6bcff1344b4f293b2ba55c85795f09"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."smmap2"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "https://github.com/gitpython-developers/gitdb";
+        license = licenses.bsdOriginal;
+        description = "Git Object Database";
+      };
+    };
+
+
+
+    "py" = python.mkDerivation {
+      name = "py-1.5.2";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/90/e3/e075127d39d35f09a500ebb4a90afd10f9ef0a1d28a6d09abeec0e444fdd/py-1.5.2.tar.gz"; sha256 = "ca18943e28235417756316bfada6cd96b23ce60dd532642690dcfdaba988a76d"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "http://py.readthedocs.io/";
+        license = licenses.mit;
+        description = "library with cross-python path, ini-parsing, io, code, log facilities";
+      };
+    };
+
+
+
+    "pytest" = python.mkDerivation {
+      name = "pytest-3.2.5";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/1f/f8/8cd74c16952163ce0db0bd95fdd8810cbf093c08be00e6e665ebf0dc3138/pytest-3.2.5.tar.gz"; sha256 = "6d5bd4f7113b444c55a3bbb5c738a3dd80d43563d063fc42dcb0aaefbdd78b81"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."py"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "http://pytest.org";
+        license = licenses.mit;
+        description = "pytest: simple powerful testing with Python";
+      };
+    };
+
+
+
+    "python-dateutil" = python.mkDerivation {
+      name = "python-dateutil-2.6.1";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/54/bb/f1db86504f7a49e1d9b9301531181b00a1c7325dc85a29160ee3eaa73a54/python-dateutil-2.6.1.tar.gz"; sha256 = "891c38b2a02f5bb1be3e4793866c8df49c7d19baabf9c1bad62547e0b4866aca"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."six"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "https://dateutil.readthedocs.io";
+        license = licenses.bsdOriginal;
+        description = "Extensions to the standard Python datetime module";
+      };
+    };
+
+
+
+    "six" = python.mkDerivation {
+      name = "six-1.11.0";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"; sha256 = "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "http://pypi.python.org/pypi/six/";
+        license = licenses.mit;
+        description = "Python 2 and 3 compatibility utilities";
+      };
+    };
+
+
+
+    "smmap2" = python.mkDerivation {
+      name = "smmap2-2.0.3";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/48/d8/25d9b4b875ab3c2400ec7794ceda8093b51101a9d784da608bf65ab5f5f5/smmap2-2.0.3.tar.gz"; sha256 = "c7530db63f15f09f8251094b22091298e82bf6c699a6b8344aaaef3f2e1276c3"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "https://github.com/Byron/smmap";
+        license = licenses.bsdOriginal;
+        description = "A pure python implementation of a sliding window memory map manager";
+      };
+    };
+
+  };
+  localOverridesFile = ./requirements_override.nix;
+  overrides = import localOverridesFile { inherit pkgs python; };
+  commonOverrides = [
+
+  ];
+  allOverrides =
+    (if (builtins.pathExists localOverridesFile)
+     then [overrides] else [] ) ++ commonOverrides;
+
+in python.withPackages
+   (fix' (pkgs.lib.fold
+            extends
+            generated
+            allOverrides
+         )
+   )
---
+++ src/releasewarrior/requirements_override.nix
@@ -0,0 +1,262 @@
+{ pkgs, python }:
+
+let
+
+  inherit (pkgs.lib) fileContents;
+
+  skipOverrides = overrides: self: super:
+    let
+      overridesNames = builtins.attrNames overrides;
+      superNames = builtins.attrNames super;
+    in
+      builtins.listToAttrs
+        (builtins.map
+          (name: { inherit name;
+                   value = python.overrideDerivation super."${name}" (overrides."${name}" self);
+                 }
+          )
+          (builtins.filter
+            (name: builtins.elem name superNames)
+            overridesNames
+          )
+        );
+
+in skipOverrides {
+
+  "mozilla-backend-common" = self: old: {
+    name = "mozilla-backend-common-${fileContents ./../lib/backend_common/VERSION}";
+
+    doCheck = true;
+
+    buildInputs =[
+      self."Flask-Cache"
+      self."Flask-Cors"
+      self."Flask-Login"
+      self."Flask-Migrate"
+      self."Flask-SQLAlchemy"
+      self."Jinja2"
+      self."connexion"
+      self."flake8"
+      self."flake8-coding"
+      self."flake8-quotes"
+      self."flask-oidc"
+      self."flask-talisman"
+      self."inotify"
+      self."kombu"
+      self."pdbpp"
+      self."pytest"
+      self."responses"
+      self."taskcluster"
+    ];
+
+    patchPhase = ''
+      # replace synlink with real file
+      rm -f setup.cfg
+      ln -s ${./setup.cfg} setup.cfg
+
+      # generate MANIFEST.in to make sure every file is included
+      rm -f MANIFEST.in
+      cat > MANIFEST.in <<EOF
+      recursive-include backend_common/*
+
+      include VERSION
+      include backend_common/VERSION
+      include backend_common/*.ini
+      include backend_common/*.json
+      include backend_common/*.mako
+      include backend_common/*.yml
+
+      recursive-exclude * __pycache__
+      recursive-exclude * *.py[co]
+      EOF
+    '';
+
+    preConfigure = ''
+      rm -rf build *.egg-info
+    '';
+
+    checkPhase = ''
+      export LANG=en_US.UTF-8
+      export LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive
+
+      echo "################################################################"
+      echo "## flake8 ######################################################"
+      echo "################################################################"
+      flake8 -v
+      echo "################################################################"
+
+      echo "################################################################"
+      echo "## pytest ######################################################"
+      echo "################################################################"
+      pytest tests/ -vvv -s
+      echo "################################################################"
+    '';
+  };
+
+  "mozilla-cli-common" = self: old: {
+    name = "mozilla-cli-common-${fileContents ./../lib/cli_common/VERSION}";
+
+    doCheck = true;
+
+    buildInputs =
+      [ self."flake8"
+        self."pytest"
+      ];
+
+    patchPhase = ''
+      # replace synlink with real file
+      rm -f setup.cfg
+      ln -s ${./setup.cfg} setup.cfg
+
+      # generate MANIFEST.in to make sure every file is included
+      rm -f MANIFEST.in
+      cat > MANIFEST.in <<EOF
+      recursive-include cli_common/*
+
+      include VERSION
+      include cli_common/VERSION
+      include cli_common/*.ini
+      include cli_common/*.json
+      include cli_common/*.mako
+      include cli_common/*.yml
+
+      recursive-exclude * __pycache__
+      recursive-exclude * *.py[co]
+      EOF
+    '';
+
+    preConfigure = ''
+      rm -rf build *.egg-info
+    '';
+
+    checkPhase = ''
+      export LANG=en_US.UTF-8
+      export LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive
+
+      echo "################################################################"
+      echo "## flake8 ######################################################"
+      echo "################################################################"
+      flake8 -v
+      echo "################################################################"
+
+      echo "################################################################"
+      echo "## pytest ######################################################"
+      echo "################################################################"
+      pytest tests/ -vvv -s
+      echo "################################################################"
+    '';
+  };
+
+  # -- in alphabetic order --
+
+  "async-timeout" = self: old: {
+    patchPhase = ''
+      sed -i -e "s|setup_requires=\['pytest-runner'\],||" setup.py
+    '';
+  };
+
+  "awscli" = self: old: {
+    propagatedBuildInputs = old.propagatedBuildInputs ++ (with pkgs; [ groff less ]);
+    postInstall = ''
+      mkdir -p $out/etc/bash_completion.d
+      echo "complete -C $out/bin/aws_completer aws" > $out/etc/bash_completion.d/awscli
+      mkdir -p $out/share/zsh/site-functions
+      mv $out/bin/aws_zsh_completer.sh $out/share/zsh/site-functions
+      rm $out/bin/aws.cmd
+    '';
+  };
+
+  "chardet" = self: old: {
+    patchPhase = ''
+      sed -i -e "s|setup_requires=\['pytest-runner'\],||" setup.py
+    '';
+  };
+
+  "clickclick" = self: old: {
+    patchPhase = ''
+      sed -i -e "s|setup_requires=\['six', 'flake8'\],||" setup.py
+      sed -i -e "s|command_options=command_options,||" setup.py
+    '';
+  };
+
+  "click-spinner" = self: old: {
+    patchPhase = ''
+      rm README.md
+      touch README.md
+    '';
+  };
+
+  "connexion" = self: old: {
+    patchPhase = ''
+      sed -i -e "s|setup_requires=\['flake8'\],||" setup.py
+      sed -i -e "s|jsonschema>=2.5.1|jsonschema|" setup.py
+      sed -i -e "s|'pathlib>=1.0.1; python_version < \"3.4\"',||" setup.py
+    '';
+  };
+
+  "flake8" = self: old: {
+    patchPhase = ''
+      sed -i -e "s|setup_requires=\['pytest-runner'\],||" setup.py
+    '';
+  };
+
+  "flask-talisman" = self: old: {
+    # XXX: from https://github.com/GoogleCloudPlatform/flask-talisman/pull/8
+    patchPhase = ''
+      sed -i \
+        -e "s|view_function = flask.current_app.view_functions\[|view_function = flask.current_app.view_functions.get(|" \
+        -e "s|flask.request.endpoint\]|flask.request.endpoint)|" \
+          flask_talisman/talisman.py
+    '';
+  };
+
+  "gunicorn" = self: old: {
+    patches = [
+         (pkgs.fetchurl {
+           url = "https://github.com/benoitc/gunicorn/pull/1527.patch";
+           sha256 = "14zvlm4dh432gd5n32i2x60rkq3d8wz1xlj45ldkp2z4qgp7chbk";
+         })
+      ];
+  };
+
+  "jsonschema" = self: old: {
+    patchPhase = ''
+      sed -i -e 's|setup_requires=\["vcversioner>=2.16.0.0"\],||' setup.py
+    '';
+  };
+
+  "libmozdata" = self: old: {
+    # Remove useless dependencies
+    patchPhase = ''
+      sed -i -e "s|mercurial>=3.9.1; python_version < '3.0'||" requirements.txt
+      sed -i -e "s|setuptools>=28.6.1||" requirements.txt
+    '';
+  };
+
+  "mccabe" = self: old: {
+    patchPhase = ''
+      sed -i -e "s|setup_requires=\['pytest-runner'\],||" setup.py
+    '';
+  };
+
+  "pdbpp" = self: old: {
+    patchPhase = ''
+      sed -i -e "s|setup_requires=\['setuptools_scm'\],||" setup.py
+    '';
+  };
+
+  "pytest" = self: old: {
+    patchPhase = ''
+      sed -i -e "s|setup_requires=\['setuptools-scm'\],||" setup.py
+    '';
+  };
+
+  "RBTools" = self: old: {
+    patches = [
+         (pkgs.fetchurl {
+           url = "https://github.com/La0/rbtools/commit/190b4adb768897f65cf7ec57806649bc14c8e45d.diff";
+           sha256 = "1hh6i3cffsc4fxr4jqlxralnf78529i0pspm7jn686a2s6bh26mw";
+         })
+      ];
+  };
+}
