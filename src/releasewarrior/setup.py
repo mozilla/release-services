6bbfa8f5dd6a55f60be49bc70056edfe98d17fbb
Rok Garbas <rok@garbas.si>
2017-11-17 13:17:35 +0100

    releasewarrior: split requirements.txt between tests_require and install_requires

--- src/releasewarrior/setup.py
+++ src/releasewarrior/setup.py
@@ -1,14 +1,29 @@
 from setuptools import setup, find_packages
 
-requirements = open("requirements.txt").readlines()
 version = open("version.txt").readline()
+
+
+def read_requirements(file_):
+    lines = []
+    with open(file_) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith('-e ') or line.startswith('http://') or line.startswith('https://'):
+                lines.append(line.split('#')[1].split('egg=')[1])
+            elif line.startswith('#') or line.startswith('-'):
+                pass
+            else:
+                lines.append(line)
+    return lines
+
 
 setup(
     name='releasewarrior',
     version=version,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=requirements,
+    tests_require=read_requirements('requirements-dev.txt'),
+    install_requires=read_requirements('requirements.txt'),
     entry_points="""
         [console_scripts]
         release=releasewarrior.cli:cli
