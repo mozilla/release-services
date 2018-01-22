694de626d2978be0aeb7c3375e87efc47b4f4e0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-01-22 18:17:27 +0100

    shipit_code_coverage: Cancel unscheduled tasks when one of the tasks fails

--- src/shipit_code_coverage/tests/test_utils.py
+++ src/shipit_code_coverage/tests/test_utils.py
@@ -49,6 +49,7 @@ def test_threadpoolexecutorresult():
         executor.submit(lambda: True)
         executor.submit(lambda: False)
 
+    # Test that ThreadPoolExecutorResult throws an exception when one of the tasks fails.
     try:
         with utils.ThreadPoolExecutorResult() as executor:
             executor.submit(lambda: True)
@@ -57,12 +58,31 @@ def test_threadpoolexecutorresult():
     except:
         assert True
 
+    # Tast that ThreadPoolExecutorResult's context returns as soon as a task fails.
     try:
         now = time.time()
         with utils.ThreadPoolExecutorResult() as executor:
-            executor.submit(lambda: time.sleep(10))
+            executor.submit(lambda: time.sleep(5))
             executor.submit(do_raise)
         assert False
     except:
-        assert True
-        assert time.time() - now < 5
+        assert time.time() - now < 2
+
+    # Test that futures that were not scheduled yet are cancelled.
+    try:
+        with utils.ThreadPoolExecutorResult(max_workers=1) as executor:
+            f1 = executor.submit(lambda: time.sleep(1))
+            f2 = executor.submit(do_raise)
+            executor.submit(lambda: time.sleep(1))
+            f4 = executor.submit(lambda: time.sleep(1))
+            f5 = executor.submit(lambda: time.sleep(1))
+            f6 = executor.submit(lambda: time.sleep(1))
+        assert False
+    except:
+        assert f1.done() and not f1.cancelled()
+        assert f2.exception() is not None
+        # Not enough time to cancel the third future, scheduled right after the one which raises an exception.
+        # When we try to cancel it, it's already running, so the cancellation fails.
+        assert f4.cancelled()
+        assert f5.cancelled()
+        assert f6.cancelled()
