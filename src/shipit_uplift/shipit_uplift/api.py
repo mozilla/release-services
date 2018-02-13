12aee25a314f2cf7b3be15f0759882502e9d175a
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-02-13 12:50:48 +0100

    shipit_uplift: Remove chunk mapping API

--- src/shipit_uplift/shipit_uplift/api.py
+++ src/shipit_uplift/shipit_uplift/api.py
@@ -23,7 +23,7 @@ from shipit_uplift.config import SCOPES_USER, SCOPES_BOT, SCOPES_ADMIN
 from shipit_uplift import (
     coverage_by_dir_impl, coverage_for_file_impl,
     coverage_by_changeset_impl, coverage_summary_by_changeset_impl,
-    coverage_chunks_for_patch_impl, coverage
+    coverage
 )
 from rq import Queue
 from shipit_uplift.worker import conn
@@ -421,7 +421,3 @@ def coverage_summary_by_changeset(changeset):
 
 def coverage_supported_extensions():
     return coverage.COVERAGE_EXTENSIONS
-
-
-def coverage_chunks_for_patch(patch):
-    return coverage_chunks_for_patch_impl.generate(patch)
