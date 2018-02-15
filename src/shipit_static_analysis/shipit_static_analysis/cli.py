3ebbbab453f476adbb2e902cc6b5259968a02c73
Bastien Abadie <bastien@nextcairn.com>
2018-02-15 12:33:45 +0100

    shipit_static_analysis: Simpler cache (#842)
    * shipit_static_analysis: remove LockDir, closes #749
    
    * shipit_static_analysis: Use prefix for common sharebase

--- src/shipit_static_analysis/shipit_static_analysis/cli.py
+++ src/shipit_static_analysis/shipit_static_analysis/cli.py
@@ -9,7 +9,6 @@ from shipit_static_analysis.workflow import Workflow
 from shipit_static_analysis.revisions import PhabricatorRevision, MozReviewRevision
 from shipit_static_analysis.config import settings
 from shipit_static_analysis.report import get_reporters
-from shipit_static_analysis.lock import LockDir
 from shipit_static_analysis import config, stats
 from cli_common.click import taskcluster_options
 from cli_common.log import init_logger
@@ -100,18 +99,17 @@ def main(phabricator,
             for r in mozreview.split(' ')
         ]
 
-    with LockDir(cache_root, 'shipit-sa-') as work_dir:
-        w = Workflow(work_dir, reporters, secrets['ANALYZERS'])
-        for revision in revisions:
-            try:
-                w.run(revision)
-            except Exception as e:
-                # Log errors to papertrail
-                logger.error(
-                    'Static analysis failure',
-                    revision=revision,
-                    error=e,
-                )
+    w = Workflow(cache_root, reporters, secrets['ANALYZERS'])
+    for revision in revisions:
+        try:
+            w.run(revision)
+        except Exception as e:
+            # Log errors to papertrail
+            logger.error(
+                'Static analysis failure',
+                revision=revision,
+                error=e,
+            )
 
 
 if __name__ == '__main__':
