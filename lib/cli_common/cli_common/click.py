96777a21ce14715b2a968b94fdc1d2392f075754
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-02-26 17:07:35 +0100

    cli_common: Set default values for taskcluster-secret, taskcluster-client-id and taskcluster-access-token options

--- lib/cli_common/cli_common/click.py
+++ lib/cli_common/cli_common/click.py
@@ -9,16 +9,19 @@ def taskcluster_options(func):
     @click.option(
         '--taskcluster-secret',
         help='Taskcluster Secret path',
+        default='repo:github.com/mozilla-releng/services:branch:master',
         envvar='TASKCLUSTER_SECRET',
     )
     @click.option(
         '--taskcluster-client-id',
         help='Taskcluster Client ID',
+        default=None,
         envvar='TASKCLUSTER_CLIENT_ID',
     )
     @click.option(
         '--taskcluster-access-token',
         help='Taskcluster Access token',
+        default=None,
         envvar='TASKCLUSTER_ACCESS_TOKEN'
     )
     @functools.wraps(func)
