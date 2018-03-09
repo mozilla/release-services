2dd69e3621bb740247869301d69697065287f8f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-03-09 15:10:43 +0000

    shipit_pulse_listener: Add tests for Hook.create_task

--- src/shipit_pulse_listener/tests/test_hook.py
+++ src/shipit_pulse_listener/tests/test_hook.py
@@ -1,7 +1,40 @@
 # -*- coding: utf-8 -*-
 from shipit_pulse_listener.hook import Hook
-from datetime import timedelta
+from shipit_pulse_listener import task_monitoring
+import pytest
 
 
-def test_create_task():
-    pass
+@pytest.mark.asyncio
+async def test_create_task_no_hooks_service(HooksMock):
+    hook = Hook('aGroup', 'aHook')
+
+    with pytest.raises(Exception):
+        await hook.create_task()
+
+
+@pytest.mark.asyncio
+async def test_create_task(HooksMock):
+    hook = Hook('aGroup', 'aHook')
+
+    hook.hooks = HooksMock
+
+    task_id = await hook.create_task()
+    assert task_id == 'fake_task_id'
+    assert HooksMock.obj['group_id'] == 'aGroup'
+    assert HooksMock.obj['hook_id'] == 'aHook'
+    assert HooksMock.obj['payload'] == {'extra_env': {}}
+    assert task_monitoring.tasks.qsize() == 1
+
+
+@pytest.mark.asyncio
+async def test_create_task_extra_env(HooksMock):
+    hook = Hook('aGroup', 'aHook')
+
+    hook.hooks = HooksMock
+
+    task_id = await hook.create_task({'test': 'succeeded'})
+    assert task_id == 'fake_task_id'
+    assert HooksMock.obj['group_id'] == 'aGroup'
+    assert HooksMock.obj['hook_id'] == 'aHook'
+    assert HooksMock.obj['payload'] == {'extra_env': {'test': 'succeeded'}}
+    assert task_monitoring.tasks.qsize() == 2
