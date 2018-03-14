42a1dd8d41896f7365c019b2c827721c62400ea7
sprt <hellosprt@gmail.com>
2018-03-14 19:15:50 +0100

    shipit-code-coverage: add missing import

--- src/shipit_code_coverage/shipit_code_coverage/notifier.py
+++ src/shipit_code_coverage/shipit_code_coverage/notifier.py
@@ -4,6 +4,7 @@ import requests
 
 from cli_common.log import get_logger
 from cli_common.taskcluster import get_service
+from cli_common.utils import retry
 
 from shipit_code_coverage.secrets import secrets
 
