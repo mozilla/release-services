31f3b28c809aafa1e7fe0ca0b24b9e22ecfc8329
sprt <hellosprt@gmail.com>
2018-03-15 18:39:00 +0100

    Cleaner handling of fixtures

--- src/shipit_code_coverage/tests/conftest.py
+++ src/shipit_code_coverage/tests/conftest.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 from contextlib import contextmanager
 import json
 import os
+import responses
 import tempfile
 import zipfile
 import pytest
@@ -16,13 +17,9 @@ import pytest
 FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
 
 
-def load_file(path):
-    with open(os.path.join(FIXTURES_DIR, path)) as f:
-        return f.read()
-
-
 def load_json(path):
-    return json.loads(load_file(path))
+    with open(os.path.join(FIXTURES_DIR, path)) as f:
+        return json.load(f)
 
 
 @pytest.fixture(scope='session')
@@ -73,16 +70,6 @@ def GROUP_TASKS_1():
 @pytest.fixture(scope='session')
 def GROUP_TASKS_2():
     return load_json('task-group_2.json')
-
-
-@pytest.fixture(scope='session')
-def MERCURIAL_COMMIT_RESPONSE():
-    return load_file('rev_hg_0d1e55d87931fe70ec1d007e886bcd58015ff770')
-
-
-@pytest.fixture(scope='session')
-def GITHUB_COMMIT_RESPONSE():
-    return load_file('rev_git_876c7dd30586f9c6f9c99ef7444f2d73c7acfe7c')
 
 
 @pytest.fixture(scope='session')
@@ -144,3 +131,21 @@ def grcov_uncovered_function_artifact():
 def jsvm_uncovered_function_artifact():
     with generate_coverage_artifact('jsvm_uncovered_function.info') as f:
         yield f
+
+
+@pytest.fixture(scope='session')
+def mock_get_commit(MERCURIAL_COMMIT):
+    responses.add(
+        responses.GET,
+        'https://api.pub.build.mozilla.org/mapper/gecko-dev/rev/hg/{}'.format(MERCURIAL_COMMIT),
+        body='{} 0d1e55d87931fe70ec1d007e886bcd58015ff770'.format(MERCURIAL_COMMIT),
+        status=200)
+
+
+@pytest.fixture(scope='session')
+def mock_get_mercurial(GITHUB_COMMIT):
+    responses.add(
+        responses.GET,
+        'https://api.pub.build.mozilla.org/mapper/gecko-dev/rev/git/{}'.format(GITHUB_COMMIT),
+        body='876c7dd30586f9c6f9c99ef7444f2d73c7acfe7c {}'.format(GITHUB_COMMIT),
+        status=200)
--- src/shipit_code_coverage/tests/test_github.py
+++ src/shipit_code_coverage/tests/test_github.py
@@ -6,23 +6,12 @@ from shipit_code_coverage.github import GitHubUtils
 
 
 @responses.activate
-def test_get_commit(MERCURIAL_COMMIT, MERCURIAL_COMMIT_RESPONSE):
-    responses.add(
-        responses.GET,
-        'https://api.pub.build.mozilla.org/mapper/gecko-dev/rev/hg/{}'.format(MERCURIAL_COMMIT),
-        body=MERCURIAL_COMMIT_RESPONSE,
-        status=200)  # noqa
-
+def test_get_commit(MERCURIAL_COMMIT, mock_get_commit):
     gu = GitHubUtils('', '', '')
     assert gu.get_commit(MERCURIAL_COMMIT) == MERCURIAL_COMMIT
 
 
 @responses.activate
-def test_get_mercurial(GITHUB_COMMIT, GITHUB_COMMIT_RESPONSE):
-    responses.add(responses.GET,
-        'https://api.pub.build.mozilla.org/mapper/gecko-dev/rev/git/{}'.format(GITHUB_COMMIT),
-        body=GITHUB_COMMIT_RESPONSE,
-        status=200)  # noqa
-
+def test_get_mercurial(GITHUB_COMMIT, mock_get_mercurial):
     gu = GitHubUtils('', '', '')
     assert gu.get_mercurial(GITHUB_COMMIT) == GITHUB_COMMIT
