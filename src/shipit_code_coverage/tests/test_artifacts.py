6f429580b5dba08ecff064e075363c6f795e9661
Marco <mcastelluccio@mozilla.com>
2018-03-19 10:27:32 +0000

    shipit_code_coverage: Clean artifacts tests up and make them more robust (#951)
    * shipit_code_coverage: Use pytest.raises
    
    * shipit_code_coverage: Clean artifacts tests up and make them more robust

--- src/shipit_code_coverage/tests/test_artifacts.py
+++ src/shipit_code_coverage/tests/test_artifacts.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os
-from cli_common import utils
 from shipit_code_coverage.artifacts import ArtifactsHandler
+import pytest
 
 
-FILES = ['ccov-artifacts/%s' % f for f in [
+FILES = [
     'windows_mochitest-1_code-coverage-jsvm.info',
     'linux_mochitest-2_code-coverage-grcov.zip',
     'windows_xpcshell-7_code-coverage-jsvm.info',
@@ -13,58 +13,49 @@ FILES = ['ccov-artifacts/%s' % f for f in [
     'linux_xpcshell-3_code-coverage-grcov.zip',
     'windows_cppunit_code-coverage-grcov.zip',
     'linux_firefox-ui-functional-remote_code-coverage-jsvm.info',
-]]
+]
 
 
-def test_get_chunks():
-    utils.mkdir('ccov-artifacts')
-
+@pytest.fixture
+def FAKE_ARTIFACTS_DIR(tmpdir):
     for f in FILES:
-        open(f, 'w')
+        open(os.path.join(tmpdir.strpath, f), 'w')
+    return tmpdir.strpath
 
-    a = ArtifactsHandler([], [])
+
+def test_get_chunks(FAKE_ARTIFACTS_DIR):
+    a = ArtifactsHandler([], [], parent_dir=FAKE_ARTIFACTS_DIR)
     assert set(a.get_chunks()) == set([
         'mochitest-1', 'mochitest-2', 'xpcshell-3', 'xpcshell-7',
         'cppunit', 'firefox-ui-functional-remote',
     ])
 
-    for f in FILES:
-        os.remove(f)
 
+def test_get_coverage_artifacts(FAKE_ARTIFACTS_DIR):
+    def add_dir(files):
+        return set([os.path.join(FAKE_ARTIFACTS_DIR, f) for f in files])
 
-def test_get_coverage_artifacts():
-    utils.mkdir('ccov-artifacts')
-
-    for f in FILES:
-        open(f, 'w')
-
-    a = ArtifactsHandler([], [])
-    assert set(a.get()) == set(FILES)
-    assert set(a.get(suite='mochitest')) == set([
-        'ccov-artifacts/windows_mochitest-1_code-coverage-jsvm.info',
-        'ccov-artifacts/linux_mochitest-2_code-coverage-grcov.zip'
+    a = ArtifactsHandler([], [], parent_dir=FAKE_ARTIFACTS_DIR)
+    assert set(a.get()) == add_dir(FILES)
+    assert set(a.get(suite='mochitest')) == add_dir([
+        'windows_mochitest-1_code-coverage-jsvm.info',
+        'linux_mochitest-2_code-coverage-grcov.zip'
     ])
-    assert set(a.get(chunk='xpcshell-7')) == set([
-        'ccov-artifacts/windows_xpcshell-7_code-coverage-jsvm.info',
-        'ccov-artifacts/linux_xpcshell-7_code-coverage-grcov.zip'
+    assert set(a.get(chunk='xpcshell-7')) == add_dir([
+        'windows_xpcshell-7_code-coverage-jsvm.info',
+        'linux_xpcshell-7_code-coverage-grcov.zip'
     ])
-    assert set(a.get(chunk='cppunit')) == set([
-        'ccov-artifacts/windows_cppunit_code-coverage-grcov.zip'
+    assert set(a.get(chunk='cppunit')) == add_dir([
+        'windows_cppunit_code-coverage-grcov.zip'
     ])
-    assert set(a.get(platform='windows')) == set([
-        'ccov-artifacts/windows_mochitest-1_code-coverage-jsvm.info',
-        'ccov-artifacts/windows_xpcshell-7_code-coverage-jsvm.info',
-        'ccov-artifacts/windows_cppunit_code-coverage-grcov.zip',
+    assert set(a.get(platform='windows')) == add_dir([
+        'windows_mochitest-1_code-coverage-jsvm.info',
+        'windows_xpcshell-7_code-coverage-jsvm.info',
+        'windows_cppunit_code-coverage-grcov.zip',
     ])
-    assert set(a.get(platform='linux', chunk='xpcshell-7')) == set([
-        'ccov-artifacts/linux_xpcshell-7_code-coverage-grcov.zip'
+    assert set(a.get(platform='linux', chunk='xpcshell-7')) == add_dir([
+        'linux_xpcshell-7_code-coverage-grcov.zip'
     ])
 
-    try:
+    with pytest.raises(Exception, message='suite and chunk can\'t both have a value'):
         a.get(chunk='xpcshell-7', suite='mochitest')
-        assert False, 'An exception should have been thrown'
-    except Exception:
-        pass
-
-    for f in FILES:
-        os.remove(f)
