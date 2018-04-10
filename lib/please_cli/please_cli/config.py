047f7f2502413aa10cd69820859cfcdfab302baf
Rok Garbas <rok@garbas.si>
2018-04-11 00:20:46 +0200

    add missing dns for testing S3 targets

--- lib/please_cli/please_cli/config.py
+++ lib/please_cli/please_cli/config.py
@@ -224,7 +224,7 @@ PROJECTS_CONFIG = {
             'testing': {
                 's3_bucket': 'releng-testing-docs',
                 'url': 'https://docs.testing.mozilla-releng.net',
-                'dns': 'd32jt14rospqzr.cloudfront.net.',
+                'dns': 'd1sw5c8kdn03y.cloudfront.net.',
             },
             'staging': {
                 's3_bucket': 'releng-staging-docs',
@@ -258,7 +258,7 @@ PROJECTS_CONFIG = {
             'testing': {
                 's3_bucket': 'releng-testing-frontend',
                 'url': 'https://testing.mozilla-releng.net',
-                'dns': 'dpwmwa9tge2p3.cloudfront.net.',
+                'dns': 'd1l70lpksx3ik7.cloudfront.net.',
                 'csp': [
                     'https://login.taskcluster.net',
                     'https://auth.taskcluster.net',
@@ -433,7 +433,7 @@ PROJECTS_CONFIG = {
             'testing': {
                 's3_bucket': 'shipit-testing-frontend',
                 'url': 'https://shipit.testing.mozilla-releng.net',
-                'dns': 'd2ld4e8bl8yd1l.cloudfront.net.',
+                'dns': 'd2jpisuzgldax2.cloudfront.net.',
                 'envs': {
                     'bugzilla-url': 'https://bugzilla.mozilla.org',
                 },
