c7d6b3300e80adec489b20e4200ce3ffef1b9ad7
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-04-20 12:02:19 +0200

    shipit_pulse_listener: Don't trigger coverage tasks if the groupResolved notification doesn't belong to a mozilla-central build

--- src/shipit_pulse_listener/tests/test_code_coverage_listener.py
+++ src/shipit_pulse_listener/tests/test_code_coverage_listener.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
+import json
+import os
 from datetime import datetime
 from datetime import timedelta
 
+import responses
+
 from shipit_pulse_listener.listener import HookCodeCoverage
+
+FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
 
 
 def test_is_coverage_task():
@@ -109,3 +115,70 @@ def test_parse():
     assert hook.parse({
         'taskGroupId': 'already-triggered-group'
     }) is None
+
+
+def test_is_mozilla_central_task():
+    hook = HookCodeCoverage({
+      'hookId': 'shipit-staging-code-coverage'
+    })
+
+    inbound_task = {
+        'task': {
+            'payload': {
+                'env': {
+                    'MH_BRANCH': 'mozilla-inbound',
+                }
+            }
+        }
+    }
+    assert not hook.is_mozilla_central_task(inbound_task)
+
+    try_task = {
+        'task': {
+            'payload': {
+                'env': {
+                    'MH_BRANCH': 'try',
+                }
+            }
+        }
+    }
+    assert not hook.is_mozilla_central_task(try_task)
+
+    central_task = {
+        'task': {
+            'payload': {
+                'env': {
+                    'MH_BRANCH': 'mozilla-central',
+                }
+            }
+        }
+    }
+    assert hook.is_mozilla_central_task(central_task)
+
+
+@responses.activate
+def test_wrong_branch():
+    with open(os.path.join(FIXTURES_DIR, 'bNq-VIT-Q12o6nXcaUmYNQ.json')) as f:
+        responses.add(responses.GET, 'https://queue.taskcluster.net/v1/task-group/bNq-VIT-Q12o6nXcaUmYNQ/list?limit=200', json=json.load(f), status=200, match_querystring=True)  # noqa
+
+    hook = HookCodeCoverage({
+      'hookId': 'shipit-staging-code-coverage'
+    })
+
+    assert hook.parse({
+        'taskGroupId': 'bNq-VIT-Q12o6nXcaUmYNQ'
+    }) is None
+
+
+@responses.activate
+def test_success():
+    with open(os.path.join(FIXTURES_DIR, 'RS0UwZahQ_qAcdZzEb_Y9g.json')) as f:
+        responses.add(responses.GET, 'https://queue.taskcluster.net/v1/task-group/RS0UwZahQ_qAcdZzEb_Y9g/list?limit=200', json=json.load(f), status=200, match_querystring=True)  # noqa
+
+    hook = HookCodeCoverage({
+      'hookId': 'shipit-staging-code-coverage'
+    })
+
+    assert hook.parse({
+        'taskGroupId': 'RS0UwZahQ_qAcdZzEb_Y9g'
+    }) == {'REVISION': 'ec3dd3ee2ae4b3a63529a912816a110e925eb2d0'}
