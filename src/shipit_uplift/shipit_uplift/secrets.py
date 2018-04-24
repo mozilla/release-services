bb66336fc7f776f61394d4c7cc36c167cd59242d
Bastien Abadie <bastien@nextcairn.com>
2018-04-24 14:05:44 +0200

    shipit-uplift: Remove code coverage

--- src/shipit_uplift/shipit_uplift/secrets.py
+++ src/shipit_uplift/shipit_uplift/secrets.py
@@ -12,11 +12,7 @@ secrets = cli_common.taskcluster.get_secrets(
     os.environ.get('TASKCLUSTER_SECRET'),
     shipit_uplift.config.PROJECT_NAME,
     required=[],
-    existing={x: os.environ.get(x) for x in ['REDIS_URL'] if x in os.environ},
+    existing={},
     taskcluster_client_id=os.environ.get('TASKCLUSTER_CLIENT_ID'),
     taskcluster_access_token=os.environ.get('TASKCLUSTER_ACCESS_TOKEN'),
 )
-
-REDIS_URL = secrets['REDIS_URL'] if 'REDIS_URL' in secrets else 'redis://localhost:6379'
-CODECOV_ACCESS_TOKEN = secrets['CODECOV_ACCESS_TOKEN'] if 'CODECOV_ACCESS_TOKEN' in secrets else ''
-CODECOV_REPO = secrets['CODECOV_REPO'] if 'CODECOV_REPO' in secrets else 'marco-c/gecko-dev'
