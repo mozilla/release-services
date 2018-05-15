a14d231c05b748fcd003df5b3061496d345faf20
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-05-15 20:08:39 +0200

    shipit_code_coverage: Retrieve tests -> chunk mapping one suite at a time to avoid hitting the 50K limit from ActiveData

--- src/shipit_code_coverage/shipit_code_coverage/chunk_mapping.py
+++ src/shipit_code_coverage/shipit_code_coverage/chunk_mapping.py
@@ -14,15 +14,60 @@ from shipit_code_coverage import taskcluster
 logger = get_logger(__name__)
 
 
+PLATFORMS = ['linux', 'windows']
+
+
+def get_suites(revision):
+    r = requests.post('https://activedata.allizom.org/query', json={
+        "from":"unittest",
+        "where":{"and":[
+            {"eq":{"repo.branch.name":"mozilla-central"}},
+            {"eq":{"repo.changeset.id12":revision[:12]}},
+            {"or":[
+                {"prefix":{"run.key":"test-linux64-ccov"}},
+                {"prefix":{"run.key":"test-windows10-64-ccov"}}
+            ]}
+        ]},
+        "limit":500000,
+        "groupby":["run.suite"]
+    })
+
+    suites_data = r.json()['data']
+
+    return [e[0] for e in suites_data]
+
+
+# Retrieve chunk -> tests mapping from ActiveData.
+def get_tests_chunks(revision, platform, suite):
+    if platform == 'linux':
+        run_key_prefix = 'test-linux64-ccov'
+    elif platform == 'windows':
+        run_key_prefix = 'test-windows10-64-ccov'
+
+    r = requests.post('https://activedata.allizom.org/query', json={
+        'from': 'unittest',
+        'where': {'and': [
+            {'eq': {'repo.branch.name': 'mozilla-central'}},
+            {'eq': {'repo.changeset.id12': revision[:12]}},
+            {"eq":{"run.suite": suite}},
+            {'prefix': {'run.key': run_key_prefix}},
+        ]},
+        'limit': 50000,
+        'select': ['result.test', 'run.key']
+    })
+
+    return r.json()['data']
+
+
 def generate(repo_dir, revision, artifactsHandler, out_dir='.'):
     sqlite_file = os.path.join(out_dir, 'chunk_mapping.sqlite')
     tarxz_file = os.path.join(out_dir, 'chunk_mapping.tar.xz')
 
     with ThreadPoolExecutor(max_workers=4) as executor:
         futures = {}
-        for platform in ['linux', 'windows']:
+        for platform in PLATFORMS:
             for chunk in artifactsHandler.get_chunks():
-                future = executor.submit(grcov.files_list, artifactsHandler.get(platform=platform, chunk=chunk), source_dir=repo_dir)
+                future = executor.submit(grcov.files_list, artifactsHandler.get(platform=platform, chunk=chunk))
                 futures[future] = (platform, chunk)
 
         with sqlite3.connect(sqlite_file) as conn:
@@ -35,31 +80,16 @@ def generate(repo_dir, revision, artifactsHandler, out_dir='.'):
                 files = future.result()
                 c.executemany('INSERT INTO file_to_chunk VALUES (?,?,?)', ((f, platform, chunk) for f in files))
 
-            try:
-                # Retrieve chunk -> tests mapping from ActiveData.
-                r = requests.post('https://activedata.allizom.org/query', json={
-                    'from': 'unittest',
-                    'where': {'and': [
-                        {'eq': {'repo.branch.name': 'mozilla-central'}},
-                        {'eq': {'repo.changeset.id12': revision[:12]}},
-                        {'or': [
-                            {'prefix': {'run.key': 'test-linux64-ccov'}},
-                            {'prefix': {'run.key': 'test-windows10-64-ccov'}}
-                        ]}
-                    ]},
-                    'limit': 50000,
-                    'select': ['result.test', 'run.key']
-                })
+            for platform in PLATFORMS:
+                for suite in get_suites(revision):
+                    tests_data = get_tests_chunks(revision, platform, suite)
+                    if len(tests_data) == 0:
+                        continue
 
-                tests_data = r.json()['data']
-
-                task_names = tests_data['run.key']
-                test_iter = enumerate(tests_data['result.test'])
-                chunk_test_iter = ((taskcluster.get_platform(task_names[i]), taskcluster.get_chunk(task_names[i]), test) for i, test in test_iter)
-                c.executemany('INSERT INTO chunk_to_test VALUES (?,?,?)', chunk_test_iter)
-            except KeyError:
-                # ActiveData is failing too often, so we need to ignore the error here.
-                logger.error('Failed to retrieve chunk to tests mapping from ActiveData.')
+                    task_names = tests_data['run.key']
+                    test_iter = enumerate(tests_data['result.test'])
+                    chunk_test_iter = ((taskcluster.get_platform(task_names[i]), taskcluster.get_chunk(task_names[i]), test) for i, test in test_iter)
+                    c.executemany('INSERT INTO chunk_to_test VALUES (?,?,?)', chunk_test_iter)
 
     with tarfile.open(tarxz_file, 'w:xz') as tar:
         tar.add(sqlite_file, os.path.basename(sqlite_file))
