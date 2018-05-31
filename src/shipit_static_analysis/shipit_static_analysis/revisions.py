250e630db9504ed80e16e8b2442f9527413c9ef8
Bastien Abadie <bastien@nextcairn.com>
2018-05-31 12:05:28 +0200

    shipit-static-analysis: Use top of MC instead of tip to get common hg ancestor (#1196)

--- src/shipit_static_analysis/shipit_static_analysis/revisions.py
+++ src/shipit_static_analysis/shipit_static_analysis/revisions.py
@@ -178,6 +178,9 @@ class MozReviewRevision(Revision):
         '''
         assert isinstance(repo, hglib.client.hgclient)
 
+        # Get top revision
+        top = repo.log('reverse(public())', limit=1)[0].node.decode('utf-8')
+
         # Pull revision from review
         repo.pull(
             source=REPO_REVIEW,
@@ -187,7 +190,7 @@ class MozReviewRevision(Revision):
         )
 
         # Find common ancestor revision
-        out = repo.log('ancestor(tip, {})'.format(self.mercurial))
+        out = repo.log('ancestor({}, {})'.format(top, self.mercurial))
         assert out is not None and len(out) > 0, \
             'Failed to find ancestor for {}'.format(self.mercurial)
         ancestor = out[0].node.decode('utf-8')
