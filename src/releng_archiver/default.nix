e4d275db9ae316df2a6d5e858ce06b774f330d6a
Rok Garbas <rok@garbas.si>
2018-06-01 03:10:49 +0200

    not needed with new release of pypi2nix

--- src/releng_archiver/default.nix
+++ src/releng_archiver/default.nix
@@ -27,7 +27,6 @@ let
         pushd ${self.src_path}
         ${pypi2nix}/bin/pypi2nix -v \
           -V 3.5 \
-          -W file://$TMPDIR/pypi2nix/cache/wheels \
           -E "postgresql" \
           -r requirements.txt \
           -r requirements-dev.txt
