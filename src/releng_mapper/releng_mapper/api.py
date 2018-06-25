0b3cac609a9e79a8b206115c441b26ad8a068960
Rok Garbas <rok@garbas.si>
2018-06-25 11:23:37 +0200

    releng_mapper: flake8 gardening

--- src/releng_mapper/releng_mapper/api.py
+++ src/releng_mapper/releng_mapper/api.py
@@ -6,146 +6,207 @@
 import calendar
 import re
 import time
-from typing import Optional
-from typing import Tuple
+import typing
 
 import dateutil.parser
-from flask import Response
-from flask import current_app
-from flask import request
-from sqlalchemy import text
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.exc import ProgrammingError
-from sqlalchemy.orm.exc import MultipleResultsFound
-from sqlalchemy.orm.exc import NoResultFound
-from werkzeug.exceptions import BadRequest
-from werkzeug.exceptions import Conflict
-from werkzeug.exceptions import InternalServerError
-from werkzeug.exceptions import NotFound
-from werkzeug.exceptions import UnsupportedMediaType
+import flask
+import sqlalchemy as sa
+import werkzeug.exceptions
 
-from backend_common.auth import auth
-from cli_common import log
+import backend_common.auth
+import cli_common
+import releng_mapper.models
 
-from .models import Hash
-from .models import Project
-
-logger = log.get_logger(__name__)
-
-AUTHENTICATION_SCOPE_PREFIX = 'project:releng:services/releng_mapper/permission/'
+logger = cli_common.log.get_logger(__name__)
+scope_prefix = 'project:releng:services/releng_mapper/permission/'
 
 
-@auth.require_scopes([AUTHENTICATION_SCOPE_PREFIX + 'project/insert'])
+@backend_common.auth.auth.require_scopes([scope_prefix + 'project/insert'])
 def post_project(project: str) -> dict:
-    session = current_app.db.session
-    p = Project(name=project)
-    session.add(p)
+    session = flask.current_app.db.session
 
+    p = releng_mapper.models.Project(name=project)
+
+    session.add(p)
     try:
         session.commit()
-
-    except (IntegrityError, ProgrammingError,):
-        raise Conflict('Project {} could not be inserted into the database'.format(project))
+    except (sa.exc.IntegrityError,
+            sa.exc.ProgrammingError,
+            ):
+        raise werkzeug.exceptions.Conflict(
+            'Project {} could not be inserted into the database'.format(project))
 
     return {}
 
 
-@auth.require_scopes([AUTHENTICATION_SCOPE_PREFIX + 'mapping/insert'])
-def post_hg_git_mapping(project: str, git_commit: str, hg_changeset: str) -> dict:
-    session = current_app.db.session
-    proj = _get_project(session, project)  # can raise HTTP 404 or HTTP 500
-    _add_hash(session, git_commit, hg_changeset, proj)  # can raise HTTP 400
+@backend_common.auth.auth.require_scopes([scope_prefix + 'mapping/insert'])
+def post_hg_git_mapping(project: str,
+                        git_commit: str,
+                        hg_changeset: str,
+                        ) -> dict:
+    session = flask.current_app.db.session
+
+    _add_hash(  # can raise HTTP 400
+        session,
+        git_commit,
+        hg_changeset,
+        _get_project(session, project),  # can raise HTTP 404 or HTTP 500
+    )
+
     try:
         session.commit()
-        q = Hash.query.join(Project).filter(_project_filter(project))
-        q = q.filter(text('git_commit == :commit')).params(commit=git_commit)
+        q = releng_mapper.models.Hash.query
+        q = q.join(releng_mapper.models.Project)
+        q = q.filter(_project_filter(project))
+        q = q.filter(sa.text('git_commit == :commit'))
+        q = q.params(commit=git_commit)
         return q.one().as_json()
 
-    except IntegrityError:
-        raise Conflict('Provided mapping {} {} for project {} already exists and '
-                       'cannot be reinserted'.format(git_commit, hg_changeset, project))
+    except sa.exc.IntegrityError:
+        raise werkzeug.exceptions.Conflict(
+            'Provided mapping {} {} for project {} already exists and cannot '
+            'be reinserted'.format(
+                git_commit,
+                hg_changeset,
+                project,
+            )
+        )
 
-    except NoResultFound:
-        raise InternalServerError('Provided mapping {} {} for project {} could not be inserted '
-                                  'into the database'.format(git_commit, hg_changeset, project))
+    except sa.orm.exc.NoResultFound:
+        raise werkzeug.exceptions.InternalServerError(
+            'Provided mapping {} {} for project {} could not be inserted '
+            'into the database'.format(
+                git_commit,
+                hg_changeset,
+                project,
+            )
+        )
 
-    except MultipleResultsFound:
-        raise InternalServerError('Provided mapping {git_commit} {hg_changeset} for project {project} has been inserted'
-                                  ' into the database multiple times'.format(git_commit=git_commit,
-                                                                             hg_changeset=hg_changeset,
-                                                                             project=project))
+    except sa.orm.exc.MultipleResultsFound:
+        raise werkzeug.exceptions.InternalServerError(
+            'Provided mapping {git_commit} {hg_changeset} for project '
+            '{project} has been inserted into the database multiple '
+            'times'.format(
+                git_commit=git_commit,
+                hg_changeset=hg_changeset,
+                project=project,
+            )
+        )
 
 
-@auth.require_scopes([AUTHENTICATION_SCOPE_PREFIX + 'mapping/insert'])
-def post_insert_many_ignoredups(project: str, body: str) -> dict:
-    session = current_app.db.session
-    return _insert_many(project, body.encode(), session, ignore_dups=True)
+@backend_common.auth.auth.require_scopes([scope_prefix + 'mapping/insert'])
+def post_insert_many_ignoredups(project: str,
+                                body: str,
+                                ) -> dict:
+    return _insert_many(
+        project,
+        body.encode(),
+        flask.current_app.db.session,
+        ignore_dups=True,
+    )
 
 
-@auth.require_scopes([AUTHENTICATION_SCOPE_PREFIX + 'mapping/insert'])
-def post_insert_many(project: str, body: str) -> dict:
-    session = current_app.db.session
-    return _insert_many(project, body.encode(), session, ignore_dups=False)
+@backend_common.auth.auth.require_scopes([scope_prefix + 'mapping/insert'])
+def post_insert_many(project: str,
+                     body: str,
+                     ) -> dict:
+    return _insert_many(
+        project,
+        body.encode(),
+        flask.current_app.db.session,
+        ignore_dups=False,
+    )
 
 
 def get_projects() -> dict:
-    session = current_app.db.session
-    all_projects = session.query(Project).all()
+    session = flask.current_app.db.session
+
+    all_projects = session.query(releng_mapper.models.Project).all()
     if not all_projects:
-        raise NotFound('Could not find any projects in the database.')
+        raise werkzeug.exceptions.NotFound(
+            'Could not find any projects in the database.')
 
-    return {
-        'projects': [
-            project.name
-            for project in all_projects
-        ],
-    }
+    return dict(projects=[project.name for project in all_projects])
 
 
-def get_mapfile_since(projects: str, since: str) -> Tuple[str, int, dict]:
+def get_mapfile_since(projects: str,
+                      since: str,
+                      ) -> typing.Tuple[str, int, dict]:
     try:
         since_dt = dateutil.parser.parse(since)
 
     except ValueError as e:
-        raise BadRequest('Invalid date %s specified; see https://labix.org/python-dateutil: %s' % (since, str(e)))
+        raise werkzeug.exceptions.BadRequest(
+            'Invalid date %s specified; see '
+            'https://labix.org/python-dateutil: %s' % (since, str(e)))
 
     since_epoch = calendar.timegm(since_dt.utctimetuple())
 
-    q = Hash.query.join(Project).filter(_project_filter(projects))
-    q = q.order_by(Hash.hg_changeset)
-    q = q.filter(Hash.date_added > since_epoch)
-
+    q = releng_mapper.models.Hash.query
+    q = q.join(releng_mapper.models.Project)
+    q = q.filter(_project_filter(projects))
+    q = q.order_by(releng_mapper.models.Hash.hg_changeset)
+    q = q.filter(releng_mapper.models.Hash.date_added > since_epoch)
     return _stream_mapfile(q)
 
 
-def get_full_mapfile(projects: str) -> Tuple[str, int, dict]:
-    q = Hash.query.join(Project).filter(_project_filter(projects))
-    q = q.order_by(Hash.hg_changeset)
-
+def get_full_mapfile(projects: str) -> typing.Tuple[str, int, dict]:
+    q = releng_mapper.models.Hash.query
+    q = q.join(releng_mapper.models.Project)
+    q = q.filter(_project_filter(projects))
+    q = q.order_by(releng_mapper.models.Hash.hg_changeset)
     return _stream_mapfile(q)
 
 
-def get_revision(projects: str, vcs_type: str, commit: str) -> str:
+def get_revision(projects: str,
+                 vcs_type: str,
+                 commit: str,
+                 ) -> str:
     _check_well_formed_sha(vcs_type, commit, exact_length=None)  # can raise http 400
-    q = Hash.query.join(Project).filter(_project_filter(projects))
+    q = releng_mapper.models.Hash.query
+    q = q.join(releng_mapper.models.Project)
+    q = q.filter(_project_filter(projects))
+
     if vcs_type == 'git':
-        q = q.filter(text('git_commit like :cspatttern')).params(cspatttern=commit + '%')
+        q = q.filter(sa.text('git_commit like :cspatttern'))
+        q = q.params(cspatttern=commit + '%')
 
     elif vcs_type == 'hg':
-        q = q.filter(text('hg_changeset like :cspatttern')).params(cspatttern=commit + '%')
+        q = q.filter(sa.text('hg_changeset like :cspatttern'))
+        q = q.params(cspatttern=commit + '%')
 
     try:
         row = q.one()
-    except NoResultFound:
+
+    except sa.orm.exc.NoResultFound:
         if vcs_type == 'git':
-            raise NotFound('No hg changeset found for git commit id {} in project(s) {}'.format(commit, projects))
+            raise werkzeug.exceptions.NotFound(
+                'No hg changeset found for git commit id {} in '
+                'project(s) {}'.format(
+                    commit,
+                    projects,
+                )
+            )
 
         elif vcs_type == 'hg':
-            raise NotFound('No git commit found for hg changeset {} in project(s) {}'.format(commit, projects))
+            raise werkzeug.exceptions.NotFound(
+                'No git commit found for hg changeset {} in '
+                'project(s) {}'.format(
+                    commit,
+                    projects,
+                )
+            )
 
-    except MultipleResultsFound:
-        raise InternalServerError('Internal error - multiple results returned for {} commit {} in project {} - '
-                                  'this should not be possible in database'.format(vcs_type, commit, projects))
+    except sa.orm.exc.MultipleResultsFound:
+        raise werkzeug.exceptions.InternalServerError(
+            'Internal error - multiple results returned for {} commit {} in '
+            'project {} - this should not be possible in '
+            'database'.format(
+                vcs_type,
+                commit,
+                projects,
+            )
+        )
 
     return '%s %s' % (row.git_commit, row.hg_changeset)
 
@@ -160,12 +221,12 @@ def _project_filter(projects_arg):
         A SQLAlchemy filter expression
     '''
     if ',' in projects_arg:
-        return Project.name.in_(projects_arg.split(','))
+        return releng_mapper.models.Project.name.in_(projects_arg.split(','))
     else:
-        return Project.name == projects_arg
+        return releng_mapper.models.Project.name == projects_arg
 
 
-def _stream_mapfile(query) -> Tuple[str, int, dict]:
+def _stream_mapfile(query) -> typing.Tuple[str, int, dict]:
     '''Helper method to build a map file from a SQLAlchemy query.
     Args:
         query: SQLAlchemy query
@@ -180,17 +241,20 @@ def _stream_mapfile(query) -> Tuple[str, int, dict]:
     query = query.yield_per(100)
 
     if query.count() == 0:
-        raise NotFound('No mappings found.')
+        raise werkzeug.exceptions.NotFound('No mappings found.')
 
     def contents():
         for r in query:
             yield '{} {}'.format(r.git_commit, r.hg_changeset) + '\n'
 
     if contents:
-        return Response(contents(), mimetype='text/plain')
+        return flask.Response(contents(), mimetype='text/plain')
 
 
-def _check_well_formed_sha(vcs: str, sha: str, exact_length: Optional[int]=40) -> None:
+def _check_well_formed_sha(vcs: str,
+                           sha: str,
+                           exact_length: typing.Optional[int]=40
+                           ) -> None:
     '''Helper method to check for a well-formed SHA.
     Args:
         vcs: Name of the vcs system ('hg' or 'git')
@@ -203,28 +267,37 @@ def _check_well_formed_sha(vcs: str, sha: str, exact_length: Optional[int]=40) -
         HTTP 400: Malformed SHA or unknown vcs
     '''
     if vcs not in ('git', 'hg'):
-        raise BadRequest('Unknown vcs type {}'.format(vcs))
+        raise werkzeug.exceptions.BadRequest(
+            'Unknown vcs type {}'.format(vcs))
 
     rev_regex = re.compile('''^[a-f0-9]{1,40}$''')
     if sha is None:
-        raise BadRequest('{} SHA is <None>'.format(vcs))
+        raise werkzeug.exceptions.BadRequest(
+            '{} SHA is <None>'.format(vcs))
 
     elif sha == '':
-        raise BadRequest('{} SHA is an empty string'.format(vcs))
+        raise werkzeug.exceptions.BadRequest(
+            '{} SHA is an empty string'.format(vcs))
 
     elif not rev_regex.match(sha):
-        raise BadRequest('{} SHA contains bad characters: "{}"'.format(vcs, str(sha)))
+        raise werkzeug.exceptions.BadRequest(
+            '{} SHA contains bad characters: "{}"'.format(vcs, str(sha)))
 
     if exact_length is not None and len(sha) != exact_length:
-        raise BadRequest('{vcs} SHA should be {correct} characters long, but is {actual} characters long: "{sha}"'.format(
-            vcs=vcs,
-            correct=exact_length,
-            actual=len(sha),
-            sha=str(sha)
-        ))
+        raise werkzeug.exceptions.BadRequest(
+            '{vcs} SHA should be {correct} characters long, but is '
+            '{actual} characters long: "{sha}"'.format(
+                vcs=vcs,
+                correct=exact_length,
+                actual=len(sha),
+                sha=str(sha)
+            )
+        )
 
 
-def _get_project(session, project: str) -> Project:
+def _get_project(session,
+                 project: str,
+                 ) -> releng_mapper.models.Project:
     '''Helper method to return Project class for a project with the given name.
     Args:
         session: SQLAlchemy ORM Session object
@@ -236,13 +309,17 @@ def _get_project(session, project: str) -> Project:
         HTTP 500: Multiple projects with same name found
     '''
     try:
-        return session.query(Project).filter(Project.name == project).one()
+        q = session.query(releng_mapper.models.Project)
+        q = q.filter(releng_mapper.models.Project.name == project)
+        return q.one()
 
-    except MultipleResultsFound:
-        raise InternalServerError('Multiple projects with name {} found in database'.format(project))
+    except sa.orm.exc.MultipleResultsFound:
+        raise werkzeug.exceptions.InternalServerError(
+            'Multiple projects with name {} found in database'.format(project))
 
-    except NoResultFound:
-        raise NotFound('Could not find project {} in database'.format(project))
+    except sa.orm.exc.NoResultFound:
+        raise werkzeug.exceptions.NotFound(
+            'Could not find project {} in database'.format(project))
 
 
 def _add_hash(session, git_commit: str, hg_changeset: str, project: str) -> None:
@@ -257,11 +334,20 @@ def _add_hash(session, git_commit: str, hg_changeset: str, project: str) -> None
     '''
     _check_well_formed_sha('git', git_commit)  # can raise http 400
     _check_well_formed_sha('hg', hg_changeset)  # can raise http 400
-    h = Hash(git_commit=git_commit, hg_changeset=hg_changeset, project=project, date_added=time.time())
+
+    h = releng_mapper.models.Hash(git_commit=git_commit,
+                                  hg_changeset=hg_changeset,
+                                  project=project,
+                                  date_added=time.time(),
+                                  )
     session.add(h)
 
 
-def _insert_many(project: str, body: bytes, session, ignore_dups: bool=False) -> dict:
+def _insert_many(project: str,
+                 body: bytes,
+                 session,
+                 ignore_dups: bool=False,
+                 ) -> dict:
     '''Update the database with many git-hg mappings.
     Args:
         project: Single project name string
@@ -276,8 +362,9 @@ def _insert_many(project: str, body: bytes, session, ignore_dups: bool=False) ->
         HTTP 415: Request content-type is not 'text/plain'
         HTTP 500: Multiple projects found with matching project name
     '''
-    if request.content_type != 'text/plain':
-        raise UnsupportedMediaType('HTTP request header "Content-Type" must be set to "text/plain"')
+    if flask.request.content_type != 'text/plain':
+        raise werkzeug.exceptions.UnsupportedMediaType(
+            'HTTP request header "Content-Type" must be set to "text/plain"')
 
     proj = _get_project(session, project)  # can raise HTTP 404 or HTTP 500
     for line in body.decode('utf-8').split('\n'):
@@ -287,11 +374,15 @@ def _insert_many(project: str, body: bytes, session, ignore_dups: bool=False) ->
             git_commit, hg_changeset = line.split(' ')
 
         except ValueError:
-            logger.error('Received input line: "%s" for project %s', line, project)
-            logger.error('Was expecting an input line such as '
-                         '"686a558fad7954d8481cfd6714cdd56b491d2988 fef90029cb654ad9848337e262078e403baf0c7a"')
-            logger.error('i.e. where the first hash is a git commit SHA and the second hash is a mercurial changeset SHA')
-            raise BadRequest('Input line "{}" received for project {} did not contain a space'.format(line, project))
+            logger.error(
+                'Received input line: "{}" for project {}\nWas expecting an '
+                'input line such as "686a558fad7954d8481cfd6714cdd56b491d2988 '
+                'fef90029cb654ad9848337e262078e403baf0c7a"\ni.e. where the '
+                'first hash is a git commit SHA and the second hash is a '
+                'mercurial changeset SHA'.format(line, project))
+            raise werkzeug.exceptions.BadRequest(
+                'Input line "{}" received for project {} did not contain '
+                'a space'.format(line, project))
 
         _add_hash(session, git_commit, hg_changeset, proj)  # can raise HTTP 400
 
@@ -299,15 +390,17 @@ def _insert_many(project: str, body: bytes, session, ignore_dups: bool=False) ->
             try:
                 session.commit()
 
-            except IntegrityError:
+            except sa.exc.IntegrityError:
                 session.rollback()
 
     if not ignore_dups:
         try:
             session.commit()
 
-        except IntegrityError:
+        except sa.exc.IntegrityError:
             session.rollback()
-            raise Conflict('Some of the given mappings for project {} already exist'.format(project))
+            raise werkzeug.exceptions.Conflict(
+                'Some of the given mappings for project {} already '
+                'exist'.format(project))
 
     return {}
--- src/releng_mapper/releng_mapper/models.py
+++ src/releng_mapper/releng_mapper/models.py
@@ -3,44 +3,35 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import Index
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import orm
+import sqlalchemy as sa
 
-from backend_common.db import db
-from cli_common import log
-
-from .config import APP_NAME
-
-logger = log.get_logger(__name__)
+import backend_common.db
+import releng_mapper.config
 
 
-class Project(db.Model):
+class Project(backend_common.db.db.Model):
     '''
     Object-relational mapping between python class Project
     and database table "projects"
     '''
-    __tablename__ = APP_NAME + '_projects'
+    __tablename__ = releng_mapper.config.APP_NAME + '_projects'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False, unique=True)
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(255), nullable=False, unique=True)
 
 
-class Hash(db.Model):
+class Hash(backend_common.db.db.Model):
     '''
     Object-relational mapping between python class Hash
     and database table "hashes"
     '''
-    __tablename__ = APP_NAME + '_hashes'
+    __tablename__ = releng_mapper.config.APP_NAME + '_hashes'
 
-    hg_changeset = Column(String(40), nullable=False)
-    git_commit = Column(String(40), nullable=False)
-    project_id = Column(Integer, ForeignKey(Project.id), nullable=False)
-    project = orm.relationship(Project, primaryjoin=(project_id == Project.id))
-    date_added = Column(Integer, nullable=False)
+    hg_changeset = sa.Column(sa.String(40), nullable=False)
+    git_commit = sa.Column(sa.String(40), nullable=False)
+    project_id = sa.Column(sa.Integer, sa.ForeignKey(Project.id), nullable=False)
+    project = sa.orm.relationship(Project, primaryjoin=(project_id == Project.id))
+    date_added = sa.Column(sa.Integer, nullable=False)
 
     project_name = property(lambda self: self.project.name)
 
@@ -53,13 +44,13 @@ class Hash(db.Model):
     __table_args__ = (
         # TODO: (needs verification) all queries specifying a hash are for
         # (project, hash), so these aren't used
-        Index('hg_changeset', 'hg_changeset'),
-        Index('git_commit', 'git_commit'),
+        sa.Index('hg_changeset', 'hg_changeset'),
+        sa.Index('git_commit', 'git_commit'),
         # TODO: this index is a prefix of others and will never be used
-        Index('project_id', 'project_id'),
-        Index('project_id__date_added', 'project_id', 'date_added'),
-        Index('project_id__hg_changeset', 'project_id', 'hg_changeset', unique=True),
-        Index('project_id__git_commit', 'project_id', 'git_commit', unique=True),
+        sa.Index('project_id', 'project_id'),
+        sa.Index('project_id__date_added', 'project_id', 'date_added'),
+        sa.Index('project_id__hg_changeset', 'project_id', 'hg_changeset', unique=True),
+        sa.Index('project_id__git_commit', 'project_id', 'git_commit', unique=True),
     )
 
     __mapper_args__ = {
