bbc61e8c8c49220d47abe79ea7a6f44f9134e521
Rail Aliiev <rail@mozilla.com>
2018-08-23 10:04:33 -0400

    shipit/frontend: freeze publishing to staging (#1422)

--- lib/please_cli/please_cli/config.py
+++ lib/please_cli/please_cli/config.py
@@ -879,7 +879,8 @@ PROJECTS_CONFIG = {
             {
                 'target': 'S3',
                 'options': {
-                    'testing': {
+                    # FIXME: temporarily disable staging, production, point testing to staging
+                    'staging': {
                         's3_bucket': 'shipit-testing-frontend',
                         'url': 'https://shipit.testing.mozilla-releng.net',
                         'dns': 'd2jpisuzgldax2.cloudfront.net.',
@@ -892,30 +893,30 @@ PROJECTS_CONFIG = {
                             'https://queue.taskcluster.net',
                         ],
                     },
-                    'staging': {
-                        's3_bucket': 'shipit-staging-frontend',
-                        'url': 'https://shipit.staging.mozilla-releng.net',
-                        'dns': 'd2ld4e8bl8yd1l.cloudfront.net.',
-                        'envs': {
-                            'CONFIG': 'staging',
-                        },
-                        'csp': [
-                            'https://hg.mozilla.org',
-                            'https://queue.taskcluster.net',
-                        ],
-                    },
-                    'production': {
-                        's3_bucket': 'shipit-production-frontend',
-                        'url': 'https://shipit.mozilla-releng.net',
-                        'dns': 'dve8yd1431ifz.cloudfront.net.',
-                        'envs': {
-                            'CONFIG': 'production',
-                        },
-                        'csp': [
-                            'https://hg.mozilla.org',
-                            'https://queue.taskcluster.net',
-                        ],
-                    },
+                    # 'staging': {
+                    #     's3_bucket': 'shipit-staging-frontend',
+                    #     'url': 'https://shipit.staging.mozilla-releng.net',
+                    #     'dns': 'd2ld4e8bl8yd1l.cloudfront.net.',
+                    #     'envs': {
+                    #         'CONFIG': 'staging',
+                    #     },
+                    #     'csp': [
+                    #         'https://hg.mozilla.org',
+                    #         'https://queue.taskcluster.net',
+                    #     ],
+                    # },
+                    # 'production': {
+                    #     's3_bucket': 'shipit-production-frontend',
+                    #     'url': 'https://shipit.mozilla-releng.net',
+                    #     'dns': 'dve8yd1431ifz.cloudfront.net.',
+                    #     'envs': {
+                    #         'CONFIG': 'production',
+                    #     },
+                    #     'csp': [
+                    #         'https://hg.mozilla.org',
+                    #         'https://queue.taskcluster.net',
+                    #     ],
+                    # },
                 },
             },
         ],
