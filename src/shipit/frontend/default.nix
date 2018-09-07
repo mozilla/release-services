466780ca65687679af4f5d1147872951192a5c5d
Rail Aliiev <rail@mozilla.com>
2018-09-07 14:22:04 -0400

    shipit/frontend: adjust CSPs

--- src/shipit/frontend/default.nix
+++ src/shipit/frontend/default.nix
@@ -3,6 +3,7 @@
 
 releng_pkgs.lib.mkYarnFrontend {
   src = ./.;
+  csp = "default-src 'none'; img-src 'self' *.gravatar.com data:; script-src 'self' 'unsafe-inline'; style-src 'self'; font-src 'self'; frame-src https://auth.mozilla.auth0.com;";
   src_path = "src/shipit/frontend";
   extraBuildInputs = with releng_pkgs.pkgs; [
     libpng
