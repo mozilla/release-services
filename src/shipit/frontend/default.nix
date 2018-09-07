115731f916be62b0dc593c28c47b94a755691d75
Rail Aliiev <rail@mozilla.com>
2018-09-07 16:48:22 -0400

    Add 'self' to allow auth refreshes

--- src/shipit/frontend/default.nix
+++ src/shipit/frontend/default.nix
@@ -3,7 +3,7 @@
 
 releng_pkgs.lib.mkYarnFrontend {
   src = ./.;
-  csp = "default-src 'none'; img-src 'self' *.gravatar.com data:; script-src 'self' 'unsafe-inline'; style-src 'self'; font-src 'self'; frame-src https://auth.mozilla.auth0.com;";
+  csp = "default-src 'none'; img-src 'self' *.gravatar.com data:; script-src 'self' 'unsafe-inline'; style-src 'self'; font-src 'self'; frame-src 'self' https://auth.mozilla.auth0.com;";
   src_path = "src/shipit/frontend";
   extraBuildInputs = with releng_pkgs.pkgs; [
     libpng
