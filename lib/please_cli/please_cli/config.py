b85c4313292e7909dae10b8035fd743f2164f0a3
Rok Garbas <rok@garbas.si>
2018-09-19 18:41:50 +0200

    shipit/frontend: enable deployment for staging and production

--- lib/please_cli/please_cli/config.py
+++ lib/please_cli/please_cli/config.py
@@ -879,13 +879,24 @@ PROJECTS_CONFIG = {
             {
                 'target': 'S3',
                 'options': {
-                    # FIXME: temporarily disable staging, production, point testing to staging
-                    'staging': {
+                    'testing': {
                         's3_bucket': 'shipit-testing-frontend',
                         'url': 'https://shipit.testing.mozilla-releng.net',
                         'dns': 'd2jpisuzgldax2.cloudfront.net.',
                         'envs': {
                             # Use the same API as staging
+                            'CONFIG': 'testing',
+                        },
+                        'csp': [
+                            'https://hg.mozilla.org',
+                            'https://queue.taskcluster.net',
+                        ],
+                    },
+                    'staging': {
+                        's3_bucket': 'shipit-staging-frontend',
+                        'url': 'https://shipit.staging.mozilla-releng.net',
+                        'dns': 'd2ld4e8bl8yd1l.cloudfront.net.',
+                        'envs': {
                             'CONFIG': 'staging',
                         },
                         'csp': [
@@ -893,30 +904,18 @@ PROJECTS_CONFIG = {
                             'https://queue.taskcluster.net',
                         ],
                     },
-                    # 'staging': {
-                    #     's3_bucket': 'shipit-staging-frontend',
-                    #     'url': 'https://shipit.staging.mozilla-releng.net',
-                    #     'dns': 'd2ld4e8bl8yd1l.cloudfront.net.',
-                    #     'envs': {
-                    #         'CONFIG': 'staging',
-                    #     },
-                    #     'csp': [
-                    #         'https://hg.mozilla.org',
-                    #         'https://queue.taskcluster.net',
-                    #     ],
-                    # },
-                    # 'production': {
-                    #     's3_bucket': 'shipit-production-frontend',
-                    #     'url': 'https://shipit.mozilla-releng.net',
-                    #     'dns': 'dve8yd1431ifz.cloudfront.net.',
-                    #     'envs': {
-                    #         'CONFIG': 'production',
-                    #     },
-                    #     'csp': [
-                    #         'https://hg.mozilla.org',
-                    #         'https://queue.taskcluster.net',
-                    #     ],
-                    # },
+                    'production': {
+                        's3_bucket': 'shipit-production-frontend',
+                        'url': 'https://shipit.mozilla-releng.net',
+                        'dns': 'dve8yd1431ifz.cloudfront.net.',
+                        'envs': {
+                            'CONFIG': 'production',
+                        },
+                        'csp': [
+                            'https://hg.mozilla.org',
+                            'https://queue.taskcluster.net',
+                        ],
+                    },
                 },
             },
         ],
