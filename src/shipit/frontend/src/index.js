2cefd16b48ee5e24d0ba648abc92c8faeace5999
Rok Garbas <rok@garbas.si>
2018-09-19 22:11:09 +0200

    shipit/frontend: enable back deployment (#1526)

--- src/shipit/frontend/src/index.js
+++ src/shipit/frontend/src/index.js
@@ -2,14 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
-import raven from 'raven-js';
 import { render } from 'react-dom';
 import App from './App';
 import {
   BACKEND_URL,
   RELEASE_CHANNEL,
   RELEASE_VERSION,
-  SENTRY_DSN,
 } from './config';
 
 const root = document.getElementById('root');
@@ -24,23 +22,4 @@ const load = () => {
   );
 };
 
-// When SENTRY_DSN is configured send logs to Sentry
-if (SENTRY_DSN !== null) {
-  raven
-    .config(
-      SENTRY_DSN,
-      {
-        debug: true,
-        release: RELEASE_VERSION,
-        environment: RELEASE_CHANNEL,
-        tags: {
-          server_name: 'mozilla/release-services',
-          site: 'shipit/frontend',
-        },
-      },
-    )
-    .install()
-    .context(load);
-} else {
-  load();
-}
+load();
