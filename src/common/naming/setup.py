e07ae1e3b55ffe50165a5dc41c61b9eadd55d2e9
Rok Garbas <rok@garbas.si>
2018-10-02 17:29:19 +0200

    common/naming: read_requirements method was not in setup.py

--- src/common/naming/setup.py
+++ src/common/naming/setup.py
@@ -6,6 +6,23 @@
 import setuptools
 
 
+def read_requirements(file_):
+    lines = []
+    with open(file_) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith('-e ') or line.startswith('http://') or line.startswith('https://'):
+                extras = ''
+                if '[' in line:
+                    extras = '[' + line.split('[')[1].split(']')[0] + ']'
+                line = line.split('#')[1].split('egg=')[1] + extras
+            elif line == '' or line.startswith('#') or line.startswith('-'):
+                continue
+            line = line.split('#')[0].strip()
+            lines.append(line)
+    return sorted(list(set(lines)))
+
+
 with open('VERSION') as f:
     VERSION = f.read().strip()
 
