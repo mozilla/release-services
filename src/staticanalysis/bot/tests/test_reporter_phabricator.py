e56130c20041ae1c1953c9928598e6ef6ab9a446
Bastien Abadie <bastien@nextcairn.com>
2018-10-11 16:58:12 +0200

    staticanalysis/bot: Fix unit test to trigger publication

--- src/staticanalysis/bot/tests/test_reporter_phabricator.py
+++ src/staticanalysis/bot/tests/test_reporter_phabricator.py
@@ -3,21 +3,51 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
+import urllib
+
 import responses
+
+VALID_MESSAGE = '''
+Code analysis found 1 defect in this patch:
+ - 1 defect found by clang-tidy
+
+You can run this analysis locally with:
+ - `./mach static-analysis check path/to/file.cpp` (C/C++)
+
+
+If you see a problem in this automated review, please report it here: https://bit.ly/2IyNRy2
+'''
 
 
 @responses.activate
-def test_phabricator(tmpdir, mock_phabricator):
+def test_phabricator(mock_repository, mock_phabricator):
     '''
-    Test Phabricator reporter
+    Test Phabricator reporter publication on a mock clang-tidy issue
     '''
     from static_analysis_bot.report.phabricator import PhabricatorReporter
     from static_analysis_bot.revisions import PhabricatorRevision
     from static_analysis_bot.clang.tidy import ClangTidyIssue
 
     def _check_comment(request):
-        assert request.body == 'FAIL'
-        # payload = json.loads(request.body)
+        # Check the Phabricator main comment is well formed
+        payload = urllib.parse.parse_qs(request.body)
+        assert payload['output'] == ['json']
+        assert len(payload['params']) == 1
+        details = json.loads(payload['params'][0])
+        assert details == {
+            'revision_id': 51,
+            'message': VALID_MESSAGE,
+            'attach_inlines': 1,
+            '__conduit__': {'token': 'deadbeef'},
+        }
+
+        # Outputs dummy empty response
+        resp = {
+            'error_code': None,
+            'result': None,
+        }
+        return 201, {'Content-Type': 'application/json'}, json.dumps(resp)
 
     responses.add_callback(
         responses.POST,
@@ -27,14 +57,14 @@ def test_phabricator(tmpdir, mock_phabricator):
 
     with mock_phabricator as api:
         revision = PhabricatorRevision('PHID-DIFF-abcdef', api)
-        reporter = PhabricatorReporter()
-        reporter.setup_api(api)
+        revision.lines = {
+            # Add dummy lines diff
+            'test.cpp': [41, 42, 43],
+        }
+        reporter = PhabricatorReporter(api=api)
 
-    issue_parts = ('test.cpp', '42', '51', 'error', 'dummy message', 'dummy-check')
-    issues = [
-      ClangTidyIssue(issue_parts, revision)
-    ]
+    issue_parts = ('test.cpp', '42', '51', 'error', 'dummy message', 'modernize-use-nullptr')
+    issue = ClangTidyIssue(issue_parts, revision)
+    assert issue.is_publishable()
 
-    reporter.publish(issues, revision)
-
-    # TODO: How to intercept the Phabricator review comment?
+    reporter.publish([issue, ], revision)
