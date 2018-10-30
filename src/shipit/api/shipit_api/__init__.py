d500a1adc76e7a330bcee5559dad848b7ac7fd3e
Rail Aliiev <rail@mozilla.com>
2018-10-30 12:35:56 -0400

    Fix IRC env var (#1660)

--- src/shipit/api/shipit_api/__init__.py
+++ src/shipit/api/shipit_api/__init__.py
@@ -28,7 +28,7 @@ def create_app(config=None):
     app.notify = cli_common.taskcluster.get_service(
         'notify',
         os.environ.get('TASKCLUSTER_CLIENT_ID', app.config.get('TASKCLUSTER_CLIENT_ID')),
-        os.environ.get('TASKCLUSTER_ACCESS_TOKEN', app.config.get('TASKCLUSTER_CLIENT_ID')),
+        os.environ.get('TASKCLUSTER_ACCESS_TOKEN', app.config.get('TASKCLUSTER_ACCESS_TOKEN')),
     )
 
     app.api.register(os.path.join(os.path.dirname(__file__), 'api.yml'))
