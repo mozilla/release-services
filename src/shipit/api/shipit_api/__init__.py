83a1616c780b7ab49a7ccfc04f43a34db8d7bb68
Rok Garbas <rok@garbas.si>
2018-11-15 12:04:45 +0100

    flake8 garden

--- src/shipit/api/shipit_api/__init__.py
+++ src/shipit/api/shipit_api/__init__.py
@@ -3,9 +3,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-
 import os
+
 import backend_common
 import cli_common.taskcluster
 import shipit_api.cli
