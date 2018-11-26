91a6a96247406ae77e74c54275797375dca2e2e3
Kevin Luvian <kevinlsmak1@gmail.com>
2018-11-27 04:17:28 +0800

    please-cli: change ./please check to use nix-build instead of nix-shell (#1677)

--- lib/please_cli/please_cli/check.py
+++ lib/please_cli/please_cli/check.py
@@ -35,38 +35,35 @@ PROJECTS:
     type=click.Choice(please_cli.config.PROJECTS),
     )
 @click.option(
-    '--nix-shell',
+    '--nix-build',
     required=True,
-    default=please_cli.config.NIX_BIN_DIR + 'nix-shell',
-    help='Path to nix-shell command (default: {}).'.format(
-        please_cli.config.NIX_BIN_DIR + 'nix-shell',
+    default=please_cli.config.NIX_BIN_DIR + 'nix-build',
+    help='Path to nix-build command (default: {}).'.format(
+        please_cli.config.NIX_BIN_DIR + 'nix-build',
         ),
     )
 @cli_common.cli.taskcluster_options
 @click.pass_context
-def cmd(ctx, project, nix_shell,
+def cmd(ctx, project, nix_build,
         taskcluster_secret,
         taskcluster_client_id,
         taskcluster_access_token,
-    ):
-    checks = please_cli.config.PROJECTS_CONFIG.get(project, {}).get('checks')
-
-    if not checks:
-        raise click.ClickException('No checks found for `{}` project.'.format(project))
-
-    for check_title, check_command in checks:
-        click.echo(' => {}: '.format(check_title), nl=False)
-        with click_spinner.spinner():
-            returncode, output, error = ctx.invoke(please_cli.shell.cmd,
-                                                   project=project,
-                                                   quiet=True,
-                                                   command=check_command,
-                                                   nix_shell=nix_shell,
-                                                   taskcluster_secret=taskcluster_secret,
-                                                   taskcluster_client_id=taskcluster_client_id,
-                                                   taskcluster_access_token=taskcluster_access_token,
-                                                   )
-        please_cli.utils.check_result(returncode, output, raise_exception=False)
+        ):
+    with click_spinner.spinner():
+        click.echo(f' => Testing project {project} ...', nl=False)
+        outputs = ctx.invoke(please_cli.build.cmd,
+                             project=project,
+                             nix_path_attributes=[project],
+                             interactive=False,
+                             nix_build=nix_build,
+                             taskcluster_secret=taskcluster_secret,
+                             taskcluster_client_id=taskcluster_client_id,
+                             taskcluster_access_token=taskcluster_access_token,
+                             )
+        if len(outputs) == 1:
+            click.secho('DONE', fg='green')
+        else:
+            click.secho('ERROR', fg='red')
 
 
 if __name__ == "__main__":
