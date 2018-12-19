0042bedd9458afc26c9b28214a3b6ffb023f8b7f
Rok Garbas <rok@garbas.si>
2018-12-19 13:22:01 +0100

    break retry on specific exception

--- lib/cli_common/cli_common/utils.py
+++ lib/cli_common/cli_common/utils.py
@@ -3,23 +3,30 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import concurrent.futures
 import time
-from concurrent.futures import ThreadPoolExecutor
+
+import click
+import concurrent.futures
 
 
-def retry(operation, retries=5, wait_between_retries=30):
+def retry(operation,
+          retries=5,
+          wait_between_retries=30,
+          exception_to_break=click.ClickException,
+          ):
     while True:
         try:
             return operation()
-        except Exception:
+        except Exception as e:
+            if isinstance(e, exception_to_break):
+                raise
             retries -= 1
             if retries == 0:
                 raise
             time.sleep(wait_between_retries)
 
 
-class ThreadPoolExecutorResult(ThreadPoolExecutor):
+class ThreadPoolExecutorResult(concurrent.futures.ThreadPoolExecutor):
     def __init__(self, *args, **kwargs):
         self.futures = []
         super(ThreadPoolExecutorResult, self).__init__(*args, **kwargs)
