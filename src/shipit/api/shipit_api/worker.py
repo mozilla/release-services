9a1f48c56dd6e007a0131147a19c4a883a0f8a45
Rok Garbas <rok@garbas.si>
2019-01-16 23:24:58 +0100

    cli_common/pulse: desclare exchange when it includes pulse user in the name (#1791)

--- src/shipit/api/shipit_api/worker.py
+++ src/shipit/api/shipit_api/worker.py
@@ -53,21 +53,19 @@ async def rebuild_product_details(channel, body, envelope, properties):
 def cmd():
     pulse_user = flask.current_app.config['PULSE_USER']
     pulse_pass = flask.current_app.config['PULSE_PASSWORD']
-    queue = f'exchange/{pulse_user}/{shipit_api.config.PROJECT_NAME}'
+    exchange = f'exchange/{pulse_user}/{shipit_api.config.PROJECT_NAME}'
     rebuild_product_details_consumer = cli_common.pulse.create_consumer(
         pulse_user,
         pulse_pass,
-        queue,
+        exchange,
         shipit_api.config.PULSE_ROUTE_REBUILD_PRODUCT_DETAILS,
         rebuild_product_details,
     )
-
     logger.info(
         'Listening for new messages on',
-        queue=queue,
+        exchange=exchange,
         route=shipit_api.config.PULSE_ROUTE_REBUILD_PRODUCT_DETAILS,
     )
-
     cli_common.pulse.run_consumer(asyncio.gather(*[
         rebuild_product_details_consumer,
     ]))
