ea6220d594601280cb1ea80d28f37693e0bc0bf6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-01-29 18:02:32 +0100

    Increase maximum runtime to 21 hours

--- src/bugbug/data/default.nix
+++ src/bugbug/data/default.nix
@@ -46,8 +46,8 @@ let
           "--cache-root"
           "/cache"
         ];
-        deadline = "14 hours";
-        maxRunTime = 14 * 60 * 60;
+        deadline = "21 hours";
+        maxRunTime = 21 * 60 * 60;
         taskArtifacts = {
           "public/bugs.json.xz" = {
             type = "file";
