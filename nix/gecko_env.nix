401e0cab17191fb19d8d89198c00f28785e413e3
Bastien Abadie <bastien@nextcairn.com>
2019-02-11 11:20:20 +0100

    staticanalysis/bot: Update rust-cbindgen to 0.7.1

--- nix/gecko_env.nix
+++ nix/gecko_env.nix
@@ -17,7 +17,7 @@ let
     "${glibc.dev}/include"
   ];
 
-  # Mach needs 0.6.8 at least
+  # Mach needs 0.7.1 at least
   # From https://github.com/NixOS/nixpkgs/blob/cdf90258e6bf911db2b56280301014a88c91be65/pkgs/development/tools/rust/cbindgen/default.nix
   rustPlatform = mkRustPlatform {
     rust = {
@@ -27,13 +27,13 @@ let
   };
   rust-cbindgen =  rustPlatform.buildRustPackage rec {
     name = "rust-cbindgen-${version}";
-    version = "0.6.8";
+    version = "0.7.1";
 
     src = fetchFromGitHub {
       owner = "eqrion";
       repo = "cbindgen";
       rev = "v${version}";
-      sha256 = "0qjdccpyv3lkhnh3f5nzarbdc46hg0c6pmpj0x7sfdm22vbr85w5";
+      sha256 = "082ggpacxj6hmycgasy07klxhlwzndb89s03mwa4khj0l6xkqcjw";
     };
 
     cargoSha256 = "1s12xn4xbxq422wgafxdb9nll0lk65wa6x3acicx5pj1y5ijjmmw";
