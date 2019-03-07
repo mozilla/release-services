ba25f6c0817bf0becc6529efd5b7823ddf31bb46
Bastien Abadie <bastien@nextcairn.com>
2019-03-07 16:21:30 +0100

    nix: bump rust-cbindgen to 0.8.2

--- nix/gecko_env.nix
+++ nix/gecko_env.nix
@@ -17,7 +17,7 @@ let
     "${glibc.dev}/include"
   ];
 
-  # Mach needs 0.7.1 at least
+  # Mach needs 0.8.2 at least
   # From https://github.com/NixOS/nixpkgs/blob/cdf90258e6bf911db2b56280301014a88c91be65/pkgs/development/tools/rust/cbindgen/default.nix
   rustPlatform = mkRustPlatform {
     rust = {
@@ -27,16 +27,16 @@ let
   };
   rust-cbindgen =  rustPlatform.buildRustPackage rec {
     name = "rust-cbindgen-${version}";
-    version = "0.7.1";
+    version = "0.8.2";
 
     src = fetchFromGitHub {
       owner = "eqrion";
       repo = "cbindgen";
       rev = "v${version}";
-      sha256 = "082ggpacxj6hmycgasy07klxhlwzndb89s03mwa4khj0l6xkqcjw";
+      sha256 = "1ck0zyhrrj61rxcmz4045m4nl04g6r971min5hz5p8cmx4h5gl9w";
     };
 
-    cargoSha256 = "1s12xn4xbxq422wgafxdb9nll0lk65wa6x3acicx5pj1y5ijjmmw";
+    cargoSha256 = "00j5nm491zil6kpjns31qyd6z7iqd77b5qp4h7149s70qjwfq2cb";
   };
 
 in gecko.overrideDerivation (old: {
