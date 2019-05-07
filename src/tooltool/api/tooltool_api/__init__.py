be6d54eb55ed7537ec58f0251089d5a9f6fc8935
Rok Garbas <rok@garbas.si>
2019-05-07 16:36:29 +0200

    tooltool/api: provide client via api

--- src/tooltool/api/tooltool_api/__init__.py
+++ src/tooltool/api/tooltool_api/__init__.py
@@ -53,4 +53,6 @@ def create_app(config: dict = None) -> flask.Flask:
     app.cli.add_command(tooltool_api.cli.cmd_replicate, 'replicate')
     app.cli.add_command(tooltool_api.cli.cmd_check_pending_uploads, 'check-pending-uploads')
 
+    app.add_url_rule('/tooltool.py', view_func=tooltool_api.api.download_client)
+
     return app
