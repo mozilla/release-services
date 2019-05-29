35a1da750a3daa419d26d13d03a4421d921265a5
Johan Lorenzo <jlorenzo@mozilla.com>
2019-05-29 15:10:48 +0200

    Bug 1554046 - Let ship-it use the Fennec new version files

--- src/shipit/frontend/src/components/mercurial.js
+++ src/shipit/frontend/src/components/mercurial.js
@@ -7,11 +7,30 @@ export async function getPushes(repo) {
   return req.json();
 }
 
+function buildUrl(repo, revision, appName, productKey) {
+  const urlParts = [`${repo}/raw-file/${revision}`];
+  if (productKey && productKey.startsWith('fennec_')) {
+    urlParts.push('mobile/android/config/version-files');
+
+    if (productKey === 'fennec_beta') {
+      urlParts.push('beta');
+    } else if (productKey === 'fennec_release') {
+      urlParts.push('release');
+    } else {
+      throw new Error(`Unsupported productKey: ${productKey}`);
+    }
+  } else {
+    urlParts.push(appName);
+  }
+  urlParts.push('version_display.txt');
+  return urlParts.join('/');
+}
+
 /**
  * Get in-tree product "display" version.
  */
-export async function getVersion(repo, revision, appName) {
-  const url = `${repo}/raw-file/${revision}/${appName}/config/version_display.txt`;
+export async function getVersion(repo, revision, appName, productKey) {
+  const url = buildUrl(repo, revision, appName, productKey);
   const res = await fetch(url);
   if (res.ok) {
     const version = await res.text();
