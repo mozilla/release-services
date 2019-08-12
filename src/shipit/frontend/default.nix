067685226aac66f54007b186d5fc9bff03d5d278
Rail Aliiev <rail@mozilla.com>
2019-08-12 09:28:27 -0400

    shipit/frontend: set frame-ancestors (#2224)

--- src/shipit/frontend/default.nix
+++ src/shipit/frontend/default.nix
@@ -7,7 +7,7 @@ in releng_pkgs.lib.mkYarnFrontend {
   project_name = "shipit/frontend";
   version = fileContents ./VERSION;
   src = ./.;
-  csp = "default-src 'none'; img-src 'self' *.gravatar.com data:; script-src 'self' 'unsafe-inline' https://cdn.auth0.com; style-src 'self'; font-src 'self'; frame-src 'self' https://auth.mozilla.auth0.com;";
+  csp = "default-src 'none'; frame-ancestors 'none'; img-src 'self' *.gravatar.com data:; script-src 'self' 'unsafe-inline' https://cdn.auth0.com; style-src 'self'; font-src 'self'; frame-src 'self' https://auth.mozilla.auth0.com;";
   extraBuildInputs = with releng_pkgs.pkgs; [
     libpng
     libpng.dev
