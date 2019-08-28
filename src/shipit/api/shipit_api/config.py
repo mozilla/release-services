688ba609ac5f7985d5bb11b5f189dbdf6983b17a
Rail Aliiev <rail@mozilla.com>
2019-08-27 23:16:22 -0400

    shipit/api: switch Thunderbird to esr68

--- src/shipit/api/shipit_api/config.py
+++ src/shipit/api/shipit_api/config.py
@@ -64,7 +64,7 @@ IOS_VERSION = '12.1'
 LATEST_THUNDERBIRD_ALPHA_VERSION = '54.0a2'
 LATEST_THUNDERBIRD_NIGHTLY_VERSION = '70.0a1'
 # TODO: Need to update this every cycle?
-THUNDERBIRD_RELEASE_BRANCH = 'releases/comm-esr60'
+THUNDERBIRD_RELEASE_BRANCH = 'releases/comm-esr68'
 THUNDERBIRD_BETA_BRANCH = 'releases/comm-beta'
 
 # Mixed
