c20901078bf17635475d0d952924112519ea9933
Johan Lorenzo <jlorenzo@mozilla.com>
2019-09-02 11:09:08 +0200

    Bump Firefox and Fennec nightlies

--- src/shipit/api/shipit_api/config.py
+++ src/shipit/api/shipit_api/config.py
@@ -40,8 +40,8 @@ ESR_BRANCH_PREFIX = 'releases/mozilla-esr'
 # day).
 # We could have used the in-tree version, but there can be race conditions,
 # e.g. version bumped, but still no builds available.
-FIREFOX_NIGHTLY = '70.0a1'
-FENNEC_NIGHTLY = '68.1a1'
+FIREFOX_NIGHTLY = '71.0a1'
+FENNEC_NIGHTLY = '68.2a1'
 # The next 6 dates are information about the current and next release
 # They must be updated at the same time as FIREFOX_NIGHTLY
 # They can be found: https://wiki.mozilla.org/Release_Management/Calendar
