3173820314236186bf26d3d70a38432d1c995ff5
Rok Garbas <rok@garbas.si>
2019-09-23 16:50:27 +0200

    fix some styling again

--- src/releng_frontend/src/App/Layout.elm
+++ src/releng_frontend/src/App/Layout.elm
@@ -112,10 +112,15 @@ view : (App.Model -> Html.Html App.Msg) -> App.Model -> Html.Html App.Msg
 view viewRoute model =
     let
         routeName =
-            model.route
-                |> toString
-                |> String.toLower
-                |> String.dropRight (String.length "Route")
+            case model.route of
+                App.ToolToolRoute ->
+                    "tooltool"
+
+                App.TreeStatusRoute _ ->
+                    "treestatus"
+
+                _ ->
+                    ""
 
         isLoading =
             case model.user.tokens of
